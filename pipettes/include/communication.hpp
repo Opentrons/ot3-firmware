c30b8e2c01b8967f543a894e764b9ef0e0f11c05
amit lissack <amit@opentrons.com>
2021-06-25 08:55:51 -0400

    bit_utils

--- pipettes/include/communication.hpp
+++ pipettes/include/communication.hpp
@@ -2,13 +2,16 @@
 
 #include <array>
 #include <concepts>
+#include <span>
 
 #include "pipette_messages.h"
+#include "bit_utils.hpp"
 
 template <class C>
 concept ReaderProtocol = requires(C c, int l, uint8_t* buff) {
     {c.recv(l, buff)};
 };
+
 
 class MessageReader {
   public:
@@ -23,6 +26,7 @@ class MessageReader {
     std::array<uint8_t, max_payload_length> payload_buffer{};
 };
 
+
 template <ReaderProtocol reader>
 auto MessageReader::read_command(reader& communication)
     -> pipette_messages::ReceivedMessage {
@@ -35,13 +39,10 @@ auto MessageReader::read_command(reader& communication)
     // Read the remainder
     communication.recv(length, payload_buffer.data());
 
-    const auto *iterator = payload_buffer.cbegin();
+    auto span = std::span{payload_buffer};
 
     uint32_t arbitration_id = 0;
-    arbitration_id |= (*iterator++ << 24);
-    arbitration_id |= (*iterator++ << 16);
-    arbitration_id |= (*iterator++ << 8);
-    arbitration_id |= *iterator++;
+    bit_utils::bytes_to_int(span.subspan(0, 4), arbitration_id);
 
     switch (arbitration_id) {
         case static_cast<uint32_t>(pipette_messages::MessageType::stop):
@@ -52,10 +53,7 @@ auto MessageReader::read_command(reader& communication)
             break;
         case static_cast<uint32_t>(pipette_messages::MessageType::set_speed): {
             uint32_t speed = 0;
-            speed |= (*iterator++ << 24);
-            speed |= (*iterator++ << 16);
-            speed |= (*iterator++ << 8);
-            speed |= *iterator++;
+            bit_utils::bytes_to_int(span.subspan(4, 4), speed);
             r = pipette_messages::SetSpeed{speed};
             break;
         }
