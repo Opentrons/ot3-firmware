9092c9c5b9676fbf59c9a2df6bd89263ac1dc1ac
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2021-07-02 15:45:48 -0400

    use hal spi msp funcs

--- gantry/firmware/main.cpp
+++ gantry/firmware/main.cpp
@@ -9,8 +9,8 @@
 #include "task.h"
 // clang-format on
 
-#include "common/firmware/uart.h"
 #include "common/firmware/spi.h"
+#include "common/firmware/uart.h"
 
 constexpr auto stack_size = 100;
 static std::array<StackType_t, stack_size>
@@ -20,19 +20,19 @@ static std::array<StackType_t, stack_size>
 static StaticTask_t
     data;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
-static void vTaskCode(void* vParam);
-static void spiTask(void* spiParam);
+static void vTaskCode(void *vParam);
+static void spiTask(void *spiParam);
 static void driveMotor();
 
 uint8_t aTxBuffer[5] = {0x04, 0, 0, 0, 0};
-//uint8_t aTxBuffer[] = "Hi";
+// uint8_t aTxBuffer[] = "Hi";
 uint8_t aRxBuffer[5];
 __IO uint32_t wTransferState = TRANSFER_WAIT;
 
-static uint16_t Buffercmp(uint8_t *pBuffer1, uint8_t *pBuffer2, uint16_t BufferLength);
+static uint16_t Buffercmp(uint8_t *pBuffer1, uint8_t *pBuffer2,
+                          uint16_t BufferLength);
 
-
-static void vTaskCode(void* vParam) {
+static void vTaskCode(void *vParam) {
     vParam = nullptr;
 
     constexpr auto timeout = 0xFFFF;
@@ -56,53 +56,60 @@ static void vTaskCode(void* vParam) {
     }
 }
 
-static void spiTask(void* spiParam) {
+static void spiTask(void *spiParam) {
     spiParam = nullptr;
-    MX_GPIOB_Init();
-    MX_DMA_Init();
+    //    MX_DMA_Init();
     SPI_HandleTypeDef hspi1 = MX_SPI1_Init();
     constexpr auto timeout = 0xFFFF;
 
-    /*##-1- Start the Full Duplex Communication process ########################*/
+    /*##-1- Start the Full Duplex Communication process
+     * ########################*/
     /* While the SPI in TransmitReceive process, user can transmit data through
        "aTxBuffer" buffer & receive data through "aRxBuffer" */
     for (;;) {
-        HAL_SPI_TransmitReceive(&hspi1, aTxBuffer, aRxBuffer, sizeof(aTxBuffer), timeout);
+        //        __HAL_SPI_DISABLE(&hspi1);
+        HAL_SPI_Transmit(&hspi1, aTxBuffer, sizeof(aTxBuffer), timeout);
         __HAL_SPI_DISABLE(&hspi1);
-//        while(hspi1.State == HAL_SPI_STATE_BUSY);
-        vTaskDelay(20);
+        //        __HAL_SPI_DISABLE(&hspi1);
+        //        while(hspi1.State == HAL_SPI_STATE_BUSY);
+        //        vTaskDelay(20);
     }
-//        if (HAL_SPI_TransmitReceive(&hspi1, (uint8_t *)aTxBuffer, (uint8_t *)aRxBuffer, BUFFERSIZE, timeout) != HAL_OK)
-//        {
-//            /* Transfer error in transmission process */
-//            Spi_Error_Handler();
-//        }
-//
-//        /*##-2- Wait for the end of the transfer ###################################*/
-//        /*  Before starting a new communication transfer, you must wait the callback call
-//            to get the transfer complete confirmation or an error detection.
-//            For simplicity reasons, this example is just waiting till the end of the
-//            transfer, but application may perform other tasks while transfer operation
-//            is ongoing. */
-//        while (wTransferState == TRANSFER_WAIT)
-//        {
-////            HAL_SPI_TransmitReceive(&hspi1, aTxBuffer, aRxBuffer, sizeof(aTxBuffer), timeout);
-//        }
-//
-//        switch (wTransferState)
-//        {
-//            case TRANSFER_COMPLETE :
-//                /*##-3- Compare the sent and received buffers ##############################*/
-//                if (Buffercmp((uint8_t *)aTxBuffer, (uint8_t *)aRxBuffer, BUFFERSIZE))
-//                {
-//                    /* Processing Error */
-//                    Spi_Error_Handler();
-//                }
-//                break;
-//            default :
-//                Spi_Error_Handler();
-//                break;
-//        }
+    //        if (HAL_SPI_TransmitReceive(&hspi1, (uint8_t *)aTxBuffer, (uint8_t
+    //        *)aRxBuffer, BUFFERSIZE, timeout) != HAL_OK)
+    //        {
+    //            /* Transfer error in transmission process */
+    //            Spi_Error_Handler();
+    //        }
+    //
+    //        /*##-2- Wait for the end of the transfer
+    //        ###################################*/
+    //        /*  Before starting a new communication transfer, you must wait
+    //        the callback call
+    //            to get the transfer complete confirmation or an error
+    //            detection. For simplicity reasons, this example is just
+    //            waiting till the end of the transfer, but application may
+    //            perform other tasks while transfer operation is ongoing. */
+    //        while (wTransferState == TRANSFER_WAIT)
+    //        {
+    ////            HAL_SPI_TransmitReceive(&hspi1, aTxBuffer, aRxBuffer,
+    /// sizeof(aTxBuffer), timeout);
+    //        }
+    //
+    //        switch (wTransferState)
+    //        {
+    //            case TRANSFER_COMPLETE :
+    //                /*##-3- Compare the sent and received buffers
+    //                ##############################*/ if (Buffercmp((uint8_t
+    //                *)aTxBuffer, (uint8_t *)aRxBuffer, BUFFERSIZE))
+    //                {
+    //                    /* Processing Error */
+    //                    Spi_Error_Handler();
+    //                }
+    //                break;
+    //            default :
+    //                Spi_Error_Handler();
+    //                break;
+    //        }
 }
 
 static void driveMotor() {
@@ -120,8 +127,8 @@ auto main() -> int {
     MX_GPIOA_Init();
     __HAL_RCC_SPI2_CLK_ENABLE();
 
-//    xTaskCreateStatic(vTaskCode, "USART Task", stack.size(), nullptr, 1,
-//                      stack.data(), &data);
+    //    xTaskCreateStatic(vTaskCode, "USART Task", stack.size(), nullptr, 1,
+    //                      stack.data(), &data);
 
     xTaskCreateStatic(spiTask, "SPI Task", stack.size(), nullptr, 1,
                       stack.data(), &data);
@@ -130,12 +137,10 @@ auto main() -> int {
     return 0;
 }
 
-static uint16_t Buffercmp(uint8_t *pBuffer1, uint8_t *pBuffer2, uint16_t BufferLength)
-{
-    while (BufferLength--)
-    {
-        if ((*pBuffer1) != *pBuffer2)
-        {
+static uint16_t Buffercmp(uint8_t *pBuffer1, uint8_t *pBuffer2,
+                          uint16_t BufferLength) {
+    while (BufferLength--) {
+        if ((*pBuffer1) != *pBuffer2) {
             return BufferLength;
         }
         pBuffer1++;
--- gantry/firmware/spi.c
+++ gantry/firmware/spi.c
@@ -1,12 +1,79 @@
 #include "common/firmware/spi.h"
+/**
+ * @brief SPI MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hspi: SPI handle pointer
+ * @retval None
+ */
+void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
+    // PB14 -> MISO
+    // PB12 -> CS
+    // PB13 -> CLK
+    // PB15 -> MOSI
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (hspi->Instance == SPI2) {
+        /* USER CODE BEGIN SPI2_MspInit 0 */
+
+        /* USER CODE END SPI2_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_SPI2_CLK_ENABLE();
+
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**SPI2 GPIO Configuration
+        PB12     ------> SPI2_NSS
+        PB13     ------> SPI2_SCK
+        PB14     ------> SPI2_MISO
+        PB15     ------> SPI2_MOSI
+        */
+        GPIO_InitStruct.Pin =
+            GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN SPI2_MspInit 1 */
+
+        /* USER CODE END SPI2_MspInit 1 */
+    }
+}
 
 /**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
-SPI_HandleTypeDef MX_SPI1_Init() {
+ * @brief SPI MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hspi: SPI handle pointer
+ * @retval None
+ */
+void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi) {
+    if (hspi->Instance == SPI2) {
+        /* USER CODE BEGIN SPI2_MspDeInit 0 */
 
+        /* USER CODE END SPI2_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_SPI2_CLK_DISABLE();
+
+        /**SPI2 GPIO Configuration
+        PB12     ------> SPI2_NSS
+        PB13     ------> SPI2_SCK
+        PB14     ------> SPI2_MISO
+        PB15     ------> SPI2_MOSI
+        */
+        HAL_GPIO_DeInit(GPIOB,
+                        GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
+
+        /* USER CODE BEGIN SPI2_MspDeInit 1 */
+
+        /* USER CODE END SPI2_MspDeInit 1 */
+    }
+}
+
+/**
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
+SPI_HandleTypeDef MX_SPI1_Init() {
     /* USER CODE BEGIN SPI2_Init 0 */
 
     /* USER CODE END SPI2_Init 0 */
@@ -17,26 +84,23 @@ SPI_HandleTypeDef MX_SPI1_Init() {
     /* SPI2 parameter configuration*/
     SPI_HandleTypeDef hspi1 = {
         .Instance = SPI2,
-        .Init = {
-            .Mode = SPI_MODE_MASTER,
-            .Direction = SPI_DIRECTION_2LINES,
-            .DataSize = SPI_DATASIZE_8BIT,
-            .CLKPolarity = SPI_POLARITY_LOW,
-            .CLKPhase = SPI_PHASE_2EDGE,
-            .NSS = SPI_NSS_HARD_OUTPUT,
-            .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32,
-            .FirstBit = SPI_FIRSTBIT_MSB,
-            .TIMode = SPI_TIMODE_DISABLE,
-            .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
-            .CRCPolynomial = 7,
-            .CRCLength = SPI_CRC_LENGTH_DATASIZE,
-            .NSSPMode = SPI_NSS_PULSE_DISABLE
-        }
+        .Init = {.Mode = SPI_MODE_MASTER,
+                 .Direction = SPI_DIRECTION_2LINES,
+                 .DataSize = SPI_DATASIZE_8BIT,
+                 .CLKPolarity = SPI_POLARITY_LOW,
+                 .CLKPhase = SPI_PHASE_2EDGE,
+                 .NSS = SPI_NSS_HARD_OUTPUT,
+                 .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32,
+                 .FirstBit = SPI_FIRSTBIT_MSB,
+                 .TIMode = SPI_TIMODE_DISABLE,
+                 .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
+                 .CRCPolynomial = 7,
+                 .CRCLength = SPI_CRC_LENGTH_DATASIZE,
+                 .NSSPMode = SPI_NSS_PULSE_ENABLE}
 
     };
 
-    if (HAL_SPI_Init(&hspi1) != HAL_OK)
-    {
+    if (HAL_SPI_Init(&hspi1) != HAL_OK) {
         Spi_Error_Handler();
     }
     /* USER CODE BEGIN SPI2_Init 2 */
@@ -46,13 +110,11 @@ SPI_HandleTypeDef MX_SPI1_Init() {
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-void MX_GPIOA_Init(void)
-{
-
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+void MX_GPIOA_Init(void) {
     /* GPIO Ports Clock Enable */
     __HAL_RCC_GPIOA_CLK_ENABLE();
     GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -60,43 +122,16 @@ void MX_GPIOA_Init(void)
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-//    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+    //    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
     HAL_GPIO_Init(
-        GPIOA,  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
-        &GPIO_InitStruct);  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
-    }
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-void MX_GPIOB_Init(void)
-{
-
-    //PB14 -> MISO
-    //PB12 -> CS
-    //PB13 -> CLK
-    //PB15 -> MOSI
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-    HAL_GPIO_Init(
-        GPIOB,  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
+        GPIOA,              // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
         &GPIO_InitStruct);  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
 }
 
 /**
-  * Enable DMA controller clock
-  */
-void MX_DMA_Init(void)
-{
-
+ * Enable DMA controller clock
+ */
+void MX_DMA_Init(void) {
     /* DMA controller clock enable */
     __HAL_RCC_DMAMUX1_CLK_ENABLE();
     __HAL_RCC_DMA1_CLK_ENABLE();
@@ -108,19 +143,14 @@ void MX_DMA_Init(void)
     /* DMA1_Channel3_IRQn interrupt configuration */
     HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
-
 }
 
-
-void Spi_Error_Handler(void)
-{
+void Spi_Error_Handler(void) {
     /* USER CODE BEGIN Error_Handler_Debug */
-    /* User can add his own implementation to report the HAL error return state */
+    /* User can add his own implementation to report the HAL error return state
+     */
     __disable_irq();
-    while (1)
-    {
+    while (1) {
     }
     /* USER CODE END Error_Handler_Debug */
 }
-
-
