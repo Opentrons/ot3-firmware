eae9b43dc55ade02e918809f16a2e4b2486e4e6e
amitlissack <amit@opentrons.com>
2021-07-12 11:03:48 -0400

    chore: common, gantry, and pipettes add their respective include dirs to for… (#52)
    * common, gantry, and pipettes add their respective include dirs to format target.
    
    * error.

--- include/pipettes/firmware/motor_control.hpp
+++ include/pipettes/firmware/motor_control.hpp
@@ -8,11 +8,11 @@ namespace motor_control {
  * TODO (2021-06-29): Issue SPI commands to motor.
  */
 struct MotorControl {
-    void set_speed(uint32_t s) {speed = s;}
-    [[nodiscard]] auto get_speed() const -> uint32_t  {return speed;}
+    void set_speed(uint32_t s) { speed = s; }
+    [[nodiscard]] auto get_speed() const -> uint32_t { return speed; }
 
-private:
+  private:
     uint32_t speed{0};
 };
 
-}
+}  // namespace motor_control
