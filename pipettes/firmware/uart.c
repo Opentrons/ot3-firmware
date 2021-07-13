c3a3e1ee3c0562965197e2c0c4366a660f6f4f66
amit lissack <amit@opentrons.com>
2021-07-13 14:04:16 -0400

    more files

--- pipettes/firmware/uart.c
+++ pipettes/firmware/uart.c
@@ -157,3 +157,69 @@ static void Error_Handler() {
         HAL_Delay(100);
     }
 }
+
+
+/**
+* @brief FDCAN MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hfdcan: FDCAN handle pointer
+* @retval None
+*/
+void HAL_FDCAN_MspInit(FDCAN_HandleTypeDef* hfdcan)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if(hfdcan->Instance==FDCAN1)
+    {
+        /* USER CODE BEGIN FDCAN1_MspInit 0 */
+
+        /* USER CODE END FDCAN1_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_FDCAN_CLK_ENABLE();
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**FDCAN1 GPIO Configuration
+        PA11     ------> FDCAN1_RX
+        PA12     ------> FDCAN1_TX
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Alternate = GPIO_AF9_FDCAN1;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN FDCAN1_MspInit 1 */
+
+        /* USER CODE END FDCAN1_MspInit 1 */
+    }
+
+}
+
+/**
+* @brief FDCAN MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hfdcan: FDCAN handle pointer
+* @retval None
+*/
+void HAL_FDCAN_MspDeInit(FDCAN_HandleTypeDef* hfdcan)
+{
+    if(hfdcan->Instance==FDCAN1)
+    {
+        /* USER CODE BEGIN FDCAN1_MspDeInit 0 */
+
+        /* USER CODE END FDCAN1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_FDCAN_CLK_DISABLE();
+
+        /**FDCAN1 GPIO Configuration
+        PA11     ------> FDCAN1_RX
+        PA12     ------> FDCAN1_TX
+        */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+
+        /* USER CODE BEGIN FDCAN1_MspDeInit 1 */
+
+        /* USER CODE END FDCAN1_MspDeInit 1 */
+    }
+
+}
