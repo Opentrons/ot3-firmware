00127100746cd129a1c2304a88240fd6e727a066
amit lissack <amit@opentrons.com>
2021-07-15 12:42:14 -0400

    can loop back.

--- pipettes/firmware/uart.c
+++ pipettes/firmware/uart.c
@@ -142,7 +142,7 @@ UART_HandleTypeDef MX_LPUART1_UART_Init() {
 void RCC_Peripheral_Clock_Select() {
     RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_FDCAN;
     PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
     PeriphClkInit.FdcanClockSelection = RCC_FDCANCLKSOURCE_PCLK1;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
@@ -156,70 +156,4 @@ static void Error_Handler() {
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
         HAL_Delay(100);
     }
-}
-
-
-/**
-* @brief FDCAN MSP Initialization
-* This function configures the hardware resources used in this example
-* @param hfdcan: FDCAN handle pointer
-* @retval None
-*/
-void HAL_FDCAN_MspInit(FDCAN_HandleTypeDef* hfdcan)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    if(hfdcan->Instance==FDCAN1)
-    {
-        /* USER CODE BEGIN FDCAN1_MspInit 0 */
-
-        /* USER CODE END FDCAN1_MspInit 0 */
-        /* Peripheral clock enable */
-        __HAL_RCC_FDCAN_CLK_ENABLE();
-
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        /**FDCAN1 GPIO Configuration
-        PA11     ------> FDCAN1_RX
-        PA12     ------> FDCAN1_TX
-        */
-        GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-        GPIO_InitStruct.Alternate = GPIO_AF9_FDCAN1;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-        /* USER CODE BEGIN FDCAN1_MspInit 1 */
-
-        /* USER CODE END FDCAN1_MspInit 1 */
-    }
-
-}
-
-/**
-* @brief FDCAN MSP De-Initialization
-* This function freeze the hardware resources used in this example
-* @param hfdcan: FDCAN handle pointer
-* @retval None
-*/
-void HAL_FDCAN_MspDeInit(FDCAN_HandleTypeDef* hfdcan)
-{
-    if(hfdcan->Instance==FDCAN1)
-    {
-        /* USER CODE BEGIN FDCAN1_MspDeInit 0 */
-
-        /* USER CODE END FDCAN1_MspDeInit 0 */
-        /* Peripheral clock disable */
-        __HAL_RCC_FDCAN_CLK_DISABLE();
-
-        /**FDCAN1 GPIO Configuration
-        PA11     ------> FDCAN1_RX
-        PA12     ------> FDCAN1_TX
-        */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
-
-        /* USER CODE BEGIN FDCAN1_MspDeInit 1 */
-
-        /* USER CODE END FDCAN1_MspDeInit 1 */
-    }
-
 }
