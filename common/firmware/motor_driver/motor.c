bed0e4cec4b7f801e1ea8b4ebb5563641ae5e3a5
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2021-08-12 11:00:26 -0400

    refactor(mc): add gpio config file in each individual subsystem

--- common/firmware/motor_driver/motor.c
+++ common/firmware/motor_driver/motor.c
@@ -2,17 +2,27 @@
  * A C file to act as a middle layer to HAL related functions for GPIO.
  */
 
+#include "motor_gpio_conf.h"
 #include "stm32g4xx_hal_conf.h"
 
+void Set_Direction() {
+    HAL_GPIO_WritePin(DIRECTION_GPIO_PORT, DIRECTION_PIN, GPIO_PIN_SET);
+}
 
-void Set_Direction() { HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET); }
+void Set_Step() { HAL_GPIO_WritePin(STEP_GPIO_PORT, STEP_PIN, GPIO_PIN_SET); }
 
-void Set_Step() { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET); }
+void Reset_Step() {
+    HAL_GPIO_WritePin(STEP_GPIO_PORT, STEP_PIN, GPIO_PIN_RESET);
+}
 
-void Reset_Step() { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET); }
+void Reset_Direction() {
+    HAL_GPIO_WritePin(DIRECTION_GPIO_PORT, DIRECTION_PIN, GPIO_PIN_RESET);
+}
 
-void Reset_Direction() { HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET); }
+void Set_Enable_Pin() {
+    HAL_GPIO_WritePin(ENABLE_GPIO_PORT, ENABLE_PIN, GPIO_PIN_SET);
+}
 
-void Set_Enable_Pin() { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET); }
-
-void Reset_Enable_Pin() { HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET); }
+void Reset_Enable_Pin() {
+    HAL_GPIO_WritePin(ENABLE_GPIO_PORT, ENABLE_PIN, GPIO_PIN_RESET);
+}
