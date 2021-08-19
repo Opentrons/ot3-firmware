df35d24996b0117fc488b45187cc7bc59b52a9f1
amit lissack <amit@opentrons.com>
2021-08-19 11:44:08 -0400

    comments

--- can/simulator/rtos_main.cpp
+++ can/simulator/rtos_main.cpp
@@ -21,14 +21,27 @@ static auto buffer = FreeRTOMessageBuffer<ReceiveBufferSize>{};
 static auto transport = socket_can::SocketCanTransport{};
 static auto canbus = sim_canbus::SimCANBus(transport, buffer);
 
+/**
+ * The parsed message handler. It will be passed into a DispatchParseTarget
+ * templetized with the same message types.
+ * @tparam Bus A CanBus type
+ */
 template <CanBus Bus>
 struct Loopback {
+    /**
+     * Constructor
+     * @param can_bus A CanBus instance.
+     */
     Loopback(Bus &can_bus) : writer{canbus} {}
     Loopback(const Loopback &) = delete;
     Loopback(const Loopback &&) = delete;
     auto operator=(const Loopback &) -> Loopback & = delete;
     auto operator=(const Loopback &&) -> Loopback && = delete;
 
+    /**
+     * The message handling function.
+     * @param m A variant of the various message types.
+     */
     void handle(
         std::variant<std::monostate, SetSpeedRequest, GetSpeedRequest> &m) {
         std::visit([this](auto o) { this->visit(o); }, m);
@@ -36,24 +49,37 @@ struct Loopback {
 
     void visit(std::monostate &m) {}
 
+    /**
+     * Handler for all message types.
+     * @param m Serializable.
+     */
     template <message_core::Serializable Serializable>
     void visit(const Serializable &m) {
+        // Loop it back
         writer.write(NodeId::host, m);
     }
 
     can_message_writer::MessageWriter<decltype(canbus)> writer;
 };
 
+// Create global handler
 static auto handler = Loopback{canbus};
 
+// Create a DispatchParseTarget to parse messages in message buffer and pass
+// them to the handler.
 static auto dispatcher = can_dispatch::DispatchParseTarget<
     Loopback<decltype(canbus)>, SetSpeedRequest, GetSpeedRequest>{handler};
 
+// A Message Buffer poller that reads from buffer and send to dispatcher
 static auto poller =
     freertos_can_dispatch::FreeRTOSCanBufferPoller(buffer, dispatcher);
+// The message buffer freertos task
 static auto dispatcher_task =
     FreeRTOSTask<256, 5, decltype(poller)>("dispatcher", poller);
 
+/**
+ * The socket can reader. Reads from socket and writes to message buffer.
+ */
 void can_bus_poll_task(void *) {
     transport.open("vcan0");
     while (canbus.read_message()) {
