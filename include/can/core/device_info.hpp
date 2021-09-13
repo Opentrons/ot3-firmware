fc68127f17a0357516eff2c26c3c99c15fd7391a
amit lissack <amit@opentrons.com>
2021-09-13 10:38:11 -0400

    add DeviceInfoHandler helper for processing DeviceInfoRequest message

---
+++ include/can/core/device_info.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "ids.hpp"
+#include "message_writer.hpp"
+#include "messages.hpp"
+#include "can_bus.hpp"
+
+
+using namespace can_ids;
+using namespace can_message_writer;
+using namespace can_messages;
+
+
+namespace can_device_info {
+
+/**
+ * A HandlesMessages implementing class that will respond to DeviceInfoRequest
+ * with a DeviceInfoResponse.
+ *
+ * @tparam Writer The can bus writer type.
+ */
+template <can_bus::CanBus Writer>
+class DeviceInfoHandler {
+  public:
+    /**
+     * Constructor
+     *
+     * @param writer A message writer for sending the response
+     * @param node_id The node id of this device
+     * @param version The firmware version on this device
+     */
+    DeviceInfoHandler(MessageWriter<Writer> & writer, NodeId node_id, uint32_t version): writer(writer), response(node_id, version) {}
+    DeviceInfoHandler(const DeviceInfoHandler &) = delete;
+    DeviceInfoHandler(const DeviceInfoHandler &&) = delete;
+    DeviceInfoHandler &operator=(const DeviceInfoHandler &) = delete;
+    DeviceInfoHandler &&operator=(const DeviceInfoHandler &&) = delete;
+
+    using MessageType = std::variant<std::monostate, DeviceInfoRequest>;
+
+    /**
+     * Message handler
+     * @param m The incoming message.
+     */
+    void handle(MessageType &m) {
+        std::visit([this](auto o) { this->visit(o); }, m);
+    }
+
+  private:
+    void visit(std::monostate &m) {}
+
+    void visit(DeviceInfoRequest &m) {
+        writer.write(NodeId::host, response);
+    }
+
+    DeviceInfoResponse response;
+    MessageWriter<Writer> & writer;
+};
+
+}
