b544b3da1576f8b59b2606dcd11a3c09b7e17114
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2021-09-17 15:11:17 -0400

    refactor(spi): remove HAL includes where possible
    To reduce warnings emitted from macros in the st bsp files, we should try to ensure that we are not
    importing hal files unnecessarily.
    
    RET-193

--- common/firmware/spi/spi_comms.cpp
+++ common/firmware/spi/spi_comms.cpp
@@ -22,19 +22,15 @@ using namespace spi;
  */
 
 /*
- * Private Functions
- */
-
-/*
  * Public Functions
  */
 
-Spi::Spi() : handle(MX_SPI2_Init()) {}
+Spi::Spi() { SPI2_init(); }
 
 void Spi::transmit_receive(const BufferType& transmit, BufferType& receive) {
     Reset_CS_Pin();
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
-    HAL_SPI_TransmitReceive(&handle, const_cast<uint8_t*>(transmit.data()),
-                            receive.data(), BUFFER_SIZE, TIMEOUT);
+    hal_transmit_receive(const_cast<uint8_t*>(transmit.data()), receive.data(),
+                         BUFFER_SIZE, TIMEOUT);
     Set_CS_Pin();
 }
