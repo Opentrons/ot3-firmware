7803669617700fa830024374540b511abd9b5437
amit lissack <amit@opentrons.com>
2021-09-17 13:57:09 -0400

    lint.

--- include/pipettes/core/eeprom.hpp
+++ include/pipettes/core/eeprom.hpp
@@ -15,7 +15,8 @@ template <class I2C>
 concept EEPromPolicy =
     requires(I2C i2c_comms, const uint8_t transmit,
              std::array<uint8_t, I2C::BUFFER_SIZE>& receive) {
-    {i2c_comms.BUFFER_SIZE};
+    /** A static integer member */
+    {I2C::BUFFER_SIZE} -> std::convertible_to<int>;
     {i2c_comms.transmit(transmit)};
     {i2c_comms.receive(receive)};
 };
@@ -27,7 +28,7 @@ void write(EEPromWriter& writer, const uint8_t serial_number) {
 
 template <eeprom::EEPromPolicy EEPromWriter>
 uint8_t read(EEPromWriter& writer) {
-    using BufferType = std::array<uint8_t, writer.BUFFER_SIZE>;
+    using BufferType = std::array<uint8_t, EEPromWriter::BUFFER_SIZE>;
     BufferType rxBuffer{0};
     uint8_t data = 0x0;
 
