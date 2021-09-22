94b114ca1820653610e3a0f6c53a12222a1e598d
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2021-09-22 18:33:43 -0400

    Start test structure

---
+++ include/pipettes/tests/mock_message_queue.hpp
@@ -0,0 +1,61 @@
+namespace mock_message_queue {
+
+struct MockMessageQueue {
+    explicit MockMessageQueue(uint8_t notification_bit, const char* name)
+    : FreeRTOSMessageQueue(notification_bit) {
+        vQueueAddToRegistry(queue, name);
+    }
+    explicit MockMessageQueue(uint8_t notification_bit)
+    : sent_bit(notification_bit) {}
+    MockMessageQueue& operator=(MockMessageQueue&) = delete;
+    MockMessageQueue&& operator=(MockMessageQueue&&) = delete;
+    MockMessageQueue(MockMessageQueue&) = delete;
+    MockMessageQueue(MockMessageQueue&&) = delete;
+
+    ~MockMessageQueue() {
+        vQueueUnregisterQueue(queue);
+        vQueueDelete(queue);
+    }
+
+    [[nodiscard]] auto try_write(const Message& message,
+                                 const uint32_t timeout_ticks = 0) -> bool {
+
+        return sent;
+    }
+
+    auto try_read(Message* message, uint32_t timeout_ticks = 0) -> bool {
+        message = queue_data_structure.back()
+        return true;
+    }
+
+    [[nodiscard]] auto try_write_isr(const Message& message) -> bool {
+        queue_data_structure.data()[]
+        return true;
+    }
+
+    auto try_read_isr(Message* message) const -> bool {
+        return try_read(message);
+    }
+
+    [[nodiscard]] auto has_message() const -> bool {
+        return queue_data_structure.empty() != 0;
+    }
+
+    [[nodiscard]] auto has_message_isr() const -> bool {
+        return queue_data_structure.empty() != 0;
+    }
+
+    [[nodiscard]] auto peek_isr(Message* message) const -> bool {
+        return try_read(message);
+    }
+
+    void reset() {
+        std::array<uint8_t, queue_size * sizeof(Message)> empty_queue{};
+        queue_data_structure = empty_queue;
+    }
+
+  private:
+    std::array<uint8_t, queue_size * sizeof(Message)> queue_data_structure{};
+    uint8_t sent_bit;
+};
+}  // namespace mock_message_queue
--- pipettes/tests/test_motor_interrupt.cpp
+++ pipettes/tests/test_motor_interrupt.cpp
@@ -1,36 +1,33 @@
 #include "catch2/catch.hpp"
-
 #include "motor-control/core/motor_interrupt_handler.hpp"
 
 SCENARIO("read and write pipette serial numbers") {
     GIVEN("write command") {
-    const uint8_t DUMMY_VALUE = 0x2;
+        const uint8_t DUMMY_VALUE = 0x2;
 
-    TestI2C testI2C{};
+        TestI2C testI2C{};
 
-    WHEN("i2c is initialized") {
-        THEN("the current value stored should be zero") {
-            const uint8_t ZERO_VALUE = 0x0;
-            REQUIRE(ZERO_VALUE == testI2C.stored);
+        WHEN("i2c is initialized") {
+            THEN("the current value stored should be zero") {
+                const uint8_t ZERO_VALUE = 0x0;
+                REQUIRE(ZERO_VALUE == testI2C.stored);
+            }
         }
-    }
 
-    WHEN("the command is received") {
-        THEN("the value should be stored") {
-            write(testI2C, DUMMY_VALUE);
-            REQUIRE(DUMMY_VALUE == testI2C.stored);
+        WHEN("the command is received") {
+            THEN("the value should be stored") {
+                write(testI2C, DUMMY_VALUE);
+                REQUIRE(DUMMY_VALUE == testI2C.stored);
+            }
         }
-    }
 
-    WHEN("the value is queried") {
-        THEN("the stored value should be returned") {
-            const uint8_t received_buff = read(testI2C);
-            REQUIRE(DUMMY_VALUE == received_buff);
+        WHEN("the value is queried") {
+            THEN("the stored value should be returned") {
+                const uint8_t received_buff = read(testI2C);
+                REQUIRE(DUMMY_VALUE == received_buff);
             }
         }
     }
 
-    GIVEN("A new device") {
-
-    }
+    GIVEN("A new device") {}
 }
