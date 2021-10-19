9179526f2dd3becb00c8f2bcf15b142afa652ae1
amit lissack <amit@opentrons.com>
2021-10-19 14:47:06 -0400

    implement move group and tests.

---
+++ common/tests/test_move_group.cpp
@@ -0,0 +1,73 @@
+#include <array>
+#include <variant>
+
+#include "can/core/messages.hpp"
+#include "catch2/catch.hpp"
+#include "motor-control/core/motion_group.hpp"
+
+bool operator==(const can_messages::AddLinearMoveRequest& a,
+                const can_messages::AddLinearMoveRequest& b) {
+    return a.duration == b.duration && a.seq_id == b.seq_id &&
+           a.acceleration == b.acceleration && a.group_id == b.group_id &&
+           a.velocity == b.velocity;
+}
+
+SCENARIO("Testing a move group") {
+    auto group = move_group::MoveGroup<5>{};
+
+    GIVEN("a move group") {
+        WHEN("is created") {
+            THEN("it is initially empty") { REQUIRE(group.empty()); }
+            THEN("the size is 0") { REQUIRE(group.size() == 0); }
+        }
+
+        WHEN("set_move is called") {
+            auto linear_move =
+                can_messages::AddLinearMoveRequest{.group_id = 0,
+                                                   .seq_id = 1,
+                                                   .duration = 2,
+                                                   .acceleration = 3,
+                                                   .velocity = 4};
+            auto result = group.set_move(linear_move);
+            auto read_group = group.get_move(linear_move.seq_id);
+
+            THEN("result is true.") { REQUIRE(result == true); }
+            THEN("the stored type is correct.") {
+                REQUIRE(
+                    std::holds_alternative<can_messages::AddLinearMoveRequest>(
+                        read_group));
+            }
+            THEN("the object is stored correctly.") {
+                REQUIRE(std::get<can_messages::AddLinearMoveRequest>(
+                            read_group) == linear_move);
+            }
+            THEN("it is not empty") { REQUIRE(!group.empty()); }
+            THEN("the size is 1") { REQUIRE(group.size() == 1); }
+        }
+
+        WHEN("set_move is called with too high a seq_id") {
+            auto linear_move =
+                can_messages::AddLinearMoveRequest{.group_id = 0,
+                                                   .seq_id = 123,
+                                                   .duration = 2,
+                                                   .acceleration = 3,
+                                                   .velocity = 4};
+            auto result = group.set_move(linear_move);
+            THEN("result is false.") { REQUIRE(result == false); }
+        }
+
+        WHEN("clear is called") {
+            auto linear_move =
+                can_messages::AddLinearMoveRequest{.group_id = 0,
+                                                   .seq_id = 1,
+                                                   .duration = 2,
+                                                   .acceleration = 3,
+                                                   .velocity = 4};
+            group.set_move(linear_move);
+            group.clear();
+
+            THEN("it is empty") { REQUIRE(group.empty()); }
+            THEN("the size is 0") { REQUIRE(group.size() == 0); }
+        }
+    }
+}
--- include/motor-control/core/motion_group.hpp
+++ include/motor-control/core/motion_group.hpp
@@ -1,2 +1,78 @@
+#include <array>
+#include <type_traits>
+#include <variant>
 
-class MotionGroupManager {};
+#include "can/core/messages.hpp"
+
+namespace move_group {
+
+template <std::size_t MaxElements>
+class MoveGroup {
+  public:
+    using MoveTypes =
+        std::variant<std::monostate, can_messages::AddLinearMoveRequest>;
+
+    MoveGroup() {}
+
+    /**
+     *
+     * @param move
+     */
+    auto set_move(can_messages::AddLinearMoveRequest& move) -> bool {
+        if (move.seq_id >= storage.size()) {
+            // Error
+            return false;
+        }
+        storage[move.seq_id] = move;
+        return true;
+    }
+
+    /**
+     *
+     * @return
+     */
+    auto size() const -> std::size_t {
+        int count = 0;
+        for (const MoveTypes& m : storage) {
+            if (!std::holds_alternative<std::monostate>(m)) {
+                ++count;
+            };
+        }
+        return count;
+    }
+
+    /**
+     *
+     * @return
+     */
+    auto empty() const -> bool {
+        for (const MoveTypes& m : storage) {
+            if (!std::holds_alternative<std::monostate>(m)) return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     */
+    void clear() {
+        for (MoveTypes& m : storage) {
+            m = std::monostate{};
+        }
+    }
+
+    /**
+     *
+     * @param seq_id
+     * @return
+     */
+    const MoveTypes& get_move(uint8_t seq_id) const { return storage[seq_id]; }
+
+  private:
+    std::array<MoveTypes, MaxElements> storage{};
+};
+
+template <std::size_t MaxElements>
+using MoveGroupManager = std::array<MoveGroup, MaxElements>;
+
+}  // namespace move_group
