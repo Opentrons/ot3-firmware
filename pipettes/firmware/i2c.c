7d667f13f6cf26334a863f023de966931144a157
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2021-11-08 13:07:04 -0500

    refactor(pipettes): change gpio ports to match L5 specs
    RET-232

--- pipettes/firmware/i2c.c
+++ pipettes/firmware/i2c.c
@@ -11,7 +11,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c) {
         __HAL_RCC_I2C1_CLK_ENABLE();
         __HAL_RCC_GPIOB_CLK_ENABLE();
         GPIO_InitTypeDef GPIO_InitStruct = {0};
-        GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_8;
+        GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
         GPIO_InitStruct.Pull = GPIO_PULLUP;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
