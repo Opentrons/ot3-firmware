4e36cb93bd4d2304daac61460e1fe461f052326e
sotaoverride <sotaoverride@gmail.com>
2021-11-09 14:38:28 -0500

    lint

--- common/firmware/spi/spi_comms.cpp
+++ common/firmware/spi/spi_comms.cpp
@@ -25,7 +25,8 @@ using namespace spi;
  * Public Functions
  */
 
-Spi::Spi() { SPI2_init();  SPI3_init(); }
+Spi::Spi() {                SPI2_init();  
+SPI3_init(); }
 
 void Spi::transmit_receive(const BufferType& transmit, BufferType& receive) {
     Reset_CS_Pin();
