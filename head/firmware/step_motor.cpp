5060b68fbf91360b28a1470bb181ad4e4f8f76e3
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2021-11-09 15:49:34 -0500

    update head & pipettes

--- head/firmware/step_motor.cpp
+++ head/firmware/step_motor.cpp
@@ -28,3 +28,12 @@ void start_motor_handler(
 }
 
 void reset_motor_handler() { handler_class.reset(); }
+
+MoveStatus get_move_status() {
+    auto buffered_move = handler_class.get_buffered_move();
+    MoveStatus msg = {.group_id = buffered_move.group_id,
+                      .seq_id = buffered_move.seq_id,
+                      .remaining_duration = handler_class.get_remaining_ticks(),
+                      .current_position = handler_class.get_current_position()};
+    return msg;
+}
