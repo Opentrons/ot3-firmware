f15566e5cf58bf3291b016e5cd13397a88cfd8bd
sotaoverride <sotaoverride@gmail.com>
2021-11-10 07:45:48 -0500

    fix pins, spi trans/recv handlers

--- common/firmware/head-ff/spi/spi.c
+++ common/firmware/head-ff/spi/spi.c
@@ -3,7 +3,7 @@
 
 #include "platform_specific_hal_conf.h"
 
-SPI_HandleTypeDef handle;
+SPI_HandleTypeDef handle, handle_SPI2, handle_SPI3;
 
 /**
  * @brief SPI MSP Initialization
@@ -28,8 +28,8 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
         PB14     ------> SPI2_MISO
         PB15     ------> SPI2_MOSI
          Step/Dir
-         PB1  ---> Dir Pin Motor 1
-         PA8  ---> Step Pin Motor 1
+         PC7  ---> Dir Pin Motor A
+         PC6  ---> Step Pin Motor A
         */
         GPIO_InitStruct.Pin = GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -42,17 +42,18 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
         GPIO_InitStruct.Pin = GPIO_PIN_12;
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        // Dir/Step pin
-        GPIO_InitStruct.Pin = GPIO_PIN_1;
+           
+        // PB11 A motor control  
+        // ctrl /Dir/Step pin
+        GPIO_InitStruct.Pin = GPIO_PIN_11;
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_10;
+        GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-        HAL_GPIO_Init(GPIOA,  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
+        HAL_GPIO_Init(GPIOC,  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
                       &GPIO_InitStruct);
     }
     // PC11 -> MISO
@@ -66,13 +67,13 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
         __HAL_RCC_GPIOA_CLK_ENABLE();
         __HAL_RCC_GPIOC_CLK_ENABLE();
         /**SPI3 GPIO Configuration
-        PA04     ------> SPI3_NSS
+        PA4     ------> SPI3_NSS
         PC10     ------> SPI3_SCK
         PC11     ------> SPI3_MISO
         PC12     ------> SPI3_MOSI
          Step/Dir
-         PB1  ---> Dir Pin Motor on SPI3 (Z-axis)
-         PA8  ---> Step Pin Motor on SPI3 (Z-axis)
+         PC1  ---> Dir Pin Motor on SPI3 (Z-axis)
+         PC0  ---> Step Pin Motor on SPI3 (Z-axis)
         */
         GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -93,7 +94,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-        GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+        GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         HAL_GPIO_Init(GPIOC,  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
                       &GPIO_InitStruct);
@@ -118,10 +119,14 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi) {
         PB13     ------> SPI2_SCK
         PB14     ------> SPI2_MISO
         PB15     ------> SPI2_MOSI
+        
+        Step/Dir
+         PC7  ---> Dir Pin Motor on SPI2 (A-axis)
+         PC6  ---> Step Pin Motor on SPI2 (A-axis)
         */
         HAL_GPIO_DeInit(GPIOB,
                         GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8 | GPIO_PIN_9);
+        HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
     }
     else if (hspi->Instance == SPI3) {
         /* Peripheral clock disable */
@@ -132,9 +137,13 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi) {
         PC10     ------> SPI3_SCK
         PC11     ------> SPI3_MISO
         PC12     ------> SPI3_MOSI
+
+        Step/Dir
+         PC1  ---> Dir Pin Motor on SPI3 (Z-axis)
+         PC0  ---> Step Pin Motor on SPI3 (Z-axis)
         */
         HAL_GPIO_DeInit(GPIOC,
-                        GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_4 | GPIO_PIN_6 | GPIO_PIN_7);
+                        GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_0 | GPIO_PIN_1);
         HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
     }
 }
@@ -205,22 +214,30 @@ SPI_HandleTypeDef MX_SPI3_Init() {
 }
 
 void SPI2_init() {
-    handle = MX_SPI2_Init();
+    handle_SPI2 = MX_SPI2_Init();
 }
 
 void SPI3_init() {
-    handle = MX_SPI3_Init();
+    handle_SPI3 = MX_SPI3_Init();
 }
 
 void Set_CS_Pin() { 
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
 
 }
 
 void Reset_CS_Pin() {
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 }
 
 void hal_transmit_receive(uint8_t* transmit, uint8_t* receive, uint16_t buff_size, uint32_t timeout) {
     HAL_SPI_TransmitReceive(&handle, transmit, receive, buff_size, timeout);
+}
+void hal_transmit_receive_SPI2(uint8_t* transmit, uint8_t* receive, uint16_t buff_size, uint32_t timeout) {
+    HAL_SPI_TransmitReceive(&handle_SPI2, transmit, receive, buff_size, timeout);
+}
+void hal_transmit_receive_SPOI3(uint8_t* transmit, uint8_t* receive, uint16_t buff_size, uint32_t timeout) {
+    HAL_SPI_TransmitReceive(&handle_SPI3, transmit, receive, buff_size, timeout);
 }
--- include/common/firmware/spi.h
+++ include/common/firmware/spi.h
@@ -9,7 +9,10 @@ void SPI2_init();
 void SPI3_init();
 void Set_CS_Pin();
 void Reset_CS_Pin();
+//todo refactor. have single trans/recv func for all SPIs
 void hal_transmit_receive(uint8_t* transmit, uint8_t *receive, uint16_t buff_size, uint32_t timeout);
+void hal_transmit_receive_SPI2(uint8_t* transmit, uint8_t *receive, uint16_t buff_size, uint32_t timeout);
+void hal_transmit_receive_SPI3(uint8_t* transmit, uint8_t *receive, uint16_t buff_size, uint32_t timeout);
 
 #ifdef __cplusplus
 }  // extern "C"
