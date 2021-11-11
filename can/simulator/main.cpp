700880f85a2fa5c244c0fc05478897e937c3a4ff
amitlissack <amit@opentrons.com>
2021-11-11 15:21:53 -0500

    Update can/simulator/main.cpp
    Co-authored-by: Seth Foster <seth@opentrons.com>

--- can/simulator/main.cpp
+++ can/simulator/main.cpp
@@ -5,7 +5,7 @@
 #include "can/core/freertos_can_dispatch.hpp"
 #include "can/core/message_writer.hpp"
 #include "can/core/messages.hpp"
-#include "can/simlib//sim_canbus.hpp"
+#include "can/simlib/sim_canbus.hpp"
 #include "can/simlib/socket_can.hpp"
 #include "common/core/freertos_message_buffer.hpp"
 #include "common/core/freertos_synchronization.hpp"
