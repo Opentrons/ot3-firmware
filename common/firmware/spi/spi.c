2029af5f2b2a42c6e04730977e07c13993259e43
sotaoverride <sotaoverride@gmail.com>
2021-11-11 15:37:03 -0500

    refactor spi common

--- common/firmware/spi/spi.c
+++ common/firmware/spi/spi.c
@@ -3,6 +3,136 @@
 
 #include "platform_specific_hal_conf.h"
 
+////
+struct pin_group {
+    GPIO_TypeDef  * port;
+    size_t no_of_pins;
+    uint32_t* pins;
+    GPIO_InitTypeDef GPIO_InitStruct;
+    uint32_t Mode; /*!< Specifies the operating mode for the selected pins.
+                      This parameter can be a value of @ref GPIO_mode */
+
+    uint32_t Pull; /*!< Specifies the Pull-up or Pull-Down activation for the
+                      selected pins. This parameter can be a value of @ref
+                      GPIO_pull */
+
+    uint32_t Speed; /*!< Specifies the speed for the selected pins.
+                        This parameter can be a value of @ref GPIO_speed */
+
+    uint32_t Alternate; /*!< Peripheral to be connected to the selected pins
+                             This parameter can be a value of @ref
+                           GPIOEx_Alternate_function_selection */
+};
+
+struct SPI_config {
+    SPI_interface SPI_int;
+    component comp;
+    sub_component sub_comp;
+    struct pin_group* pg;
+    size_t no_of_groups;
+};
+
+struct SPI_config_group {
+    struct SPI_config* sc;
+    size_t no_of_groups;
+};
+
+/**SPI3 GPIO Configuration
+        PA4     ------> SPI3_NSS
+        PC10     ------> SPI3_SCK
+        PC11     ------> SPI3_MISO
+        PC12     ------> SPI3_MOSI
+         Step/Dir
+         PC1  ---> Dir Pin Motor on SPI3 (Z-axis)
+         PC0  ---> Step Pin Motor on SPI3 (Z-axis)
+        ctrl pin
+        PC4
+----z stage pins for ff board---
+pins with similiar configurations, pull up etc,
+are grouped together. Some groups did
+end up with just one pin, oh well.
+*/
+struct SPI_config* commonConfig(void) {
+    uint32_t common_pins_sck_miso_mosi[3] = {GPIO_PIN_13, GPIO_PIN_14,
+                                              GPIO_PIN_15};
+    uint32_t common_pins_cs[1] = {GPIO_PIN_12};
+    uint32_t z_stage_pins_ctrl[1] = {GPIO_PIN_10};
+    uint32_t z_stage_pins_dir[1] = {GPIO_PIN_1};
+    uint32_t z_stage_pins_step[1] = {GPIO_PIN_8};
+
+
+    struct pin_group common_pins_sck_miso_mosi_group = {
+
+        .no_of_pins = sizeof(common_pins_sck_miso_mosi) /
+                      sizeof(common_pins_sck_miso_mosi[0]),
+        .port = GPIOB,
+        .pins = common_pins_sck_miso_mosi,
+        .Mode = GPIO_MODE_AF_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_LOW,
+        .Alternate = GPIO_AF5_SPI2
+        };
+
+    struct pin_group common_pins_cs_group = {
+
+        .no_of_pins = sizeof(common_pins_cs) / sizeof(common_pins_cs[0]),
+        .port = GPIOB,
+        .pins = common_pins_cs,
+        .Mode = GPIO_MODE_OUTPUT_PP};
+
+    struct pin_group common_pins_ctrl_group = {
+
+        .no_of_pins = sizeof(common_pins_ctrl) / sizeof(common_pins_ctrl[0]),
+        .port = GPIOA,
+        .pins = common_pins_ctrl,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_LOW};
+
+    struct pin_group common_pins_dir_group = {
+
+        .no_of_pins =
+            sizeof(common_pins_dir / sizeof(common_pins_dir[0]),
+        .port = GPIOB,
+        .pins = common_pins_dir,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_LOW};
+
+    struct pin_group common_pins_step_group = {
+
+        .no_of_pins =
+            sizeof(common_pins_dir / sizeof(common_pins_step[0]),
+        .port = GPIOA,
+        .pins = common_pins_dir,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_LOW};
+
+    struct pin_group _pg[4] = {common_pins_sck_miso_mosi_group,
+                               common_pins_cs_group, common_pins_ctrl_group,
+                               common_pins_dir_group, 
+                               common_pins_step_group};
+    struct SPI_config common_config = {
+        .comp = common,
+        .sub_comp = na,
+        .SPI_int = _SPI2,
+        .no_of_groups = 5,
+        // todo check mcu heap situation and maybe get back to dynamic
+        .pg = _pg};
+    return &common_config;
+}
+
+void* SPIConfigInit(size_t no_of_groups) {
+    struct SPI_config_group scg;
+    scg.no_of_groups = no_of_groups;
+    struct SPI_config arr[no_of_groups];
+    arr[0] = *(commonConfig());
+    scg.sc = arr;
+    return &scg;
+}
+////
+
 SPI_HandleTypeDef handle;
 
 /**
@@ -111,6 +241,25 @@ SPI_HandleTypeDef MX_SPI2_Init() {
         Error_Handler();
     }
     return hspi2;
+}
+void SPI_init() {
+    struct SPI_config_group * scg_ptr= (struct SPI_config_group *)SPIConfigInit(1);
+    for (int i = 0; i < scg_ptr->no_of_groups; i++) {
+        switch(scg_ptr->sc[i].SPI_int) {
+            case _SPI0:
+                break;
+            case _SPI1:
+                break;
+            case _SPI2:
+                handle_SPI2 = MX_SPI2_Init();
+                break;
+            case _SPI3:
+                handle_SPI3 = MX_SPI3_Init(); 
+                break;
+            default:
+                printf("Invalid SPI interface\n");
+        }
+    }
 }
 void SPI_init() {
     SPI2_init();
--- include/common/firmware/spi_comms.hpp
+++ include/common/firmware/spi_comms.hpp
@@ -9,8 +9,11 @@ class Spi {
     using BufferType = std::array<uint8_t, BUFFER_SIZE>;
     Spi();
     void transmit_receive(const BufferType& transmit, BufferType& receive);
+    void* get_SPI_config();
+    void set_SPI_config();
 
   private:
     static constexpr uint32_t TIMEOUT = 0xFFFF;
+    void* SPI_config;
 };
 }  // namespace spi
