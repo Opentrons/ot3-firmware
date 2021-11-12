1ab435cfd57bf2edffa45faa231472ab069fecdc
sotaoverride <sotaoverride@gmail.com>
2021-11-11 19:12:12 -0500

    forgot to make vars static

--- common/firmware/spi/spi.c
+++ common/firmware/spi/spi.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common/firmware/spi.h"
 #include "common/firmware/errors.h"
 
@@ -53,15 +55,15 @@ are grouped together. Some groups did
 end up with just one pin, oh well.
 */
 struct SPI_config* commonConfig(void) {
-    uint32_t common_pins_sck_miso_mosi[3] = {GPIO_PIN_13, GPIO_PIN_14,
+    static uint32_t common_pins_sck_miso_mosi[3] = {GPIO_PIN_13, GPIO_PIN_14,
                                               GPIO_PIN_15};
-    uint32_t common_pins_cs[1] = {GPIO_PIN_12};
-    uint32_t common_pins_ctrl[1] = {GPIO_PIN_10};
-    uint32_t common_pins_dir[1] = {GPIO_PIN_1};
-    uint32_t common_pins_step[1] = {GPIO_PIN_8};
+    static uint32_t common_pins_cs[1] = {GPIO_PIN_12};
+    static uint32_t common_pins_ctrl[1] = {GPIO_PIN_10};
+    static uint32_t common_pins_dir[1] = {GPIO_PIN_1};
+    static uint32_t common_pins_step[1] = {GPIO_PIN_8};
 
 
-    struct pin_group common_pins_sck_miso_mosi_group = {
+    static struct pin_group common_pins_sck_miso_mosi_group = {
 
         .no_of_pins = sizeof(common_pins_sck_miso_mosi) /
                       sizeof(common_pins_sck_miso_mosi[0]),
@@ -73,14 +75,14 @@ struct SPI_config* commonConfig(void) {
         .Alternate = GPIO_AF5_SPI2
         };
 
-    struct pin_group common_pins_cs_group = {
+    static struct pin_group common_pins_cs_group = {
 
         .no_of_pins = sizeof(common_pins_cs) / sizeof(common_pins_cs[0]),
         .port = GPIOB,
         .pins = common_pins_cs,
         .Mode = GPIO_MODE_OUTPUT_PP};
 
-    struct pin_group common_pins_ctrl_group = {
+    static struct pin_group common_pins_ctrl_group = {
 
         .no_of_pins = sizeof(common_pins_ctrl) / sizeof(common_pins_ctrl[0]),
         .port = GPIOA,
@@ -89,7 +91,7 @@ struct SPI_config* commonConfig(void) {
         .Pull = GPIO_NOPULL,
         .Speed = GPIO_SPEED_FREQ_LOW};
 
-    struct pin_group common_pins_dir_group = {
+    static struct pin_group common_pins_dir_group = {
 
         .no_of_pins =
             sizeof(common_pins_dir) / sizeof(common_pins_dir[0]),
@@ -99,7 +101,7 @@ struct SPI_config* commonConfig(void) {
         .Pull = GPIO_NOPULL,
         .Speed = GPIO_SPEED_FREQ_LOW};
 
-    struct pin_group common_pins_step_group = {
+    static struct pin_group common_pins_step_group = {
 
         .no_of_pins =
             sizeof(common_pins_dir) / sizeof(common_pins_step[0]),
@@ -109,7 +111,7 @@ struct SPI_config* commonConfig(void) {
         .Pull = GPIO_NOPULL,
         .Speed = GPIO_SPEED_FREQ_LOW};
 
-    struct pin_group _pg[4] = {common_pins_sck_miso_mosi_group,
+    struct pin_group _pg[5] = {common_pins_sck_miso_mosi_group,
                                common_pins_cs_group, common_pins_ctrl_group,
                                common_pins_dir_group, 
                                common_pins_step_group};
@@ -120,16 +122,55 @@ struct SPI_config* commonConfig(void) {
         .no_of_groups = 5,
         // todo check mcu heap situation and maybe get back to dynamic
         .pg = _pg};
-    return &common_config;
+    static struct SPI_config common_config_;
+    common_config_=common_config;
+    return &common_config_;
 }
 
 void* SPIConfigInit(size_t no_of_groups) {
-    struct SPI_config_group scg;
+    static struct SPI_config_group scg;
     scg.no_of_groups = no_of_groups;
     struct SPI_config arr[no_of_groups];
     arr[0] = *(commonConfig());
     scg.sc = arr;
     return &scg;
+}
+
+void SPI2_configs(void) {
+    struct SPI_config* SPI_config_tmp = commonConfig();
+        /* Peripheral clock enable */
+        __HAL_RCC_SPI2_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**SPI3 GPIO Configuration
+        PA4     ------> SPI3_NSS
+        PC10     ------> SPI3_SCK
+        PC11     ------> SPI3_MISO
+        PC12     ------> SPI3_MOSI
+         Step/Dir
+         PC1  ---> Dir Pin Motor on SPI3 (Z-axis)
+         PC0  ---> Step Pin Motor on SPI3 (Z-axis)
+        */
+
+        for (int i = 0; i < SPI_config_tmp->no_of_groups; i++) {
+            int total_pins_in_group = SPI_config_tmp->pg[i].no_of_pins;
+            uint32_t pin_tmp = SPI_config_tmp->pg[i].pins[0];
+            for (int j = 0; j < total_pins_in_group; j++) {
+                pin_tmp = pin_tmp | SPI_config_tmp->pg[i].pins[j];
+            }
+            // GPIO_InitStruct
+            SPI_config_tmp->pg[i].GPIO_InitStruct.Pin = pin_tmp;
+            SPI_config_tmp->pg[i].GPIO_InitStruct.Mode =
+                SPI_config_tmp->pg[i].Mode;
+            SPI_config_tmp->pg[i].GPIO_InitStruct.Pull =
+                SPI_config_tmp->pg[i].Pull;
+            SPI_config_tmp->pg[i].GPIO_InitStruct.Speed =
+                SPI_config_tmp->pg[i].Speed;
+            SPI_config_tmp->pg[i].GPIO_InitStruct.Alternate =
+                SPI_config_tmp->pg[i].Alternate;
+            HAL_GPIO_Init(SPI_config_tmp->pg[i].port, &SPI_config_tmp->pg[i].GPIO_InitStruct);
+        }
+
 }
 ////
 
@@ -146,45 +187,9 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
     // PB12 -> CS
     // PB13 -> CLK
     // PB15 -> MOSI
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
+   
     if (hspi->Instance == SPI2) {
-        /* Peripheral clock enable */
-        __HAL_RCC_SPI2_CLK_ENABLE();
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        __HAL_RCC_GPIOB_CLK_ENABLE();
-        /**SPI2 GPIO Configuration
-        PB12     ------> SPI2_NSS
-        PB13     ------> SPI2_SCK
-        PB14     ------> SPI2_MISO
-        PB15     ------> SPI2_MOSI
-
-         Step/Dir
-         PB1  ---> Dir Pin Motor 1
-         PA8  ---> Step Pin Motor 1
-        */
-        GPIO_InitStruct.Pin = GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-        GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        // Chip select
-        GPIO_InitStruct.Pin = GPIO_PIN_12;
-        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        // Dir/Step pin
-        GPIO_InitStruct.Pin = GPIO_PIN_1;
-        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_10;
-        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-        HAL_GPIO_Init(GPIOA,  // NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
-                      &GPIO_InitStruct);
+        SPI2_configs();
     }
 }
 
