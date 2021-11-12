374d335d4acec9ab07e7c7f95beacd5dcd38221c
sotaoverride <sotaoverride@gmail.com>
2021-11-12 11:24:43 -0500

    spi_comms.pp fixes for spi handle

--- common/firmware/spi/spi_comms.cpp
+++ common/firmware/spi/spi_comms.cpp
@@ -1,7 +1,7 @@
 #include "common/firmware/spi_comms.hpp"
 
 #include "common/firmware/spi.h"
-
+#include "platform_specific_hal_conf.h"
 using namespace spi;
 
 /*
@@ -32,13 +32,99 @@ Spi::Spi(struct module mod) {
     Spi::set_SPI_config(mod.comp, mod.sub_comp, mod.intf, mod.ptr);
     SPI_init();
 }
-void Spi::set_SPI_config(component comp, sub_component sub_comp, SPI_interface intf,void* SPI_HandleTypeDef_instance){
-    if (comp == head && sub_comp == motor_z){
-        
-        //this->SPI_config->SPI_HandleTypeDef_instance=&MX_SPI3_Init();
-        this->SPI_config->comp=comp;
-        this->SPI_config->sub_comp=sub_comp;
-        this->SPI_config->intf=intf;
+
+SPI_HandleTypeDef MX_SPI_Init(spi::SPI_interface intf, SPI_HandleTypeDef* tmp) {
+    static SPI_HandleTypeDef hspi;
+    switch (intf) {
+        case spi::_SPI1:
+            /* SPI parameter configuration*/
+            __HAL_RCC_SPI1_CLK_ENABLE();
+            hspi =
+            {.Instance = SPI1,
+             .Init = {.Mode = SPI_MODE_MASTER,
+                      .Direction = SPI_DIRECTION_2LINES,
+                      .DataSize = SPI_DATASIZE_8BIT,
+                      .CLKPolarity = SPI_POLARITY_HIGH,
+                      .CLKPhase = SPI_PHASE_2EDGE,
+                      .NSS = SPI_NSS_SOFT,
+                      .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32,
+                      .FirstBit = SPI_FIRSTBIT_MSB,
+                      .TIMode = SPI_TIMODE_DISABLE,
+                      .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
+                      .CRCPolynomial = 7,
+                      .CRCLength = SPI_CRC_LENGTH_DATASIZE,
+                      .NSSPMode = SPI_NSS_PULSE_DISABLE}
+
+            };
+
+            if (HAL_SPI_Init(&hspi) != HAL_OK) {
+               // Error_Handler();
+            }
+            break;
+        case spi::_SPI2:
+            /* SPI parameter configuration*/
+            __HAL_RCC_SPI2_CLK_ENABLE();
+            hspi =
+            {.Instance = SPI2,
+             .Init = {.Mode = SPI_MODE_MASTER,
+                      .Direction = SPI_DIRECTION_2LINES,
+                      .DataSize = SPI_DATASIZE_8BIT,
+                      .CLKPolarity = SPI_POLARITY_HIGH,
+                      .CLKPhase = SPI_PHASE_2EDGE,
+                      .NSS = SPI_NSS_SOFT,
+                      .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32,
+                      .FirstBit = SPI_FIRSTBIT_MSB,
+                      .TIMode = SPI_TIMODE_DISABLE,
+                      .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
+                      .CRCPolynomial = 7,
+                      .CRCLength = SPI_CRC_LENGTH_DATASIZE,
+                      .NSSPMode = SPI_NSS_PULSE_DISABLE}
+
+            };
+
+            if (HAL_SPI_Init(&hspi) != HAL_OK) {
+               // Error_Handler();
+            }
+            break;
+        case spi::_SPI3:
+            /* SPI parameter configuration*/
+            __HAL_RCC_SPI3_CLK_ENABLE();
+            hspi =
+            {.Instance = SPI3,
+             .Init = {.Mode = SPI_MODE_MASTER,
+                      .Direction = SPI_DIRECTION_2LINES,
+                      .DataSize = SPI_DATASIZE_8BIT,
+                      .CLKPolarity = SPI_POLARITY_HIGH,
+                      .CLKPhase = SPI_PHASE_2EDGE,
+                      .NSS = SPI_NSS_SOFT,
+                      .BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32,
+                      .FirstBit = SPI_FIRSTBIT_MSB,
+                      .TIMode = SPI_TIMODE_DISABLE,
+                      .CRCCalculation = SPI_CRCCALCULATION_DISABLE,
+                      .CRCPolynomial = 7,
+                      .CRCLength = SPI_CRC_LENGTH_DATASIZE,
+                      .NSSPMode = SPI_NSS_PULSE_DISABLE}
+
+            };
+
+            if (HAL_SPI_Init(&hspi) != HAL_OK) {
+               // Error_Handler();
+            }
+            break;
+    }
+    //tmp=&hspi;
+    return hspi;
+}
+void Spi::set_SPI_config(component comp, sub_component sub_comp,
+                         SPI_interface intf, void* SPI_HandleTypeDef_instance) {
+    if (comp == head && sub_comp == motor_z) {
+        // null check??
+        SPI_HandleTypeDef* tmp;
+        SPI_HandleTypeDef tmp2 = MX_SPI_Init(intf, tmp);
+        this->SPI_config->ptr = &tmp2;
+        this->SPI_config->comp = comp;
+        this->SPI_config->sub_comp = sub_comp;
+        this->SPI_config->intf = intf;
     }
 }
 void Spi::transmit_receive(const BufferType& transmit, BufferType& receive) {
