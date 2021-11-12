2b018b9bc742ee474ede96bc9e731360ef0d5a41
sotaoverride <sotaoverride@gmail.com>
2021-11-12 13:09:07 -0500

    clang-format issues on ci

--- include/common/firmware/spi_comms.hpp
+++ include/common/firmware/spi_comms.hpp
@@ -1,5 +1,3 @@
-// clang-format off
-
 #pragma once
 #include <cstdint>
 #include <span>
@@ -10,26 +8,26 @@ typedef enum { gantry_x, gantry_y, motor_a, motor_z, na } sub_component;
 typedef enum { _SPI0, _SPI1, _SPI2, _SPI3 } SPI_interface;
 
 struct module {
-  component comp;
-  sub_component sub_comp;
-  SPI_interface intf;
-  // SPI_HandleTypeDef
-  void *ptr;
+    component comp;
+    sub_component sub_comp;
+    SPI_interface intf;
+    // SPI_HandleTypeDef
+    void *ptr;
 };
 
 class Spi {
- public:
-  static constexpr auto BUFFER_SIZE = 5;
-  using BufferType = std::array<uint8_t, BUFFER_SIZE>;
-  Spi();
-  Spi(struct module);
-  void transmit_receive(const BufferType &transmit, BufferType &receive);
-  void *get_SPI_config();
-  void set_SPI_config(component, sub_component, SPI_interface, void *);
+  public:
+    static constexpr auto BUFFER_SIZE = 5;
+    using BufferType = std::array<uint8_t, BUFFER_SIZE>;
+    Spi();
+    Spi(struct module);
+    void transmit_receive(const BufferType &transmit, BufferType &receive);
+    void *get_SPI_config();
+    void set_SPI_config(component, sub_component, SPI_interface, void *);
 
- private:
-  static constexpr uint32_t TIMEOUT = 0xFFFF;
-  struct module *SPI_config;
+  private:
+    static constexpr uint32_t TIMEOUT = 0xFFFF;
+    struct module *SPI_config;
 };
 }  // namespace spi
-// clang-format on
+
