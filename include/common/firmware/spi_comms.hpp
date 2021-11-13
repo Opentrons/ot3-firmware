feefac10c211a972aa129b9c8a8b01506c861bca
sotaoverride <sotaoverride@gmail.com>
2021-11-12 19:20:37 -0500

    clang-format

--- include/common/firmware/spi_comms.hpp
+++ include/common/firmware/spi_comms.hpp
@@ -3,17 +3,17 @@
 #include <span>
 
 namespace spi {
-struct SPI_interface{
-  /*
-    Incase there are multiple handles
-    involed(like how the head was different SPI
-    interfaces for it's A and Z motor) SPI_handle
-    can point to an array of handles. SPI_handle
-    to be instantiated in respective project's
-    spi.c under void* get_SPI_handle(void*)
+struct SPI_interface {
+    /*
+      Incase there are multiple handles
+      involed(like how the head was different SPI
+      interfaces for it's A and Z motor) SPI_handle
+      can point to an array of handles. SPI_handle
+      to be instantiated in respective project's
+      spi.c under void* get_SPI_handle(void*)
 
-  */
-  void* SPI_handle;
+    */
+    void* SPI_handle;
 };
 class Spi {
   public:
@@ -21,6 +21,7 @@ class Spi {
     using BufferType = std::array<uint8_t, BUFFER_SIZE>;
     Spi();
     void transmit_receive(const BufferType& transmit, BufferType& receive);
+
   private:
     static constexpr uint32_t TIMEOUT = 0xFFFF;
     struct SPI_interface SPI_intf;
