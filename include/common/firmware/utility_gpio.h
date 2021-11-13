be346885b402b18c3669b26d1fe219a245cf94c8
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2021-11-13 18:15:51 -0500

    feat(pipettes): add limit switch and LED gpios
    Bring up the endstop for the pipettes subproject.

--- include/common/firmware/utility_gpio.h
+++ include/common/firmware/utility_gpio.h
@@ -6,12 +6,12 @@
 extern "C" {
 #endif  // __cplusplus
 
-void limit_switch_gpio_init(void);
-void LED_drive_gpio_init(void);
-void turn_on_LED_pin(void);
-void turn_off_LED_pin(void);
-
-void utility_gpio_init(void);
+void limit_switch_gpio_init();
+void LED_drive_gpio_init();
+void turn_on_LED_pin();
+void turn_off_LED_pin();
+void check_limit_switch();
+void utility_gpio_init();
 
 #ifdef __cplusplus
 }  // extern "C"
