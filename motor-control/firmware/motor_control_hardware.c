43718157b1bc846f0db303a23d45c98c0c5db407
Seth Foster <seth@opentrons.com>
2021-11-30 15:20:48 -0500

    make gpio invert a function not a ternary

--- motor-control/firmware/motor_control_hardware.c
+++ motor-control/firmware/motor_control_hardware.c
@@ -13,10 +13,20 @@ void motor_hardware_set_pin(void* port, uint16_t pin, uint8_t active_setting) {
     HAL_GPIO_WritePin(port, pin, active_setting);
 }
 
+static uint8_t invert_gpio_value(uint8_t setting) {
+    switch(setting) {
+        case GPIO_PIN_SET:
+            return GPIO_PIN_RESET;
+        case GPIO_PIN_RESET:
+            return GPIO_PIN_SET;
+        default:
+            return GPIO_PIN_SET;
+    }
+}
+
 void motor_hardware_reset_pin(void* port, uint16_t pin, uint8_t active_setting) {
     HAL_GPIO_WritePin(
-        port, pin,
-        (active_setting == GPIO_PIN_SET) ? GPIO_PIN_RESET : GPIO_PIN_RESET);
+        port, pin, invert_gpio_value(active_setting));
 }
 
 void motor_hardware_start_timer(void* htim) { HAL_TIM_Base_Start_IT(htim); }
