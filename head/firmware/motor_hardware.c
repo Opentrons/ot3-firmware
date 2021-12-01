d5edbf994c60b4a714fa9982ad3e4213b41452f1
amit lissack <amit@opentrons.com>
2021-12-01 16:52:02 -0500

    standardize gantry and head to use 100khz motor interrupt timer.

--- head/firmware/motor_hardware.c
+++ head/firmware/motor_hardware.c
@@ -194,7 +194,7 @@ void MX_TIM7_Init(void) {
     TIM_MasterConfigTypeDef sMasterConfig = {0};
 
     htim7.Instance = TIM7;
-    htim7.Init.Prescaler = 500;
+    htim7.Init.Prescaler = 849;
     htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
     htim7.Init.Period = 1;
     htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
