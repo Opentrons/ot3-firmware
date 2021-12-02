7df8935900127672d63d16b6cae64f25d74aa3d2
amit lissack <amit@opentrons.com>
2021-12-01 19:18:04 -0500

    more tests.

--- can/tests/test_arbitratration_id.cpp
+++ can/tests/test_arbitratration_id.cpp
@@ -20,12 +20,36 @@ SCENARIO("Arbitration ID from integer") {
 SCENARIO("Arbitration ID to integer") {
     GIVEN("an arbitration id object with all bits set") {
         auto subject = ArbitrationId();
-        subject.node_id(0xFFFF);
-        subject.function_code(0xFFFF);
+        subject.node_id(0xFF);
+        subject.function_code(0xFF);
         subject.message_id(0xFFFF);
 
         WHEN("converted to an integer") {
             THEN("All 29-bits are set") { REQUIRE(subject == 0x1FFFFFFF); }
+        }
+    }
+}
+
+SCENARIO("Arbitration ID") {
+    GIVEN("an arbitration id object") {
+        auto subject = ArbitrationId();
+        subject.node_id(123);
+        subject.function_code(210);
+        subject.message_id(4321);
+
+        WHEN("converted to an integer") {
+            THEN("the bits are set correctly") {
+                REQUIRE(subject == ((4321 << 15) | (123 << 7) | 210));
+            }
+        }
+
+        WHEN("creating another arbitration id from integer") {
+            auto subject2 = ArbitrationId(subject);
+            THEN("the individual values are the same") {
+                REQUIRE(subject.node_id() == subject2.node_id());
+                REQUIRE(subject.function_code() == subject2.function_code());
+                REQUIRE(subject.message_id() == subject2.message_id());
+            }
         }
     }
 }
