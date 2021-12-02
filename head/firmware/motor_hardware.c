7f31577116d01e2807dc1b07a68e493b9c42c8da
amit lissack <amit@opentrons.com>
2021-12-02 14:11:26 -0500

    change head's period

--- head/firmware/motor_hardware.c
+++ head/firmware/motor_hardware.c
@@ -196,7 +196,7 @@ void MX_TIM7_Init(void) {
     htim7.Instance = TIM7;
     htim7.Init.Prescaler = 849;
     htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim7.Init.Period = 1;
+    htim7.Init.Period = 2;
     htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
     if (HAL_TIM_Base_Init(&htim7) != HAL_OK) {
         Error_Handler();
