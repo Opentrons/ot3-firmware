05078ae71abd7d261822d3857204e580f69f0f3a
sotaoverride <aatrapps@gmail.com>
2021-12-08 14:49:12 -0500

    working config

--- head/firmware/adc.c
+++ head/firmware/adc.c
@@ -4,8 +4,8 @@
 
 #include "common/firmware/errors.h"
 
-ADC_HandleTypeDef adc1;
-ADC_HandleTypeDef adc2;
+ADC_HandleTypeDef adc1 = {0};
+ADC_HandleTypeDef adc2 = {0};
 
 typedef enum { connected, disconnected } state;
 
@@ -30,17 +30,21 @@ void MX_ADC1_Init(ADC_HandleTypeDef* adc1) {
     /** Common config
      */
     adc1->Instance = ADC1;
-    adc1->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV1;
+    adc1->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
     adc1->Init.Resolution = ADC_RESOLUTION_12B;
-    adc1->Init.ScanConvMode = ADC_SCAN_ENABLE;
-    adc1->Init.ContinuousConvMode = DISABLE;
-    adc1->Init.DiscontinuousConvMode = DISABLE;
     adc1->Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    adc1->Init.NbrOfConversion = 1;
-    adc1->Init.DMAContinuousRequests = DISABLE;
+    adc1->Init.GainCompensation = 0;
+    adc1->Init.ScanConvMode = ADC_SCAN_DISABLE;
     adc1->Init.EOCSelection = ADC_EOC_SINGLE_CONV;
     adc1->Init.LowPowerAutoWait = DISABLE;
+    adc1->Init.ContinuousConvMode = DISABLE;
+    adc1->Init.NbrOfConversion = 1;
+    adc1->Init.DiscontinuousConvMode = DISABLE;
+    adc1->Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    adc1->Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    adc1->Init.DMAContinuousRequests = DISABLE;
     adc1->Init.Overrun = ADC_OVR_DATA_PRESERVED;
+    adc1->Init.OversamplingMode = DISABLE;
     if (HAL_ADC_Init(adc1) != HAL_OK) {
         Error_Handler();
     }
@@ -54,20 +58,23 @@ void MX_ADC1_Init(ADC_HandleTypeDef* adc1) {
 void MX_ADC2_Init(ADC_HandleTypeDef* adc2) {
     /** Common config
      */
+
     adc2->Instance = ADC2;
-    adc2->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV1;
+    adc2->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
     adc2->Init.Resolution = ADC_RESOLUTION_12B;
-    adc2->Init.ScanConvMode = ADC_SCAN_ENABLE;
-    adc2->Init.ContinuousConvMode = DISABLE;
-    adc2->Init.DiscontinuousConvMode = DISABLE;
-    adc2->Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    adc2->Init.ExternalTrigConv = ADC_SOFTWARE_START;
     adc2->Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    adc2->Init.NbrOfConversion = 2;
-    adc2->Init.DMAContinuousRequests = DISABLE;
+    adc2->Init.GainCompensation = 0;
+    adc2->Init.ScanConvMode = ADC_SCAN_DISABLE;
     adc2->Init.EOCSelection = ADC_EOC_SINGLE_CONV;
     adc2->Init.LowPowerAutoWait = DISABLE;
+    adc2->Init.ContinuousConvMode = DISABLE;
+    adc2->Init.NbrOfConversion = 1;
+    adc2->Init.DiscontinuousConvMode = DISABLE;
+    adc2->Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    adc2->Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    adc2->Init.DMAContinuousRequests = DISABLE;
     adc2->Init.Overrun = ADC_OVR_DATA_PRESERVED;
+    adc2->Init.OversamplingMode = DISABLE;
     if (HAL_ADC_Init(adc2) != HAL_OK) {
         Error_Handler();
     }
@@ -129,7 +136,11 @@ void ADC_set_chan(uint32_t chan, ADC_HandleTypeDef* handle) {
      * the sequencer and its sample time.
      */
     sConfig.Channel = chan;
+    sConfig.Rank = ADC_REGULAR_RANK_1;
     sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+    sConfig.SingleDiff = ADC_SINGLE_ENDED;
+    sConfig.OffsetNumber = ADC_OFFSET_NONE;
+    sConfig.Offset = 0;
     if (HAL_ADC_ConfigChannel(handle, &sConfig) != HAL_OK) {
         Error_Handler();
     }
@@ -154,6 +165,8 @@ void adc_read_voltages() {
     HAL_ADC_Start(&adc2);
     HAL_ADC_PollForConversion(&adc2, HAL_MAX_DELAY);
     adc3_value = HAL_ADC_GetValue(&adc2);
+
+    // HAL_ADC_Stop(&adc2);
 
     struct voltage_read voltage_read = {
         .z_motor = adc3_value, .a_motor = adc2_value, .gripper = adc1_value};
