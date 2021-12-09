2d828e14b61d1db7e0a4e65a1ef70bb74df9802d
Laura Cox <laura@opentrons.com>
2021-12-09 11:10:34 -0500

    feat(common): enable software timers in FreeRTOS
    Quick PR to add the ability to use software timers in FreeRTOS

---
+++ include/common/core/freertos_timer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "FreeRTOS.h"
+#include "timers.h"
+
+namespace freertos_timer {
+
+// pdMS_TO_TICKS converts milliseconds to ticks. This can only be used for
+// FreeRTOS tick rates less than 1000 Hz.
+template <TickType_t timer_period = pdMS_TO_TICKS(2)>
+class FreeRTOSTimer {
+
+  public:
+    /*
+     * A software timer class. The priority of software timers in FreeRTOS is
+     * currently set to 6. Any tasks utilizing this timer should have either the
+     * same priority or higher priority than 6 for execution.
+     */
+    FreeRTOSTimer(const char * name, TimerCallbackFunction_t callback) :
+        block_time(timer_period) {
+        timer = xTimerCreateStatic(
+            name, block_time, AutoReload, this, callback, &timer_buffer);
+    }
+    auto operator=(FreeRTOSTimer&) -> FreeRTOSTimer& = delete;
+    auto operator=(FreeRTOSTimer&&) -> FreeRTOSTimer&& = delete;
+    FreeRTOSTimer(FreeRTOSTimer&) = delete;
+    FreeRTOSTimer(FreeRTOSTimer&&) = delete;
+
+    void start() {
+        xTimerStart(timer, block_time);
+    }
+
+    void stop() {
+        xTimerStop(timer, block_time);
+    }
+
+  private:
+    TimerHandle_t timer;
+    TickType_t block_time;
+    StaticTimer_t timer_buffer{};
+    UBaseType_t AutoReload = pdTRUE;
+};
+
+} // namespace freertos_timer
+
