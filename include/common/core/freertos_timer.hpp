e55439c734417c156a58216d0efc2c1ed3610ad8
Laura Cox <laura@opentrons.com>
2021-12-13 16:30:52 -0500

    feat(common): add FreeRTOS software timer support

---
+++ include/common/core/freertos_timer.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <functional>
+
+#include "FreeRTOS.h"
+#include "timers.h"
+
+namespace freertos_timer {
+
+// pdMS_TO_TICKS converts milliseconds to ticks. This can only be used for
+// FreeRTOS tick rates less than 1000 Hz.
+template <TickType_t timer_period = pdMS_TO_TICKS(2)>
+class FreeRTOSTimer {
+  public:
+    /*
+     * A software timer class. The priority of software timers in FreeRTOS is
+     * currently set to 6. Any tasks utilizing this timer should have either the
+     * same priority or higher priority than 6 for execution.
+     */
+    FreeRTOSTimer(const char* name, std::function<void()> callback)
+        : block_time(timer_period), callback(callback) {
+        timer = xTimerCreateStatic(name, block_time, auto_reload, this,
+                                   timer_callback, &timer_buffer);
+    }
+    auto operator=(FreeRTOSTimer&) -> FreeRTOSTimer& = delete;
+    auto operator=(FreeRTOSTimer&&) -> FreeRTOSTimer&& = delete;
+    FreeRTOSTimer(FreeRTOSTimer&) = delete;
+    FreeRTOSTimer(FreeRTOSTimer&&) = delete;
+
+    void start() { xTimerStart(timer, 0); }
+
+    void stop() { xTimerStop(timer, 0); }
+
+  private:
+    TimerHandle_t timer{};
+    TickType_t block_time;
+    std::function<void()> callback;
+    StaticTimer_t timer_buffer{};
+    UBaseType_t auto_reload = pdTRUE;
+
+    static void timer_callback(TimerHandle_t xTimer) {
+        auto timer_id = pvTimerGetTimerID(xTimer);
+        auto instance =
+            reinterpret_cast<FreeRTOSTimer<timer_period>*>(timer_id);
+        instance->callback();
+    }
+};
+
+}  // namespace freertos_timer
--- pipettes/firmware/main.cpp
+++ pipettes/firmware/main.cpp
@@ -9,12 +9,20 @@
 
 // clang-format on
 
+#include "common/core/freertos_timer.hpp"
 #include "common/firmware/clocking.h"
+
+static void callback() {}
 
 auto main() -> int {
     HardwareInit();
     RCC_Peripheral_Clock_Select();
     MX_ICACHE_Init();
 
+    // Dummy timer code, should be removed once SW timers are implemented
+    auto timer = freertos_timer::FreeRTOSTimer("test timer", callback);
+    timer.start();
+
     vTaskStartScheduler();
 }
+
