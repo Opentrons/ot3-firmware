c8dc50c984ba4f66385e79da99feb77c7e3e446e
amit lissack <amit@opentrons.com>
2021-12-16 14:12:43 -0500

    move group task.

--- gantry/simulator/main.cpp
+++ gantry/simulator/main.cpp
@@ -6,6 +6,7 @@
 #include "common/core/freertos_message_queue_poller.hpp"
 #include "motor-control/core/tasks/motor_driver_task.hpp"
 #include "motor-control/core/tasks/motion_controller_task.hpp"
+#include "motor-control/core/tasks/move_group_task.hpp"
 
 static auto motor_driver_message_queue =
     freertos_message_queue::FreeRTOSMessageQueue<
@@ -16,9 +17,25 @@ static auto motor_driver_task_handler =
 static auto motor_driver_task_entry = motor_driver_task::MotorDriverTask(
     motor_driver_message_queue, motor_driver_task_handler);
 
-static auto motion_controller_queue = freertos_message_queue::FreeRTOSMessageQueue<motion_controller_task::TaskMessage>{};
-static auto motion_controller_task_handler = motion_controller_task::MotionControllerMessageHandler{interfaces::get_motor().motion_controller};
-static auto motion_controller_task_entry = motion_controller_task::MotorControllerTask<lms::BeltConfig>{motion_controller_queue, motion_controller_task_handler};
+static auto motion_controller_queue =
+    freertos_message_queue::FreeRTOSMessageQueue<
+        motion_controller_task::TaskMessage>{};
+static auto motion_controller_task_handler =
+    motion_controller_task::MotionControllerMessageHandler{
+        interfaces::get_motor().motion_controller};
+static auto motion_controller_task_entry =
+    motion_controller_task::MotorControllerTask<lms::BeltConfig>{
+        motion_controller_queue, motion_controller_task_handler};
+
+static auto move_group_manager = move_group_task::MoveGroupType{};
+
+static auto move_group_message_queue =
+    freertos_message_queue::FreeRTOSMessageQueue<
+        move_group_task::TaskMessage>{};
+static auto move_group_task_handler =
+    move_group_task::MoveGroupMessageHandler(move_group_manager);
+static auto move_group_task_entry = move_group_task::MoveGroupTask(
+    move_group_message_queue, move_group_task_handler);
 
 int main() {
     interfaces::initialize();
--- include/motor-control/core/tasks/move_group_task.hpp
+++ include/motor-control/core/tasks/move_group_task.hpp
@@ -1,1 +1,89 @@
 #pragma once
+
+#include <variant>
+#include "can/core/messages.hpp"
+#include "motor-control/core/motion_group.hpp"
+
+namespace move_group_task {
+
+using TaskMessage = std::variant<std::monostate, can_messages::AddLinearMoveRequest,
+      can_messages::GetMoveGroupRequest, can_messages::ClearAllMoveGroupsRequest, can_messages::ExecuteMoveGroupRequest>;
+
+constexpr std::size_t max_groups = 6;
+constexpr std::size_t max_moves_per_group = 5;
+
+using MoveGroupType =
+    move_group::MoveGroupManager<max_groups, max_moves_per_group,
+                                 can_messages::AddLinearMoveRequest>;
+
+
+/**
+ * The handler of move group messages
+ */
+class MoveGroupMessageHandler {
+  public:
+    MoveGroupMessageHandler(MoveGroupType &move_group_manager)
+        : move_groups{move_group_manager} {}
+    ~MoveGroupMessageHandler() = default;
+
+    /**
+     * Called upon arrival of new message
+     * @param message
+     */
+    void handle_message(const TaskMessage& message) {
+        std::visit([this](auto m){this->handle(m);}, message);
+    }
+
+  private:
+    void handle(std::monostate m) {
+        static_cast<void>(m);
+    }
+
+    void handle(const can_messages::AddLinearMoveRequest& m) {
+        static_cast<void>(move_groups[m.group_id].set_move(m));
+    }
+
+    void handle(const can_messages::GetMoveGroupRequest& m) {
+        auto group = move_groups[m.group_id];
+        auto response = can_messages::GetMoveGroupResponse{
+            .group_id = m.group_id,
+            .num_moves = static_cast<uint8_t>(group.size()),
+            .total_duration = group.get_duration()};
+
+//        message_writer.write(NodeId::host, response);
+    }
+
+    void handle(const can_messages::ClearAllMoveGroupsRequest& m) {
+        for (auto &group : move_groups) {
+            group.clear();
+        }
+    }
+
+    void handle(const can_messages::ExecuteMoveGroupRequest& m) {
+        auto group = move_groups[m.group_id];
+        for (std::size_t i = 0; i < max_moves_per_group;
+             i++) {
+            auto move = group.get_move(i);
+            std::visit([this](auto &m) { this->visit_move(m); }, move);
+        }
+    }
+
+    void visit_move(const std::monostate &m) {
+
+    }
+
+    void visit_move(const can_messages::AddLinearMoveRequest &m) {
+        //motor.motion_controller.move(m);
+    }
+
+    MoveGroupType& move_groups;
+};
+
+/**
+ * The task type.
+ */
+using MoveGroupTask =
+    freertos_message_queue_poller::FreeRTOSMessageQueuePoller<
+        TaskMessage, MoveGroupMessageHandler>;
+
+}
