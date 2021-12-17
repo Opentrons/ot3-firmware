d50a3c27794e82d29e40988ee5433f1710b123f0
sotaoverride <aatrapps@gmail.com>
2021-12-17 11:02:07 -0500

    lint

--- head/firmware/presence_sensor.cpp
+++ head/firmware/presence_sensor.cpp
@@ -1,15 +1,15 @@
 #include "can/core/dispatch.hpp"
 #include "can/core/freertos_can_dispatch.hpp"
 #include "can/core/ids.hpp"
+#include "can/core/message_handlers/presence_sensing.hpp"
 #include "can/core/message_writer.hpp"
 #include "can/core/messages.hpp"
-#include "can/core/message_handlers/presence_sensing.hpp"
 #include "can/firmware/hal_can_bus.hpp"
 #include "common/core/freertos_message_queue.hpp"
 #include "common/core/freertos_task.hpp"
-#include "presence_sensor/core/presence_sensor_messages.hpp"
-#include "presence_sensor/core/presence_sensor_class.hpp"
 #include "common/core/message_buffer.hpp"
+#include "presence_sensor/core/presence_sensor_class.hpp"
+#include "presence_sensor/core/presence_sensor_messages.hpp"
 
 #pragma GCC diagnostic push
 // NOLINTNEXTLINE(clang-diagnostic-unknown-warning-option)
@@ -36,9 +36,8 @@ using namespace can_message_buffer;
 
 adc::ADC_interface ADC_intf2 = {
 
-    .ADC_handle = &adc2
-};
-    
+    .ADC_handle = &adc2};
+
 adc::ADC ADC_comms2(ADC_intf2);
 
 adc::ADC_interface ADC_intf1 = {
@@ -48,25 +47,32 @@ adc::ADC_interface ADC_intf1 = {
 };
 
 adc::ADC ADC_comms1(ADC_intf1);
-hal_can_bus::HalCanBus can_bus_1 = hal_can_bus::HalCanBus(can_get_device_handle());
+hal_can_bus::HalCanBus can_bus_1 =
+    hal_can_bus::HalCanBus(can_get_device_handle());
 
-//extern hal_can_bus::HalCanBus can_bus_1;
-can_message_writer::MessageWriter message_writer_presence_sensor = MessageWriter(can_bus_1, NodeId::presence_sensor);
+// extern hal_can_bus::HalCanBus can_bus_1;
+can_message_writer::MessageWriter message_writer_presence_sensor =
+    MessageWriter(can_bus_1, NodeId::presence_sensor);
 
-freertos_message_queue::FreeRTOSMessageQueue<presence_sensor_messages::Reading> presence_sensor_queue(
-    "Presence Sensing Queue");
+freertos_message_queue::FreeRTOSMessageQueue<presence_sensor_messages::Reading>
+    presence_sensor_queue("Presence Sensing Queue");
 freertos_message_queue::FreeRTOSMessageQueue<presence_sensor_messages::Ack>
     complete_queue_presence_sensing("Presence Sensing Acked");
-presence_sensor_class::PresenceSensor ps(ADC_comms2,presence_sensor_queue,complete_queue_presence_sensing);
-presence_sensing_message_handler::PresenceSensorHandler presence_sensing_handler = presence_sensing_message_handler::PresenceSensorHandler{message_writer_presence_sensor, ps};
+presence_sensor_class::PresenceSensor ps(ADC_comms2, presence_sensor_queue,
+                                         complete_queue_presence_sensing);
+presence_sensing_message_handler::PresenceSensorHandler
+    presence_sensing_handler =
+        presence_sensing_message_handler::PresenceSensorHandler{
+            message_writer_presence_sensor, ps};
 
-
-auto presence_sensor_dispatch_target = DispatchParseTarget<
-    decltype(presence_sensing_handler), can_messages::PresenceSensingRequest>
-    {presence_sensing_handler};
+auto presence_sensor_dispatch_target =
+    DispatchParseTarget<decltype(presence_sensing_handler),
+                        can_messages::PresenceSensingRequest>{
+        presence_sensing_handler};
 
 template <CanMessageBufferListener Listener>
-auto build_ps_dispatch() -> can_dispatch::Dispatcher<Listener>{
-    auto presence_sensor_dispatcher = Dispatcher([](auto _) -> bool { return true; }, presence_sensor_dispatch_target);
+auto build_ps_dispatch() -> can_dispatch::Dispatcher<Listener> {
+    auto presence_sensor_dispatcher = Dispatcher(
+        [](auto _) -> bool { return true; }, presence_sensor_dispatch_target);
     return presence_sensor_dispatcher;
 }
