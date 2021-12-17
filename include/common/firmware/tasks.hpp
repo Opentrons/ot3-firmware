914c1b41de4a35a11c5226e7a79805a17dcbc00a
Frank Sinapi <frank.sinapi@opentrons.com>
2021-12-17 16:42:38 -0500

    Changes to allow build_ps_dispatch() to discern return type

--- include/common/firmware/tasks.hpp
+++ include/common/firmware/tasks.hpp
@@ -3,12 +3,14 @@
 #include "can/core/freertos_can_dispatch.hpp"
 #include "can/core/ids.hpp"
 #include "can/core/message_handlers/presence_sensing.hpp"
+#include "presence_sensor/core/presence_sensor_class.hpp"
 #include "can/core/message_writer.hpp"
 #include "can/core/messages.hpp"
 #include "can/firmware/hal_can_bus.hpp"
 #include "common/core/freertos_message_queue.hpp"
 #include "common/core/freertos_task.hpp"
 #include "common/core/message_buffer.hpp"
+#include "common/firmware/adc_comms.hpp"
 
 
 using namespace freertos_task;
@@ -24,11 +26,8 @@ using namespace can_message_buffer;
 using namespace can_arbitration_id;
 using namespace can_message_buffer;
 
-using PresenceSenseDispatchTargetT = DispatchParseTarget<presence_sensing_message_handler::PresenceSensorHandler,
-                        can_messages::PresenceSensingRequest>;
-#if 0
-template <CanMessageBufferListener Listener>
-auto build_ps_dispatch() -> decltype(can_dispatch::Dispatcher<Listener>);
-#endif 
+using PresenceSenseDispatchTargetT = DispatchParseTarget<
+    presence_sensing_message_handler::PresenceSensorHandler<presence_sensor_class::PresenceSensor<adc::ADC>>,
+    can_messages::PresenceSensingRequest>;
 
 auto build_ps_dispatch() -> can_dispatch::Dispatcher<PresenceSenseDispatchTargetT>;
