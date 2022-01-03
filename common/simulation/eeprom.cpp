9af47ae59189ee89b099dde3a67c1ab8e0f0d4ef
amit lissack <amit@opentrons.com>
2022-01-03 13:33:20 -0500

    use std::copy

--- common/simulation/eeprom.cpp
+++ common/simulation/eeprom.cpp
@@ -5,17 +5,14 @@ auto sim_i2c::SimEEProm::master_transmit(uint16_t dev_address, uint8_t *data,
                                          uint16_t size, uint32_t timeout)
     -> bool {
     this->data.clear();
-    for (auto i = 0; i < size; i++) {
-        this->data.push_back(data[i]);
-    }
+    this->data.reserve(size);
+    std::copy(data, data + size, std::back_inserter(this->data));
     return true;
 }
 
 auto sim_i2c::SimEEProm::master_receive(uint16_t dev_address, uint8_t *data,
                                         uint16_t size, uint32_t timeout)
     -> bool {
-    for (uint32_t i = 0; i < size && i < this->data.size(); i++) {
-        data[i] = this->data[i];
-    }
+    std::copy(this->data.cbegin(), this->data.cend(), data);
     return true;
 }
