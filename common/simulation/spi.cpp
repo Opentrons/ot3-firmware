3bbffc55ef65223d1efb9e6f9e2ed4dc99d72667
amitlissack <amit@opentrons.com>
2022-01-11 14:57:45 -0500

    fix(spi): read requires two transmissions. (#209)
    * change spi simulator to closer resemble real behavior.

--- common/simulation/spi.cpp
+++ common/simulation/spi.cpp
@@ -19,14 +19,20 @@ void sim_spi::SimTMC2130Spi::transmit_receive(
     constexpr uint8_t write_mask =
         static_cast<uint8_t>(spi::TMC2130Spi::Mode::WRITE);
 
+    auto out_iter = receive.begin();
+    // Write status byte into buffer.
+    out_iter = bit_utils::int_to_bytes(status, out_iter, receive.end());
+
     uint8_t reg = control & ~write_mask;
 
     if (control & write_mask) {
+        // This is a write command.
         register_map[reg] = data;
+    } else {
+        // A read command will return the data from the previous read command.
+        out_iter = bit_utils::int_to_bytes(register_map[read_register],
+                                           out_iter, receive.end());
     }
-
-    auto out_iter = receive.begin();
-    out_iter = bit_utils::int_to_bytes(reg, out_iter, receive.end());
-    out_iter =
-        bit_utils::int_to_bytes(register_map[reg], out_iter, receive.end());
+    // The register is cached for the next read operation.
+    read_register = reg;
 }
