e9831ea6fd9e3b4b890cd0d546539a0ba5ea7e72
amit lissack <amit@opentrons.com>
2022-01-21 12:27:33 -0500

    start can

--- bootloader/firmware/main.c
+++ bootloader/firmware/main.c
@@ -1,1 +1,48 @@
-int main() {}
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static FDCAN_TxHeaderTypeDef TxHeader;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static FDCAN_RxHeaderTypeDef RxHeader;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static FDCAN_HandleTypeDef can;
+static constexpr auto buffsize = 64;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static auto buff = std::array<uint8_t, buffsize>{};
+
+int main() {
+    if (MX_FDCAN1_Init(&can) != HAL_OK) {
+        Error_Handler();
+    }
+
+    if (HAL_FDCAN_Start(&can) != HAL_OK) {
+        Error_Handler();
+    }
+
+    TxHeader.IdType = FDCAN_EXTENDED_ID;
+    TxHeader.TxFrameType = FDCAN_DATA_FRAME;
+    TxHeader.DataLength = FDCAN_DLC_BYTES_8;
+    TxHeader.ErrorStateIndicator = FDCAN_ESI_PASSIVE;
+    TxHeader.BitRateSwitch = FDCAN_BRS_OFF;
+    TxHeader.FDFormat = FDCAN_CLASSIC_CAN;
+    TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+    TxHeader.MessageMarker = 0;
+
+    for (;;) {
+        if (HAL_FDCAN_GetRxFifoFillLevel(&can, FDCAN_RX_FIFO0) > 0) {
+            // Send CAN RX data over debug UART
+            HAL_FDCAN_GetRxMessage(&can, FDCAN_RX_FIFO0, &RxHeader,
+                                   buff.data());
+
+            TxHeader.Identifier = RxHeader.Identifier;
+            TxHeader.DataLength = RxHeader.DataLength;
+            TxHeader.FDFormat = RxHeader.FDFormat;
+
+            if (HAL_FDCAN_AddMessageToTxFifoQ(&can, &TxHeader, buff.data()) !=
+                HAL_OK) {
+                // Transmission request Error
+                Error_Handler();
+            }
+        }
+        vTaskDelay(1);
+    }
+}
