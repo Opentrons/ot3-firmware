3b385d332d7b2213a7a5c03cc63abbe4711e81d6
sotaoverride <aatrapps@gmail.com>
2022-01-24 10:36:10 -0500

    add tool detection function

--- include/head/core/adc.hpp
+++ include/head/core/adc.hpp
@@ -2,8 +2,30 @@
 
 #include <concepts>
 #include <cstdint>
+#define 96_CHAN_DETECTION_UPPER_BOUND = 999
+#define 96_CHAN_DETECTION_LOWER_BOUND = 666
+
+#define GRIPPER_DETECTION_UPPER_BOUND = 999
+#define GRIPPER_DETECTION_LOWER_BOUND = 666
 
 namespace adc {
+
+struct Tool {
+    enum ToolType : uint8_t {
+        UNDEFINED = 0x00,
+        PIPETTE96CHAN = 0x00,
+        GRIPPER = 0x01,
+    };
+    uint16_t detection_upper_bound;
+    uint16_t detection_lower_bound;
+};
+
+static struct Tool OT3ToolList[] = {
+    {ToolType::UNDEFINED, 0, 0},
+    {ToolType::PIPETTE96CHAN, 96_CHAN_DETECTION_UPPER_BOUND,
+     96_CHAN_DETECTION_LOWER_BOUND},
+    {ToolType::GRIPPER, GRIPPER_DETECTION_UPPER_BOUND,
+     GRIPPER_DETECTION_LOWER_BOUND}};
 
 struct MillivoltsReadings {
     uint16_t z_motor;
