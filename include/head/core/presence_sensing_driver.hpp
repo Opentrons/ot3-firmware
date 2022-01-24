694b8636092a7dff03bf64312f2a317b74f00244
sotaoverride <aatrapps@gmail.com>
2022-01-24 11:59:15 -0500

    build and link

--- include/head/core/presence_sensing_driver.hpp
+++ include/head/core/presence_sensing_driver.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <array>
+#include <vector>
 
 #include "common/core/bit_utils.hpp"
 #include "head/core/adc.hpp"
 
 namespace presence_sensing_driver {
+static std::vector<adc::Tool> OT3ToolList{
+    {adc::ToolType::UNDEFINED, 0, 0},
+    {adc::ToolType::PIPETTE96CHAN, PIPETTE_96_CHAN_DETECTION_UPPER_BOUND,
+     PIPETTE_96_CHAN_DETECTION_LOWER_BOUND},
+    {adc::ToolType::GRIPPER, GRIPPER_DETECTION_UPPER_BOUND,
+     GRIPPER_DETECTION_LOWER_BOUND}};
 
 class PresenceSensingDriver {
   public:
@@ -30,10 +37,10 @@ class PresenceSensingDriver {
 
     auto get_tool(uint16_t reading) -> adc::Tool {
         adc::Tool td = {adc::ToolType::UNDEFINED, 0, 0};
-        for (size_t i = 0; i < adc::OT3ToolList.size(); i++)
-            if ((reading < adc::OT3ToolList[i].detection_upper_bound) &&
-                (reading >= adc::OT3ToolList[i].detection_lower_bound))
-                return adc::OT3ToolList[i];
+        for (size_t i = 0; i < OT3ToolList.size(); i++)
+            if ((reading < OT3ToolList[i].detection_upper_bound) &&
+                (reading >= OT3ToolList[i].detection_lower_bound))
+                return OT3ToolList[i];
         return td;
     }
 
