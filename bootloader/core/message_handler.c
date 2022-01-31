cf6185018fb6ca04917dca7164e3066c9ddc2ce7
amit lissack <amit@opentrons.com>
2022-01-31 14:51:09 -0500

    fw messages.

--- bootloader/core/message_handler.c
+++ bootloader/core/message_handler.c
@@ -2,15 +2,16 @@
 #include "bootloader/core/ids.h"
 #include "bootloader/core/node_id.h"
 #include "bootloader/core/version.h"
+#include "bootloader/core/messages.h"
 
 
 static HandleMessageReturn handle_device_info_request(const Message* request, Message* response);
 
 static HandleMessageReturn handle_initiate_fw_update(const Message* request, Message* response);
 
-static HandleMessageReturn handle_initiate_fw_update_data(const Message* request, Message* response);
+static HandleMessageReturn handle_fw_update_data(const Message* request, Message* response);
 
-static HandleMessageReturn handle_initiate_fw_update_complete(const Message* request, Message* response);
+static HandleMessageReturn handle_fw_update_complete(const Message* request, Message* response);
 
 static uint8_t * write_uint16(uint8_t * buffer, uint16_t value);
 
@@ -30,6 +31,15 @@ HandleMessageReturn handle_message(const Message* request, Message* response) {
     HandleMessageReturn ret = handle_message_ok;
 
     switch (request->arbitration_id.parts.message_id) {
+        case can_messageid_fw_update_data:
+            ret = handle_fw_update_data(request, response);
+            break;
+        case can_messageid_fw_update_complete:
+            ret = handle_fw_update_complete(request, response);
+            break;
+        case can_messageid_fw_update_initiate:
+            ret = handle_initiate_fw_update(request, response);
+            break;
         case can_messageid_device_info_request:
             ret = handle_device_info_request(request, response);
             break;
@@ -40,19 +50,70 @@ HandleMessageReturn handle_message(const Message* request, Message* response) {
 }
 
 
-static HandleMessageReturn handle_device_info_request(const Message* request, Message* response) {
+HandleMessageReturn handle_device_info_request(const Message* request, Message* response) {
     response->arbitration_id.id = 0;
     response->arbitration_id.parts.message_id = can_messageid_device_info_response;
     response->arbitration_id.parts.node_id = can_nodeid_host;
     response->arbitration_id.parts.originating_node_id = get_node_id();
     response->size = 4;
     write_uint32(response->data, get_version());
+    return handle_message_has_response;
+}
+
+
+HandleMessageReturn handle_initiate_fw_update(const Message* request, Message* response) {
+
     return handle_message_ok;
+}
+
+HandleMessageReturn handle_fw_update_data(const Message* request, Message* response) {
+    UpdateData data;
+    ErrorCode e = parse_update_data(request->data, request->size, &data);
+
+    if (e == can_errorcode_ok) {
+        // All is good. Pass on to flasher.
+    }
+
+    // Build response
+    uint8_t* p = response->data;
+    p = write_uint32(p, data.address);
+    p = write_uint16(p, e);
+
+    response->arbitration_id.id = 0;
+    response->arbitration_id.parts.message_id = can_messageid_fw_update_data_ack;
+    response->arbitration_id.parts.node_id = can_nodeid_host;
+    response->arbitration_id.parts.originating_node_id = get_node_id();
+    response->size = p - response->data;
+
+    return handle_message_has_response;
+}
+
+HandleMessageReturn handle_fw_update_complete(const Message* request, Message* response) {
+    UpdateComplete complete;
+    ErrorCode e = parse_update_complete(request->data, request->size, &complete);
+
+    if (e == can_errorcode_ok) {
+        e = can_errorcode_invalid_size;
+    }
+
+    // Build response
+    uint8_t* p = response->data;
+    p = write_uint16(p, e);
+
+    response->arbitration_id.id = 0;
+    response->arbitration_id.parts.message_id = can_messageid_fw_update_complete_ack;
+    response->arbitration_id.parts.node_id = can_nodeid_host;
+    response->arbitration_id.parts.originating_node_id = get_node_id();
+    response->size = p - response->data;
+
+    return handle_message_has_response;
 }
 
 
 uint8_t * write_uint16(uint8_t * buffer, uint16_t value) {
-
+    *(buffer++) = (value >> 8) & 0xFF;
+    *(buffer++) = value & 0xFF;
+    return buffer;
 }
 
 uint8_t * write_uint32(uint8_t * buffer, uint32_t value) {
--- bootloader/tests/test_message_handler.cpp
+++ bootloader/tests/test_message_handler.cpp
@@ -1,37 +1,37 @@
-#include "bootloader/core/message_handler.h"
 #include "bootloader/core/ids.h"
+#include "bootloader/core/message_handler.h"
 #include "bootloader/core/node_id.h"
 #include "bootloader/core/version.h"
 #include "catch2/catch.hpp"
 
+NodeId get_node_id(void) { return can_nodeid_pipette_bootloader; }
 
-NodeId get_node_id(void) {
-    return can_nodeid_pipette_bootloader;
-}
-
-uint32_t get_version(void) {
-    return 0xDEADBEEF;
-}
-
+uint32_t get_version(void) { return 0xDEADBEEF; }
 
 SCENARIO("device info") {
     GIVEN("a request") {
         Message request;
-        request.arbitration_id.parts.function_code=0;
-        request.arbitration_id.parts.node_id=get_node_id();
-        request.arbitration_id.parts.originating_node_id=can_nodeid_host;
-        request.arbitration_id.parts.message_id=can_messageid_device_info_request;
-        request.arbitration_id.parts.padding=0;
+        request.arbitration_id.parts.function_code = 0;
+        request.arbitration_id.parts.node_id = get_node_id();
+        request.arbitration_id.parts.originating_node_id = can_nodeid_host;
+        request.arbitration_id.parts.message_id =
+            can_messageid_device_info_request;
+        request.arbitration_id.parts.padding = 0;
 
         WHEN("received") {
             Message response;
             auto error = handle_message(&request, &response);
-            THEN("it returns ok") { REQUIRE(error == handle_message_ok); }
+            THEN("it returns response") {
+                REQUIRE(error == handle_message_has_response);
+            }
             THEN("it populates response arbitration id") {
                 REQUIRE(response.arbitration_id.parts.function_code == 0);
-                REQUIRE(response.arbitration_id.parts.node_id == can_nodeid_host);
-                REQUIRE(response.arbitration_id.parts.originating_node_id == get_node_id());
-                REQUIRE(response.arbitration_id.parts.message_id == can_messageid_device_info_response);
+                REQUIRE(response.arbitration_id.parts.node_id ==
+                        can_nodeid_host);
+                REQUIRE(response.arbitration_id.parts.originating_node_id ==
+                        get_node_id());
+                REQUIRE(response.arbitration_id.parts.message_id ==
+                        can_messageid_device_info_response);
             }
             THEN("it populates response body") {
                 REQUIRE(response.size == 4);
@@ -39,6 +39,91 @@ SCENARIO("device info") {
                 REQUIRE(response.data[1] == 0xAD);
                 REQUIRE(response.data[2] == 0xBE);
                 REQUIRE(response.data[3] == 0xEF);
+            }
+        }
+    }
+}
+
+SCENARIO("update data bad checksum error") {
+    GIVEN("a request") {
+        Message request;
+        request.arbitration_id.parts.function_code = 0;
+        request.arbitration_id.parts.node_id = get_node_id();
+        request.arbitration_id.parts.originating_node_id = can_nodeid_host;
+        request.arbitration_id.parts.message_id = can_messageid_fw_update_data;
+        request.arbitration_id.parts.padding = 0;
+        request.data[0] = 0xDE;
+        request.data[1] = 0xAD;
+        request.data[2] = 0xBE;
+        request.data[3] = 0xEF;
+        request.size = 64;
+
+        WHEN("received") {
+            Message response;
+            auto error = handle_message(&request, &response);
+            THEN("it returns response") {
+                REQUIRE(error == handle_message_has_response);
+            }
+            THEN("it populates response arbitration id") {
+                REQUIRE(response.arbitration_id.parts.function_code == 0);
+                REQUIRE(response.arbitration_id.parts.node_id ==
+                        can_nodeid_host);
+                REQUIRE(response.arbitration_id.parts.originating_node_id ==
+                        get_node_id());
+                REQUIRE(response.arbitration_id.parts.message_id ==
+                        can_messageid_fw_update_data_ack);
+            }
+            THEN("it populates response body") {
+                REQUIRE(response.size == 6);
+                REQUIRE(response.data[0] == 0xDE);
+                REQUIRE(response.data[1] == 0xAD);
+                REQUIRE(response.data[2] == 0xBE);
+                REQUIRE(response.data[3] == 0xEF);
+                REQUIRE(response.data[4] ==
+                        ((can_errorcode_bad_checksum >> 8) & 0xFF));
+                REQUIRE(response.data[5] ==
+                        (can_errorcode_bad_checksum & 0xFF));
+            }
+        }
+    }
+}
+
+SCENARIO("update data complete error") {
+    GIVEN("a request") {
+        Message request;
+        request.arbitration_id.parts.function_code = 0;
+        request.arbitration_id.parts.node_id = get_node_id();
+        request.arbitration_id.parts.originating_node_id = can_nodeid_host;
+        request.arbitration_id.parts.message_id =
+            can_messageid_fw_update_complete;
+        request.arbitration_id.parts.padding = 0;
+        request.data[0] = 0xDE;
+        request.data[1] = 0xAD;
+        request.data[2] = 0xBE;
+        request.data[3] = 0xEF;
+        request.size = 4;
+
+        WHEN("received") {
+            Message response;
+            auto error = handle_message(&request, &response);
+            THEN("it returns response") {
+                REQUIRE(error == handle_message_has_response);
+            }
+            THEN("it populates response arbitration id") {
+                REQUIRE(response.arbitration_id.parts.function_code == 0);
+                REQUIRE(response.arbitration_id.parts.node_id ==
+                        can_nodeid_host);
+                REQUIRE(response.arbitration_id.parts.originating_node_id ==
+                        get_node_id());
+                REQUIRE(response.arbitration_id.parts.message_id ==
+                        can_messageid_fw_update_complete_ack);
+            }
+            THEN("it populates response body") {
+                REQUIRE(response.size == 2);
+                REQUIRE(response.data[0] ==
+                        ((can_errorcode_invalid_size >> 8) & 0xFF));
+                REQUIRE(response.data[1] ==
+                        (can_errorcode_invalid_size & 0xFF));
             }
         }
     }
