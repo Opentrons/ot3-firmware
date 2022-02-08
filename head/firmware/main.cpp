3a04d2e7a70d7db9112e8e6213488c1afda54795
sotaoverride <aatrapps@gmail.com>
2022-02-08 14:11:19 -0500

    static

--- head/firmware/main.cpp
+++ head/firmware/main.cpp
@@ -192,7 +192,7 @@ adc::ADC_interface ADC_intf1 = {
 
 static auto ADC_comms = adc::ADC(ADC_intf1, ADC_intf2);
 
-auto attached_tools = ot3_tool_list::AttachedTool{};
+static auto attached_tools = ot3_tool_list::AttachedTool{};
 
 static auto psd =
     presence_sensing_driver::PresenceSensingDriver{ADC_comms, attached_tools};
