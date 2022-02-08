8b9053a74187866d0671cbfe6b8636525b9a107f
amit lissack <amit@opentrons.com>
2022-02-08 10:20:05 -0500

    iterate test.

--- include/bootloader/core/util.h
+++ include/bootloader/core/util.h
@@ -45,6 +45,24 @@ uint8_t * write_uint32(uint8_t * buffer, uint32_t value);
  */
 uint16_t compute_checksum(const uint8_t * begin, const uint8_t * end);
 
+
+/**
+ * Callback given to dword_address_iter.
+ */
+typedef void (*address_iter_callback) (uint32_t address, uint64_t data);
+
+/**
+ * Iterate a byte buffer creating double word values and addresses.
+ *
+ * Writes to G4 and L5 flash happen in 64 bit data increments.
+ * @param address The starting flash address
+ * @param buffer A byte buffer
+ * @param length Length in bytes of the buffer.
+ * @param callback A callback that will be called for each 64 bit address
+ */
+void dword_address_iter(uint32_t address, const uint8_t * buffer, uint8_t length, address_iter_callback callback);
+
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif  // __cplusplus
