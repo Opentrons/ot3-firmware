f1f57772be57aefa581b012d6b6469478095eef8
Seth Foster <seth@opentrons.com>
2022-02-16 11:44:34 -0500

    feat(bootloader): add pipette mount detection
    This unfortunately reimplements the mount detection algorithm in C but
    it will work just fine (and is checked by unit tests). Bootloaders
    should now properly decide which mount they're on.

--- bootloader/firmware/stm32G4/node_id_stm32g4xx.c
+++ bootloader/firmware/stm32G4/node_id_stm32g4xx.c
@@ -5,10 +5,7 @@
  * @return Node id.
  */
 CANNodeId get_node_id(void) {
-#if defined node_id_pipette_left
-    // TODO make this decision based on the mount sense voltage
-    return can_nodeid_pipette_left_bootloader;
-#elif defined node_id_head
+#if defined node_id_head
     return can_nodeid_head_bootloader;
 #elif defined node_id_gantry_x
     return can_nodeid_gantry_x_bootloader;
--- include/bootloader/core/node_id.h
+++ include/bootloader/core/node_id.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include "bootloader/core/ids.h"
 
 #ifdef __cplusplus
@@ -13,6 +14,12 @@ extern "C" {
  */
 CANNodeId get_node_id(void);
 
+/**
+ * Get the node id for a pipette from the ADC output value from reading
+ * its mount interface. Implemented in core, but only relevant in
+ * the pipettes.
+ * */
+CANNodeId determine_pipette_node_id(uint16_t sense_voltage_mv);
 #ifdef __cplusplus
 }  // extern "C"
 #endif  // __cplusplus
