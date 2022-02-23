9190fb9e3d66426a58fc54872a3a7303b3f2a1f7
Caila Marashaj <caila.marashaj@opentrons.com>
2022-02-23 10:49:18 -0500

    resolved template issue

--- head/core/can_task.cpp
+++ head/core/can_task.cpp
@@ -26,9 +26,9 @@ using MotorDispatchTarget = can_dispatch::DispatchParseTarget<
     can_messages::WriteMotorDriverRegister>;
 using MoveGroupDispatchTarget = can_dispatch::DispatchParseTarget<
     move_group_handler::MoveGroupHandler<head_tasks::MotorQueueClient>,
-    can_messages::AddLinearMoveRequest, can_messages::HomeRequest,
-    can_messages::ClearAllMoveGroupsRequest,
-    can_messages::ExecuteMoveGroupRequest, can_messages::GetMoveGroupRequest>;
+    can_messages::AddLinearMoveRequest, can_messages::ClearAllMoveGroupsRequest,
+    can_messages::ExecuteMoveGroupRequest, can_messages::GetMoveGroupRequest,
+    can_messages::HomeRequest>;
 using MotionControllerDispatchTarget = can_dispatch::DispatchParseTarget<
     motion_message_handler::MotionHandler<head_tasks::MotorQueueClient>,
     can_messages::DisableMotorRequest, can_messages::EnableMotorRequest,
