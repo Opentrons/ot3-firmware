047c5afa6eeea14e7981ac8de0a7e2f1ef83c7c6
amit lissack <amit@opentrons.com>
2022-02-24 10:05:57 -0500

    Add crc32 module.

---
+++ include/bootloader/firmware/crc32.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdint.h>
+
+/**
+ * Initialize crc module.
+ */
+void crc32_init();
+
+/**
+ * Compute the CRC
+ * @param data Data
+ * @param length Length of data
+ * @return Computed CRC
+ */
+uint32_t crc32_compute(uint8_t* data, uint8_t length);
+
+/**
+ * Continue accumulating CRC using provided data.
+ * @param data Data
+ * @param length Length of data
+ * @return Accumulated CRC
+ */
+uint32_t crc32_accumulate(uint8_t* data, uint8_t length);
+
+/**
+ * Reset the accumulated CRC value.
+ */
+void crc32_reset_accumulator();
