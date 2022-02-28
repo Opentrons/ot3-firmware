22b1b481bc32585684c9332d2a42872f614574de
amit lissack <amit@opentrons.com>
2022-02-28 14:41:17 -0500

    delete em

--- include/common/firmware/iwdg.hpp
+++ include/common/firmware/iwdg.hpp
@@ -12,6 +12,12 @@ struct TaskEntry {
 class IndependentWatchDog {
   public:
     IndependentWatchDog();
+    IndependentWatchDog(const IndependentWatchDog&) = delete;
+    IndependentWatchDog(const IndependentWatchDog&&) = delete;
+    auto operator=(const IndependentWatchDog&) -> IndependentWatchDog& = delete;
+    auto operator=(const IndependentWatchDog&&)
+        -> IndependentWatchDog& = delete;
+    ~IndependentWatchDog() = default;
 
     /**
      * Start the task.
