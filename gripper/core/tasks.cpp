3aab3d0b6dfa20bef71c557ed5251905bed8eacc
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-03-07 18:38:46 -0500

    add gripper tasks

---
+++ gripper/core/tasks.cpp
@@ -0,0 +1,84 @@
+#include "gripper/core/tasks.hpp"
+
+#include "gripper/core/can_task.hpp"
+#include "motor-control/core/tasks/motion_controller_task_starter.hpp"
+#include "motor-control/core/tasks/motor_driver_task_starter.hpp"
+#include "motor-control/core/tasks/move_group_task_starter.hpp"
+#include "motor-control/core/tasks/move_status_reporter_task_starter.hpp"
+
+static auto tasks = gripper_tasks::AllTask{};
+static auto queues = gripper_tasks::QueueClient{utils::get_node_id()};
+
+static auto mc_task_builder =
+    motion_controller_task_starter::TaskStarter<lms::BeltConfig, 512,
+                                                gripper_tasks::QueueClient>{};
+static auto motor_driver_task_builder =
+    motor_driver_task_starter::TaskStarter<512, gripper_tasks::QueueClient>{};
+static auto move_group_task_builder =
+    move_group_task_starter::TaskStarter<512, gripper_tasks::QueueClient,
+                                         gripper_tasks::QueueClient>{};
+static auto move_status_task_builder =
+    move_status_reporter_task_starter::TaskStarter<
+        512, gripper_tasks::QueueClient>{};
+
+/**
+ * Start gripper tasks.
+ */
+void gripper_tasks::start_tasks(
+    can_bus::CanBus& can_bus,
+    motion_controller::MotionController<lms::BeltConfig>& motion_controller,
+    motor_driver::MotorDriver& motor_driver) {
+    auto& can_writer = can_task::start_writer(can_bus);
+    can_task::start_reader(can_bus);
+    auto& motion = mc_task_builder.start(5, motion_controller, queues);
+    auto& motor = motor_driver_task_builder.start(5, motor_driver, queues);
+    auto& move_group = move_group_task_builder.start(5, queues, queues);
+    auto& move_status_reporter = move_status_task_builder.start(5, queues);
+
+    tasks.can_writer = &can_writer;
+    tasks.motion_controller = &motion;
+    tasks.motor_driver = &motor;
+    tasks.move_group = &move_group;
+    tasks.move_status_reporter = &move_status_reporter;
+
+    queues.motion_queue = &motion.get_queue();
+    queues.motor_queue = &motor.get_queue();
+    queues.move_group_queue = &move_group.get_queue();
+    queues.set_queue(&can_writer.get_queue());
+    queues.move_status_report_queue = &move_status_reporter.get_queue();
+}
+
+gripper_tasks::QueueClient::QueueClient(can_ids::NodeId this_fw)
+    : can_message_writer::MessageWriter{this_fw} {}
+
+void gripper_tasks::QueueClient::send_motion_controller_queue(
+    const motion_controller_task::TaskMessage& m) {
+    motion_queue->try_write(m);
+}
+
+void gripper_tasks::QueueClient::send_motor_driver_queue(
+    const motor_driver_task::TaskMessage& m) {
+    motor_queue->try_write(m);
+}
+
+void gripper_tasks::QueueClient::send_move_group_queue(
+    const move_group_task::TaskMessage& m) {
+    move_group_queue->try_write(m);
+}
+
+void gripper_tasks::QueueClient::send_move_status_reporter_queue(
+    const move_status_reporter_task::TaskMessage& m) {
+    static_cast<void>(move_status_report_queue->try_write_isr(m));
+}
+
+/**
+ * Access to the tasks singleton
+ * @return
+ */
+auto gripper_tasks::get_tasks() -> AllTask& { return tasks; }
+
+/**
+ * Access to the queues singleton
+ * @return
+ */
+auto gripper_tasks::get_queues() -> QueueClient& { return queues; }
---
+++ include/gripper/core/tasks.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include "can/core/can_writer_task.hpp"
+#include "can/core/ids.hpp"
+#include "can/core/message_writer.hpp"
+#include "motor-control/core/linear_motion_system.hpp"
+#include "motor-control/core/tasks/motion_controller_task.hpp"
+#include "motor-control/core/tasks/motor_driver_task.hpp"
+#include "motor-control/core/tasks/move_group_task.hpp"
+#include "motor-control/core/tasks/move_status_reporter_task.hpp"
+
+namespace gripper_tasks {
+
+/**
+ * Start gripper tasks.
+ */
+void start_tasks(
+    can_bus::CanBus& can_bus,
+    motion_controller::MotionController<lms::BeltConfig>& motion_controller,
+    motor_driver::MotorDriver& motor_driver);
+
+/**
+ * Access to all the message queues in the system.
+ */
+struct QueueClient : can_message_writer::MessageWriter {
+    QueueClient(can_ids::NodeId this_fw);
+
+    void send_motion_controller_queue(
+        const motion_controller_task::TaskMessage& m);
+
+    void send_motor_driver_queue(const motor_driver_task::TaskMessage& m);
+
+    void send_move_group_queue(const move_group_task::TaskMessage& m);
+
+    void send_move_status_reporter_queue(
+        const move_status_reporter_task::TaskMessage& m);
+
+    freertos_message_queue::FreeRTOSMessageQueue<
+        motion_controller_task::TaskMessage>* motion_queue{nullptr};
+    freertos_message_queue::FreeRTOSMessageQueue<
+        motor_driver_task::TaskMessage>* motor_queue{nullptr};
+    freertos_message_queue::FreeRTOSMessageQueue<move_group_task::TaskMessage>*
+        move_group_queue{nullptr};
+    freertos_message_queue::FreeRTOSMessageQueue<
+        move_status_reporter_task::TaskMessage>* move_status_report_queue{
+        nullptr};
+};
+
+/**
+ * Access to all tasks in the system.
+ */
+struct AllTask {
+    message_writer_task::MessageWriterTask<
+        freertos_message_queue::FreeRTOSMessageQueue>* can_writer{nullptr};
+    motor_driver_task::MotorDriverTask<
+        freertos_message_queue::FreeRTOSMessageQueue, QueueClient>*
+        motor_driver{nullptr};
+    motion_controller_task::MotionControllerTask<
+        freertos_message_queue::FreeRTOSMessageQueue, lms::BeltConfig,
+        QueueClient>* motion_controller{nullptr};
+    move_status_reporter_task::MoveStatusReporterTask<
+        freertos_message_queue::FreeRTOSMessageQueue, QueueClient>*
+        move_status_reporter{nullptr};
+    move_group_task::MoveGroupTask<freertos_message_queue::FreeRTOSMessageQueue,
+                                   QueueClient, QueueClient>* move_group{
+        nullptr};
+};
+
+/**
+ * Access to the tasks singleton
+ * @return
+ */
+[[nodiscard]] auto get_tasks() -> AllTask&;
+
+/**
+ * Access to the queues singleton
+ * @return
+ */
+[[nodiscard]] auto get_queues() -> QueueClient&;
+
+}  // namespace gripper_tasks
