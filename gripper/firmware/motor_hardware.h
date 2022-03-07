ad8eede3c429c0d2554ce10415844d326c0845d0
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-03-07 17:41:31 -0500

    add firmware files

---
+++ gripper/firmware/motor_hardware.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "gantry/core/axis_type.h"
+#include "common/firmware/errors.h"
+#include "platform_specific_hal_conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+extern SPI_HandleTypeDef hspi2;
+extern TIM_HandleTypeDef htim7;
+
+typedef void (*motor_interrupt_callback)();
+
+HAL_StatusTypeDef initialize_spi(enum GantryAxisType);
+void gantry_driver_CLK_init(enum GantryAxisType);
+
+void initialize_timer(motor_interrupt_callback callback);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
