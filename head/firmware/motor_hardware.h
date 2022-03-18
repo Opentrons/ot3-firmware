da58490dfd4f9ee651e3c151964db0b39bd04a4c
carlos-fernandez <fernandez.carlos40@gmail.com>
2022-03-18 15:41:25 -0400

    made pulse_counter extern

--- head/firmware/motor_hardware.h
+++ head/firmware/motor_hardware.h
@@ -11,6 +11,7 @@ extern SPI_HandleTypeDef hspi2;
 extern TIM_HandleTypeDef htim7;
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
+extern uint32_t enc_pulse_counter;
 
 typedef void (*motor_interrupt_callback)();
 HAL_StatusTypeDef initialize_spi(SPI_HandleTypeDef* hspi);
