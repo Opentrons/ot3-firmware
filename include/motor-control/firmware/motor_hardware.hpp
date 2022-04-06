35558b404309238dd1f048e8cae2af29823a78e0
carlos-fernandez <fernandez.carlos40@gmail.com>
2022-04-06 09:50:11 -0400

    fix type return

--- include/motor-control/firmware/motor_hardware.hpp
+++ include/motor-control/firmware/motor_hardware.hpp
@@ -37,7 +37,7 @@ class MotorHardware : public StepperMotorHardwareIface {
     auto check_limit_switch() -> bool final;
     void set_LED(bool status) final;
     auto check_sync_in() -> bool final;
-    auto get_encoder_pulses() final;
+    auto get_encoder_pulses() -> uint32_t final;
     void reset_encoder_pulses() final;
 
   private:
