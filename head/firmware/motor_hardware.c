b761f8c0b27bcf80121d6ccd522d8b24da59c3be
carlos-fernandez <fernandez.carlos40@gmail.com>
2022-04-08 16:10:21 -0400

    removed this GPIO setup
    this pin doesn't belong to LD2_Pin.
    GPIO_PIN_5 is linked to the Encoder index of A.

--- head/firmware/motor_hardware.c
+++ head/firmware/motor_hardware.c
@@ -234,14 +234,6 @@ void MX_GPIO_Init(void) {
     __HAL_RCC_GPIOF_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    /*Configure GPIO pin : LD2_Pin */
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_5;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
 
