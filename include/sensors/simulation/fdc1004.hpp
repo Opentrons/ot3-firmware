1f86848093c1621400abb63d64b573358dfdf3c4
Caila Marashaj <caila.marashaj@opentrons.com>
2022-04-26 14:13:01 -0400

    format

--- include/sensors/simulation/fdc1004.hpp
+++ include/sensors/simulation/fdc1004.hpp
@@ -10,12 +10,11 @@ class FDC1004 : public sensor_simulator::SensorType {
     FDC1004() {
         DEVICE_ID = fdc1004::DEVICE_ID;
         ADDRESS = fdc1004::ADDRESS;
-        REGISTER_MAP = {
-            {fdc1004::CONFIGURATION_MEASUREMENT, 0},
-            {fdc1004::FDC_CONFIGURATION, 0},
-            {fdc1004::MSB_MEASUREMENT_1, 1},
-            {fdc1004::LSB_MEASUREMENT_1, 200},
-            {fdc1004::DEVICE_ID_REGISTER, fdc1004::DEVICE_ID}};
+        REGISTER_MAP = {{fdc1004::CONFIGURATION_MEASUREMENT, 0},
+                        {fdc1004::FDC_CONFIGURATION, 0},
+                        {fdc1004::MSB_MEASUREMENT_1, 1},
+                        {fdc1004::LSB_MEASUREMENT_1, 200},
+                        {fdc1004::DEVICE_ID_REGISTER, fdc1004::DEVICE_ID}};
     }
 };
 };  // namespace fdc1004_simulator
