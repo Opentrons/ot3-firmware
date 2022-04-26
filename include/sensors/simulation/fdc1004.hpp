940059b905fd28475920522d7c7d7faa63812e47
Caila Marashaj <caila.marashaj@opentrons.com>
2022-04-26 11:33:17 -0400

    resolving lint error

--- include/sensors/simulation/fdc1004.hpp
+++ include/sensors/simulation/fdc1004.hpp
@@ -8,14 +8,14 @@ using namespace sensors;
 class FDC1004 : public sensor_simulator::SensorType {
   public:
     FDC1004() {
-        DEVICE_ID = fdc1004_utils::DEVICE_ID;
-        ADDRESS = fdc1004_utils::ADDRESS;
+        DEVICE_ID = fdc1004::DEVICE_ID;
+        ADDRESS = fdc1004::ADDRESS;
         REGISTER_MAP = {
-            {fdc1004_utils::CONFIGURATION_MEASUREMENT, 0},
-            {fdc1004_utils::FDC_CONFIGURATION, 0},
-            {fdc1004_utils::MSB_MEASUREMENT_1, 1},
-            {fdc1004_utils::LSB_MEASUREMENT_1, 200},
-            {fdc1004_utils::DEVICE_ID_REGISTER, fdc1004_utils::DEVICE_ID}};
+            {fdc1004::CONFIGURATION_MEASUREMENT, 0},
+            {fdc1004::FDC_CONFIGURATION, 0},
+            {fdc1004::MSB_MEASUREMENT_1, 1},
+            {fdc1004::LSB_MEASUREMENT_1, 200},
+            {fdc1004::DEVICE_ID_REGISTER, fdc1004::DEVICE_ID}};
     }
 };
 };  // namespace fdc1004_simulator
--- include/sensors/simulation/hdc2080.hpp
+++ include/sensors/simulation/hdc2080.hpp
@@ -8,15 +8,15 @@ using namespace sensors;
 class HDC2080 : public sensor_simulator::SensorType {
   public:
     HDC2080() {
-        DEVICE_ID = hdc2080_utils::DEVICE_ID;
-        ADDRESS = hdc2080_utils::ADDRESS;
-        REGISTER_MAP = {{hdc2080_utils::INTERRUPT_REGISTER, 0},
-                        {hdc2080_utils::DRDY_CONFIG, 0},
-                        {hdc2080_utils::MEASURE_REGISTER, 0},
-                        {hdc2080_utils::LSB_TEMPERATURE_REGISTER, 25000},
-                        {hdc2080_utils::LSB_HUMIDITY_REGISTER, 55000},
-                        {hdc2080_utils::MSB_TEMPERATURE_REGISTER, 50000},
-                        {hdc2080_utils::MSB_HUMIDITY_REGISTER, 40000}};
+        DEVICE_ID = hdc2080::DEVICE_ID;
+        ADDRESS = hdc2080::ADDRESS;
+        REGISTER_MAP = {{hdc2080::INTERRUPT_REGISTER, 0},
+                        {hdc2080::DRDY_CONFIG, 0},
+                        {hdc2080::MEASURE_REGISTER, 0},
+                        {hdc2080::LSB_TEMPERATURE_REGISTER, 25000},
+                        {hdc2080::LSB_HUMIDITY_REGISTER, 55000},
+                        {hdc2080::MSB_TEMPERATURE_REGISTER, 50000},
+                        {hdc2080::MSB_HUMIDITY_REGISTER, 40000}};
     }
 };
 };  // namespace hdc2080_simulator
