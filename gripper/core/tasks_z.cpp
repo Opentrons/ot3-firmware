263c76907950be917ebdfebe072ca371ba4177e3
Seth Foster <seth@opentrons.com>
2022-04-29 10:31:18 -0400

    fix(gripper): Initialize AllTask by reference
    The AllTask object that gets handles on all the different tasks was
    passed into the relevant task starting functions by value, which meant
    it was copied. That meant that nothign that happened inside
    g_tasks::start_task or z_tasks::start_task actually had any effect on
    the rest of the system.
    
    This was by and large probably fine since there aren't actually many
    places where tasks get other tasks queues from the central (rather than
    being initialized with them), and AllTask has no lifetime effects
    because it only holds non-owning refs, but it might fix some weird
    gripper issues.

--- gripper/core/tasks_z.cpp
+++ gripper/core/tasks_z.cpp
@@ -33,7 +33,7 @@ static auto move_status_task_builder = freertos_task::TaskStarter<
 void z_tasks::start_task(motor_class::Motor<lms::LeadScrewConfig>& z_motor,
                          spi::hardware::SpiDeviceBase& spi_device,
                          tmc2130::configs::TMC2130DriverConfig& driver_configs,
-                         AllTask gripper_tasks) {
+                         AllTask& gripper_tasks) {
     auto& motion =
         mc_task_builder.start(5, "z mc", z_motor.motion_controller, z_queues);
     auto& move_group =
