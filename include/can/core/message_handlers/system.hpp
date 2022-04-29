d5a8ca698f3e76ffc6eeba93ce95784288d5ea7e
Seth Foster <seth@opentrons.com>
2022-04-29 10:35:24 -0400

    fix(can): send version flags properly
    We were accidentally assigning the version to both the version and the
    flags. Caught by -Wunused-parameter.

--- include/can/core/message_handlers/system.hpp
+++ include/can/core/message_handlers/system.hpp
@@ -28,7 +28,7 @@ class SystemMessageHandler {
      */
     SystemMessageHandler(CanClient &writer, uint32_t version, uint32_t flags,
                          std::span<const char> version_sha)
-        : writer(writer), response{.version = version, .flags = version} {
+        : writer(writer), response{.version = version, .flags = flags} {
         std::copy_n(version_sha.begin(),
                     std::min(version_sha.size(), response.shortsha.size()),
                     response.shortsha.begin());
