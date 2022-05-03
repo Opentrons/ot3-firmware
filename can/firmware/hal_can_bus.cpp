41b622147683b9f2b79aa5b685c1798f16e92a3c
Seth Foster <seth@opentrons.com>
2022-05-03 17:34:26 -0400

    feat(can): LED can mirror protocol state
    When the CANbus peripheral goes into an error state, we may be able to
    try and send messages upstream, but they may not get through since the
    peripheral is in an error state. What we can do, though, is make the LED
    blink at different rates. This commit therefore
    
    - Adds a timer to the canbus instance that is used to drive LEDs
    - Adds interrupt callbacks through to the can bus cpp that handle FDCAN
      protocol level errors and end up changing the rate the LED flashes
    - Add new ctors to specify LEDs, and a start() method that calls the c
      can_start() so that it can also start the timer at an appropriate time

--- can/firmware/hal_can_bus.cpp
+++ can/firmware/hal_can_bus.cpp
@@ -1,6 +1,43 @@
 #include "can/firmware/hal_can_bus.hpp"
 
+#include "common/firmware/gpio.hpp"
+
 using namespace hal_can_bus;
+
+void HalCanBus::hal_state_callback(void* data, hal_can_state_t state) {
+    if (data == nullptr) {
+        return;
+    }
+    auto* slf = static_cast<HalCanBus*>(data);
+
+    if (slf->indicator.port == nullptr) {
+        return;
+    }
+    switch (state) {
+        case HAL_CAN_STATE_ERROR_PASSIVE:
+            slf->indicator_timer.update_period_from_isr(
+                BLINK_HALF_PERIOD_ERROR_PASSIVE_MS);
+            break;
+        case HAL_CAN_STATE_ERROR_ACTIVE:
+            slf->indicator_timer.update_period_from_isr(
+                BLINK_HALF_PERIOD_ERROR_ACTIVE_MS);
+            break;
+        case HAL_CAN_STATE_BUS_OFF:
+            slf->indicator_timer.stop_from_isr();
+            gpio::reset(slf->indicator);
+            break;
+    }
+}
+
+void HalCanBus::handle_timer() {
+    if (indicator.port != nullptr) {
+        if (gpio::is_set(indicator)) {
+            gpio::reset(indicator);
+        } else {
+            gpio::set(indicator);
+        }
+    }
+}
 
 void HalCanBus::set_incoming_message_callback(
     void* cb_data, IncomingMessageCallback callback) {
