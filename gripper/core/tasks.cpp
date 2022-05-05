63ae3eb053baf68e63371121c87f0c30c09a89ad
amit lissack <amit@opentrons.com>
2022-05-05 14:42:15 -0400

    gripper simulator builds

--- gripper/core/tasks.cpp
+++ gripper/core/tasks.cpp
@@ -14,6 +14,15 @@
 static auto tasks = gripper_tasks::AllTask{};
 static auto queues = gripper_tasks::QueueClient{can_ids::NodeId::gripper};
 
+static auto eeprom_task_builder =
+    freertos_task::TaskStarter<512, eeprom::task::EEPromTask>{};
+
+static auto i2c3_task_client =
+    i2c::writer::Writer<freertos_message_queue::FreeRTOSMessageQueue>();
+
+static auto i2c3_task_builder =
+    freertos_task::TaskStarter<512, i2c::tasks::I2CTask>{};
+
 /**
  * Start gripper tasks.
  */
@@ -21,11 +30,21 @@ void gripper_tasks::start_tasks(
     can_bus::CanBus& can_bus, motor_class::Motor<lms::LeadScrewConfig>& z_motor,
     brushed_motor::BrushedMotor& grip_motor,
     spi::hardware::SpiDeviceBase& spi_device,
-    tmc2130::configs::TMC2130DriverConfig& driver_configs) {
+    tmc2130::configs::TMC2130DriverConfig& driver_configs,
+    i2c::hardware::I2CDeviceBase& i2c3,
+    eeprom::write_protect::WriteProtectPin& eeprom_wp_pin) {
     auto& can_writer = can_task::start_writer(can_bus);
     can_task::start_reader(can_bus);
     tasks.can_writer = &can_writer;
     queues.set_queue(&can_writer.get_queue());
+
+    auto& i2c3_task = i2c3_task_builder.start(5, "i2c3", i2c3);
+    i2c3_task_client.set_queue(&i2c3_task.get_queue());
+    queues.i2c3_queue = &i2c3_task.get_queue();
+
+    auto& eeprom_task =
+        eeprom_task_builder.start(5, "eeprom", i2c3_task_client, eeprom_wp_pin);
+    tasks.eeprom_task = &eeprom_task;
 
     z_tasks::start_task(z_motor, spi_device, driver_configs, tasks);
 
