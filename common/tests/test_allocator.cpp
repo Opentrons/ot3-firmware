eb39a36fa2d245be332797a4e6b6b0cd34482106
amit lissack <amit@opentrons.com>
2022-05-09 13:58:26 -0400

    it compiles.

--- common/tests/test_allocator.cpp
+++ common/tests/test_allocator.cpp
@@ -5,12 +5,19 @@
 #include "common/core/allocator.hpp"
 
 TEST_CASE("Ordered Map Object") {
-    std::map<std::string, int, Allocator=PoolAllocator<200, 5>> task_map {};
+    auto constexpr max_entries=5;
+    std::map<std::string, int, std::less<std::string>, PoolAllocator<std::pair<const std::string, int>, max_entries>> subject{};
+    GIVEN("an entry added to the map") {
+        subject["a"] = 12;
+        THEN("it should be in the map") {
+            REQUIRE( subject["a"] == 12);
+        }
+    }
 }
 
 TEST_CASE("Unordered Map Object") {
 
-    std::unordered_map<std::string, int, Allocator=PoolAllocator<200, 5>> task_map {};
+//    std::unordered_map<std::string, int, Allocator=PoolAllocator<200, 5>> task_map {};
 }
 
 TEST_CASE("") {
--- include/common/core/allocator.hpp
+++ include/common/core/allocator.hpp
@@ -1,7 +1,6 @@
-#include <concepts>
-#include <memory>
+#include <array>
 
-#include "common/core/linkedlist.hpp"
+#include<variant>
 
 /*
  * A generic pool allocator class that can be used to statically allocate memory
@@ -11,72 +10,78 @@
  *
  *
  */
-
-template <typename TotalMemoryBlock, typename IndividualBlock>
-concept Allocator = requires(TotalMemoryBlock m, IndividualBlock b) {
-    std::is_integral<m>;
-    std::is_integral<b>;
-    {m / b} -> std::convertable_to<int>;
-};
+//
+//template <typename TotalMemoryBlock, typename IndividualBlock>
+//concept Allocator = requires(TotalMemoryBlock m, IndividualBlock b) {
+//    std::is_integral<m>;
+//    std::is_integral<b>;
+//    {m / b} -> std::convertable_to<int>;
+//};
 
 // should have type of object and max number of.
 // should have backing be an array and/or pointer to array. Do not use malloc!
 //only support allocating memory for one type of object at a time.
 
 // Use concept to check that individual size
-template<typename TotalMemoryBlock, typename IndividualBlock>
-requires Allocator<TotalMemoryBlock, IndividualBlock>
+template<typename Element, std::size_t MaxElements>
 class PoolAllocator {
   public:
-    PoolAllocator() memory(TotalMemoryBlock), chunks(IndividualBlock) : {
-        start_ptr = malloc(memory);
-        total_blocks = memory / chunks;
-        build();
+    using value_type = Element;
+    using size_type = std::size_t;
+    using difference_type = ptrdiff_t;
+    using pointer = Element*;
+    using const_pointer = const Element*;
+    using reference = Element&;
+    using const_reference = const Element&;
+
+    PoolAllocator() {
     }
     auto operator=(PoolAllocator&) -> PoolAllocator& = delete;
     auto operator=(PoolAllocator&&) -> PoolAllocator&& = delete;
     PoolAllocator(PoolAllocator&) = delete;
     PoolAllocator(PoolAllocator&&) = delete;
-    ~PoolAllocator() = free(start_ptr);
+    ~PoolAllocator() = default;
 
     void free_block(void *ptr) {
-        free(ptr);
+//        free(ptr);
     }
 
-    void build() {
-        // Create a linked-list with all free positions
-        for (int i = 0; i < total_blocks; ++i) {
-            std::size_t address = (std::size_t) start_ptr + i * block;
-            free_memory_list.push((Node *) address);
-        }
+//    void build() {
+//        // Create a linked-list with all free positions
+//        for (int i = 0; i < total_blocks; ++i) {
+//            std::size_t address = (std::size_t) start_ptr + i * block;
+//            free_memory_list.push((Node *) address);
+//        }
+//    }
+
+    void deallocate(void *, size_t) {
+//        mem_used -= block;
+//        free_memory_list.push((Node *) ptr);
     }
 
-    void deallocate(void * ptr, size_t size) {
-        mem_used -= block;
-        free_memory_list.push((Node *) ptr);
-    }
-
-    void * allocate(size_t size, const void * = 0) {
-        mem_used += block;
-        return free_memory_list.pop();
+    pointer allocate(size_t, const void * = 0) {
+        return nullptr;
 
     }
 
     void reset() {
-        mem_used = 0;
+//        mem_used = 0;
     }
 
-  private:
-    int memory;
-    int block;
-    int total_blocks;
-    int mem_used = 0;
-    void * start_ptr = nullptr;
+    template <class U>
+    struct rebind { typedef PoolAllocator<U, MaxElements> other; };
 
-    struct  LinkedListStruct{};
-    using Node = linkedlist::StackLinkedList<LinkedListStruct>::Node;
-    linkedlist::StackLinkedList<LinkedListStruct> free_memory_list;
-}
+  private:
+//    int memory;
+//    int block;
+//    int total_blocks;
+//    int mem_used = 0;
+//    void * start_ptr = nullptr;
+
+    using ElementType = std::variant<std::monostate, Element>;
+    using MemoryType = std::array<ElementType, MaxElements>;
+    MemoryType backing{};
+};
 
 
 //template<typename Alloc = std::allocator<void>>
