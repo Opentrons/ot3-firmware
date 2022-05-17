8ebde50cd22a9623a071cde6d28bd7df751f6bfb
amit lissack <amit@opentrons.com>
2022-05-17 16:34:32 -0400

    add addresses.

---
+++ include/eeprom/core/addresses.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "types.hpp"
+
+namespace eeprom {
+namespace addresses {
+
+constexpr types::data_length serial_number_length = 12;
+constexpr types::address serial_number_address_begin = 0;
+constexpr types::address serial_number_address_end = serial_number_address_begin + serial_number_length;
+
+
+}
+}
