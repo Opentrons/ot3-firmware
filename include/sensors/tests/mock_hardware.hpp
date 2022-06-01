cabfe34e1e16258a46986e40f4ada602cf872d94
Caila Marashaj <caila.marashaj@opentrons.com>
2022-05-31 21:47:35 -0400

    updated check_data in mock hardware

--- include/sensors/tests/mock_hardware.hpp
+++ include/sensors/tests/mock_hardware.hpp
@@ -12,14 +12,15 @@ class MockSensorHardware : public sensors::hardware::SensorHardwareBase {
         sync_state = false;
         sync_reset_calls++;
     }
-    auto check_data_ready() -> void override { return true; }
-
+    auto check_data_ready() -> void override { return data_ready; }
+    void change_data_ready_value(bool value)->void { data_ready = value; }
     auto get_sync_state_mock() const -> bool { return sync_state; }
     auto get_sync_set_calls() const -> uint32_t { return sync_set_calls; }
     auto get_sync_reset_calls() const -> uint32_t { return sync_reset_calls; }
 
   private:
     bool sync_state = false;
+    bool data_ready = false;
     uint32_t sync_set_calls = 0;
     uint32_t sync_reset_calls = 0;
 };
