6d861d8991dd617a3107a49ea254d3bdaf00a9e9
Caila Marashaj <caila.marashaj@opentrons.com>
2022-06-01 10:11:35 -0400

    added return val to simulator check_data_ready

--- include/sensors/simulation/hardware.hpp
+++ include/sensors/simulation/hardware.hpp
@@ -8,7 +8,11 @@ class SimulatedSensorHardware : public SensorHardwareBase {
   public:
     auto set_sync_out() -> void override {}
     auto reset_sync_out() -> void override {}
-    auto check_data_ready() -> bool override {}
+    auto check_data_ready() -> bool override { return data_ready; }
+    auto change_data_ready_val(bool val) -> void { data_ready = val; }
+
+  private:
+    bool data_ready = false;
 };
 };  // namespace hardware
 };  // namespace sensors
