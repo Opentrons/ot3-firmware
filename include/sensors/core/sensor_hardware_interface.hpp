92d949aed994272d9b81b6a6f7b85f767cd3a2d2
Caila Marashaj <caila.marashaj@opentrons.com>
2022-06-01 14:33:54 -0400

    moved include gpio.h to sensor_hardware_interface

--- include/sensors/core/sensor_hardware_interface.hpp
+++ include/sensors/core/sensor_hardware_interface.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "common/firmware/gpio.hpp"
+
 namespace sensors {
 namespace hardware {
 
--- include/sensors/firmware/sensor_hardware.hpp
+++ include/sensors/firmware/sensor_hardware.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "common/firmware/gpio.hpp"
 #include "sensors/core/sensor_hardware_interface.hpp"
 
 namespace sensors {
@@ -9,12 +8,11 @@ namespace hardware {
 class SensorHardware : public SensorHardwareBase {
   public:
     SensorHardware(SensorHardwareConfiguration hardware) : hardware(hardware) {}
-    auto set_sync() -> void override { gpio::set(hardware.sync_out); }
-    auto reset_sync() -> void override { gpio::reset(hardware.sync_out); }
-    auto check_data_ready() -> bool override {
-        return gpio::is_set(hardware.data_ready);
-    }
+    auto set_sync() -> void override;
+    auto reset_sync() -> void override;
+    auto check_data_ready() -> bool override;
     SensorHardwareConfiguration hardware;
 };
+
 };  // namespace hardware
 };  // namespace sensors
