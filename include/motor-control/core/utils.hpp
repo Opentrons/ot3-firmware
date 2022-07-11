e6b769c93599bd749862be053c1392f75fa1fa03
Seth Foster <seth@opentrons.com>
2022-07-11 16:06:10 -0400

    new fixed point handling for two signeds
    These have to be tag dispatched because sq0_31 is an alias for int32_t
    but they are very different. i wish we had unit types

--- include/motor-control/core/utils.hpp
+++ include/motor-control/core/utils.hpp
@@ -17,6 +17,10 @@ auto fixed_point_multiply(sq31_31 a, sq0_31 b) -> sq0_31;
 
 auto fixed_point_multiply(sq31_31 a, uint32_t b) -> uint32_t;
 
+struct radix_offset_0 {};
+
+auto fixed_point_multiply(sq31_31 a, int32_t b, radix_offset_0) -> int32_t;
+
 auto fixed_point_to_float(uint32_t data, int to_radix) -> float;
 
 auto signed_fixed_point_to_float(int32_t data, int to_radix) -> float;
