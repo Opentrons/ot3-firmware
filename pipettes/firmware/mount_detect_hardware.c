4781ca69a9a49238f549d7e96e31b17dcb668842
Laura Cox <31892318+Laura-Danielle@users.noreply.github.com>
2022-07-14 11:38:20 -0400

    refactor(pipettes): update the G4 pin values for low throughput pipettes (#409)

--- pipettes/firmware/mount_detect_hardware.c
+++ pipettes/firmware/mount_detect_hardware.c
@@ -48,7 +48,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc) {
 
         __HAL_RCC_GPIOB_CLK_ENABLE();
 
-        GPIO_InitStruct.Pin = GPIO_PIN_1;
+        GPIO_InitStruct.Pin = GPIO_PIN_0;
         GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
