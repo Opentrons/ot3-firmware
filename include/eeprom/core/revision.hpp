95058fa19a741626488fc59a72fa83ffe0958de8
Ryan Howard <ryan.howard@opentrons.com>
2022-07-15 15:44:02 -0400

    feature(eeprom): add revision accessor and tests

---
+++ include/eeprom/core/revision.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <array>
+
+#include "accessor.hpp"
+#include "addresses.hpp"
+#include "task.hpp"
+
+namespace eeprom {
+namespace revision {
+
+using RevisionType = std::array<uint8_t, addresses::revision_length>;
+
+/**
+ * Class that reads and writes serial numbers.
+ * @tparam EEPromTaskClient client of eeprom task
+ */
+template <task::TaskClient EEPromTaskClient>
+class RevisionAccessor
+    : public accessor::EEPromAccessor<EEPromTaskClient, RevisionType,
+                                      addresses::revision_address_begin,
+                                      addresses::revision_address_end,
+                                      addresses::revision_length> {
+  public:
+    explicit RevisionAccessor(
+        EEPromTaskClient& eeprom_client,
+        accessor::ReadListener<RevisionType>& listener)
+        : accessor::EEPromAccessor<
+              EEPromTaskClient, RevisionType,
+              addresses::revision_address_begin,
+              addresses::revision_address_end,
+              addresses::revision_length>::EEPromAccessor(eeprom_client,
+                                                               listener) {}
+};
+
+}  // namespace revision
+}  // namespace eeprom
