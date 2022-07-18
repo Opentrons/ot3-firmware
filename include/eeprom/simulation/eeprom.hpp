f1e426b32e95f17fa9b61a98777a20ce83ed193e
Laura Cox <laura@opentrons.com>
2022-07-18 14:53:18 -0400

    refactor(eeprom): temporarily reset eeprom simulation to handle on the 8 bit address
    I was unable to figure out why the eeprom on the python side cannot handle 16 bit addresses even
    though Amit added it before leaving. This is a temporary fix as obviously the simulator should be
    able to handle 16 bit addresses. I made a ticket to look into it.

--- include/eeprom/simulation/eeprom.hpp
+++ include/eeprom/simulation/eeprom.hpp
@@ -61,9 +61,9 @@ class EEProm : public I2CDeviceBase,
 
   private:
     std::array<uint8_t, static_cast<size_t>(
-                            hardware_iface::EEpromMemorySize::ST_16_KBYTE)>
+                            hardware_iface::EEpromMemorySize::MICROCHIP_256_BYTE)>
         backing{};
-    uint16_t current_address{0};
+    uint8_t current_address{0};
     bool write_protected{true};
 };
 
