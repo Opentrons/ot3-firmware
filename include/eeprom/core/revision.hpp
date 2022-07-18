56c115d2963a2b89ce74b73dc558c3cc71a7872f
Ryan Howard <ryan.howard@opentrons.com>
2022-07-18 09:20:35 -0400

    fix(eeprom): format

--- include/eeprom/core/revision.hpp
+++ include/eeprom/core/revision.hpp
@@ -17,20 +17,17 @@ using RevisionType = std::array<uint8_t, addresses::revision_length>;
  */
 template <task::TaskClient EEPromTaskClient>
 class RevisionAccessor
-    : public accessor::EEPromAccessor<EEPromTaskClient, RevisionType,
-                                      addresses::revision_address_begin,
-                                      addresses::revision_address_end,
-                                      addresses::revision_length> {
+    : public accessor::EEPromAccessor<
+          EEPromTaskClient, RevisionType, addresses::revision_address_begin,
+          addresses::revision_address_end, addresses::revision_length> {
   public:
-    explicit RevisionAccessor(
-        EEPromTaskClient& eeprom_client,
-        accessor::ReadListener<RevisionType>& listener)
+    explicit RevisionAccessor(EEPromTaskClient& eeprom_client,
+                              accessor::ReadListener<RevisionType>& listener)
         : accessor::EEPromAccessor<
-              EEPromTaskClient, RevisionType,
-              addresses::revision_address_begin,
+              EEPromTaskClient, RevisionType, addresses::revision_address_begin,
               addresses::revision_address_end,
               addresses::revision_length>::EEPromAccessor(eeprom_client,
-                                                               listener) {}
+                                                          listener) {}
 };
 
 }  // namespace revision
