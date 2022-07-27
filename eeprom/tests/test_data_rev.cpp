f40fe849ba8b47529a1f01d932ba762447d48813
Ryan Howard <ryan.howard@opentrons.com>
2022-07-27 11:06:16 -0400

    feature(eeprom): add an accessor for data revision and associated tests

---
+++ eeprom/tests/test_data_rev.cpp
@@ -0,0 +1,142 @@
+#include <vector>
+
+#include "catch2/catch.hpp"
+#include "eeprom/core/accessor.hpp"
+#include "eeprom/core/data_rev.hpp"
+#include "eeprom/tests/mock_eeprom_task_client.hpp"
+
+using namespace eeprom;
+
+struct DV_MockListener
+    : accessor::ReadListener<data_revision::DataRevisionType> {
+    void on_read(const data_revision::DataRevisionType& data_rev) {
+        this->data_rev = data_rev;
+        call_count++;
+    }
+    data_revision::DataRevisionType data_rev{};
+    int call_count{0};
+};
+
+SCENARIO("Writing data revision") {
+    auto queue_client = MockEEPromTaskClient{};
+    auto read_listener = DV_MockListener{};
+    auto subject = data_revision::DataRevAccessor{queue_client, read_listener};
+
+    GIVEN("A data revision to write") {
+        auto data = data_revision::DataRevisionType{1, 2};
+
+        WHEN("the writing data revision") {
+            subject.write(data);
+
+            THEN("there is an eeprom write") {
+                REQUIRE(
+                    queue_client.messages.size() ==
+                    (addresses::data_revision_length / types::max_data_length) +
+                        (addresses::data_revision_length %
+                             types::max_data_length !=
+                         0));
+                message::WriteEepromMessage write_message;
+                types::data_length expected_bytes;
+                for (ulong i = 0; i < queue_client.messages.size(); i++) {
+                    expected_bytes =
+                        ((addresses::data_revision_length -
+                          (i * types::max_data_length)) > types::max_data_length
+                             ? types::max_data_length
+                             : addresses::data_revision_length %
+                                   types::max_data_length);
+
+                    write_message = std::get<message::WriteEepromMessage>(
+                        queue_client.messages[i]);
+                    REQUIRE(write_message.memory_address ==
+                            addresses::data_revision_address_begin +
+                                (i * types::max_data_length));
+
+                    REQUIRE(write_message.length == expected_bytes);
+                    REQUIRE(std::equal(write_message.data.begin(),
+                                       &(write_message.data[expected_bytes]),
+                                       &(data[i * types::max_data_length])));
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Reading data revision") {
+    auto queue_client = MockEEPromTaskClient{};
+    auto read_listener = DV_MockListener{};
+    auto subject = data_revision::DataRevAccessor{queue_client, read_listener};
+
+    GIVEN("A request to read the data revision") {
+        WHEN("reading the data revision") {
+            subject.start_read();
+
+            THEN("there is an eeprom read") {
+                REQUIRE(
+                    queue_client.messages.size() ==
+                    (addresses::data_revision_length / types::max_data_length) +
+                        (addresses::data_revision_length %
+                             types::max_data_length !=
+                         0));
+
+                types::data_length expected_bytes;
+                message::ReadEepromMessage read_message;
+                for (ulong i = 0; i < queue_client.messages.size(); i++) {
+                    expected_bytes =
+                        ((addresses::data_revision_length -
+                          (i * types::max_data_length)) > types::max_data_length
+                             ? types::max_data_length
+                             : addresses::data_revision_length %
+                                   types::max_data_length);
+
+                    read_message = std::get<message::ReadEepromMessage>(
+                        queue_client.messages[i]);
+
+                    REQUIRE(read_message.memory_address ==
+                            addresses::data_revision_address_begin +
+                                (i * types::max_data_length));
+                    REQUIRE(read_message.length == expected_bytes);
+                    REQUIRE(read_message.callback_param == &subject);
+                }
+            }
+        }
+    }
+
+    GIVEN("A request to read the data revision") {
+        subject.start_read();
+
+        WHEN("the read completes") {
+            auto data_rev = data_revision::DataRevisionType{2, 4};
+
+            message::ReadEepromMessage read_message;
+            types::data_length num_bytes;
+
+            auto data = types::EepromData{};
+            auto expected_num_read_response =
+                (addresses::data_revision_length / types::max_data_length) +
+                (addresses::data_revision_length % types::max_data_length != 0);
+
+            for (auto i = 0; i < expected_num_read_response; i++) {
+                num_bytes =
+                    ((addresses::data_revision_length -
+                      (i * types::max_data_length)) > types::max_data_length
+                         ? types::max_data_length
+                         : addresses::data_revision_length %
+                               types::max_data_length);
+                read_message = std::get<message::ReadEepromMessage>(
+                    queue_client.messages[i]);
+
+                std::copy_n(data_rev.cbegin() + (i * types::max_data_length),
+                            num_bytes, data.begin());
+                read_message.callback(
+                    {.memory_address = read_message.memory_address,
+                     .length = num_bytes,
+                     .data = data},
+                    read_message.callback_param);
+            }
+            THEN("then the listener is called once") {
+                REQUIRE(read_listener.call_count == 1);
+                REQUIRE(read_listener.data_rev == data_rev);
+            }
+        }
+    }
+}
---
+++ include/eeprom/core/data_rev.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+
+#include "accessor.hpp"
+#include "addresses.hpp"
+#include "common/core/bit_utils.hpp"
+#include "task.hpp"
+
+namespace eeprom {
+namespace data_revision {
+
+using DataRevisionType = std::vector<uint8_t>;
+
+template <task::TaskClient EEPromTaskClient>
+class DataRevAccessor
+    : public accessor::EEPromAccessor<EEPromTaskClient, DataRevisionType,
+                                      addresses::data_revision_address_begin,
+                                      addresses::data_revision_length> {
+    using accessor::EEPromAccessor<
+        EEPromTaskClient, DataRevisionType,
+        addresses::data_revision_address_begin,
+        addresses::data_revision_length>::EEPromAccessor;
+
+  public:
+    explicit DataRevAccessor(EEPromTaskClient& eeprom_client,
+                             accessor::ReadListener<DataRevisionType>& listener)
+        : accessor::EEPromAccessor<
+              EEPromTaskClient, DataRevisionType,
+              addresses::data_revision_address_begin,
+              addresses::data_revision_length>::EEPromAccessor(eeprom_client,
+                                                               listener) {}
+};
+
+}  // namespace data_revision
+}  // namespace eeprom
