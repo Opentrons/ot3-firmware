f5691ae18f8b56ffe04ca0ecbdfd6811e18a4a02
Laura Cox <laura@opentrons.com>
2022-07-27 18:46:40 -0400

    fix simulation

--- include/sensors/simulation/hdc3020.hpp
+++ include/sensors/simulation/hdc3020.hpp
@@ -8,17 +8,16 @@ namespace hdc3020_simulator {
 using namespace sensors;
 using namespace i2c::hardware;
 
-class HDC3020 : public I2CRegisterMap<uint8_t, uint16_t> {
+class HDC3020 : public I2CRegisterMap<uint8_t, uint64_t> {
   public:
     HDC3020()
         : I2CRegisterMap(hdc3020::ADDRESS,
-                         {{hdc3020::TRIGGER_ON_DEMAND_MODE, 0xAABB},
-                          {hdc3020::AUTO_MEASURE_1M2S, 0xAABB},
-                          {hdc3020::AUTO_MEASURE_1M1S, 0xAABB},
-                          {hdc3020::AUTO_MEASURE_2M1S, 0xAABB},
-                          {hdc3020::AUTO_MEASURE_4M1S, 0xAABB},
-                          {hdc3020::AUTO_MEASURE_10M1S, 0xAABB},
-                          {hdc3020::READ_STATUS_REGISTER, 0xAABB},
-                          {hdc3020::MANUFACTURE_ID, 0}}) {}
+                         {{static_cast<uint8_t>(hdc3020::Registers::TRIGGER_ON_DEMAND_MODE), 0x209631509031},
+                          {static_cast<uint8_t>(hdc3020::Registers::AUTO_MEASURE_1M2S), 0x209631509031},
+                          {static_cast<uint8_t>(hdc3020::Registers::AUTO_MEASURE_1M1S), 0x209631509031},
+                          {static_cast<uint8_t>(hdc3020::Registers::AUTO_MEASURE_2M1S), 0x209631509031},
+                          {static_cast<uint8_t>(hdc3020::Registers::AUTO_MEASURE_4M1S), 0x209631509031},
+                          {static_cast<uint8_t>(hdc3020::Registers::AUTO_MEASURE_10M1S), 0x209631509031},
+                          {static_cast<uint8_t>(hdc3020::Registers::AUTO_MEASURE_STATUS), 0x0}}) {}
 };
 };  // namespace hdc3020_simulator
