2be68e9c749ef48c2f1176a19c2016f457cec941
Derek Maggio <derek.maggio@opentrons.com>
2022-08-03 07:34:09 -0700

    Change hardware back to dataclasses

--- ot3_state_manager/ot3_state_manager/hardware.py
+++ ot3_state_manager/ot3_state_manager/hardware.py
@@ -2,74 +2,73 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass, field
+
 from opentrons.hardware_control.types import OT3Axis
 
 from ot3_state_manager.measurable_states import Force, Position
 from ot3_state_manager.pipette_model import PipetteModel
 
 
+@dataclass
 class GantryX:
     """Gantry X Hardware."""
 
-    def __init__(self) -> None:
-        """Creates GantryX object."""
-        self.position = Position(axis=OT3Axis.X)
+    position: Position = field(default_factory=lambda: Position(axis=OT3Axis.X))
 
 
+@dataclass
 class GantryY:
     """Gantry Y Hardware."""
 
-    def __init__(self) -> None:
-        """Creates GantryY object."""
-        self.position = Position(axis=OT3Axis.Y)
+    position: Position = field(default_factory=lambda: Position(axis=OT3Axis.Y))
 
 
+@dataclass
 class Gripper:
     """Gripper Hardware."""
 
     # Gripper z is controlled by gripper board so it is being filed under gripper
-    def __init__(self) -> None:
-        """Creates Gripper object."""
-        self.gripper_z_position = Position(axis=OT3Axis.Z_G)
-        self.position = Position(axis=OT3Axis.G)
-        self.jaw_force = Force(axis=OT3Axis.G)
+    gripper_z_position: Position = field(
+        default_factory=lambda: Position(axis=OT3Axis.Z_G)
+    )
+    position: Position = field(default_factory=lambda: Position(axis=OT3Axis.G))
+    jaw_force: Force = field(default_factory=lambda: Force(axis=OT3Axis.G))
 
 
+@dataclass
 class LeftPipette:
     """Left Pipette Hardware."""
 
-    def __init__(self, model: PipetteModel) -> None:
-        """Creates LeftPipette object."""
-        self.model = model
-        self.position = Position(axis=OT3Axis.P_L)
+    model: PipetteModel
+    position: Position = field(default_factory=lambda: Position(axis=OT3Axis.P_L))
 
 
+@dataclass
 class RightPipette:
     """Right Pipette Hardware."""
 
-    def __init__(self, model: PipetteModel) -> None:
-        """Creates RightPipette object."""
-        self.model = model
-        self.position = Position(axis=OT3Axis.P_R)
+    model: PipetteModel
+    position: Position = field(default_factory=lambda: Position(axis=OT3Axis.P_R))
 
 
+@dataclass
 class Head:
     """OT3 Head Hardware."""
 
-    def __init__(self) -> None:
-        """Creates Head object."""
-        # Putting z control for pipettes under head because that is the board that
-        # actually controls it.
-        self.left_pipette_z_position = Position(axis=OT3Axis.Z_L)
-        self.right_pipette_z_position = Position(axis=OT3Axis.Z_R)
+    left_pipette_z_position: Position = field(
+        default_factory=lambda: Position(axis=OT3Axis.Z_L)
+    )
+    right_pipette_z_position: Position = field(
+        default_factory=lambda: Position(axis=OT3Axis.Z_R)
+    )
 
 
+@dataclass
 class SyncPin:
     """OT3 Sync Pin."""
 
-    def __init__(self) -> None:
-        """Creates SyncPin object."""
-        self.sync_pin = False
+    sync_pin: bool = field(default_factory=lambda: False)
 
     def set_sync_pin_high(self) -> None:
         """Sets sync pin high."""
