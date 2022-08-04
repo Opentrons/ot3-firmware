ef951e2ee85254ce5c129aec360c064fffee21ce
Derek Maggio <derek.maggio@opentrons.com>
2022-08-04 06:45:46 -0700

    Fix test_ot3_state_manager

--- ot3_state_manager/ot3_state_manager/ot3_state_manager.py
+++ ot3_state_manager/ot3_state_manager/ot3_state_manager.py
@@ -5,18 +5,17 @@ from __future__ import annotations
 import argparse
 import asyncio
 import logging
-from typing import Optional
+from asyncio import BaseProtocol
+from typing import Tuple, cast
 
-from opentrons.hardware_control.types import OT3Axis
-
+from ot3_state_manager.messages import handle_message
 from ot3_state_manager.ot3_state import OT3State
 from ot3_state_manager.pipette_model import PipetteModel
-from ot3_state_manager.messages import handle_message
 
 log = logging.getLogger(__name__)
 
 
-class OT3StateManager:
+class OT3StateManager(BaseProtocol):
     """Async socket server that accepts pulse messages."""
 
     def __init__(self, ot3_state: OT3State) -> None:
@@ -29,22 +28,40 @@ class OT3StateManager:
         """
         self._ot3_state = ot3_state
         self.transport = None
+        self._connected = False
 
-    def connection_made(self, transport):
-        print("Server: Connection Made")
+    def connection_made(self, transport) -> None:  # noqa: ANN001
+        """Called when connection is made from client."""
+        self._connected = True
         self.transport = transport
 
-    def datagram_received(self, data, addr):
-        print("Received from client: ", data)
-        self.transport.sendto(handle_message(data, self._ot3_state), addr)
-        self.transport = None
+    def datagram_received(self, data: bytes, addr: Tuple[str, str]) -> None:
+        """Called when a datagram is received from client."""
+        if self.transport is not None:
+            self.transport.sendto(handle_message(data, self._ot3_state), addr)
 
-    async def start_server(self, host, port) -> None:
+    async def start_server(self, host: str, port: int) -> OT3StateManager:
+        """Starts OT3StateManager UDP Datagram server"""
         loop = asyncio.get_running_loop()
-        await loop.create_datagram_endpoint(lambda: self, local_addr=(host, port))
+        transport, protocol = await loop.create_datagram_endpoint(
+            lambda: self,
+            local_addr=(host, port),
+        )
+        return cast(OT3StateManager, protocol)  # Casting for mypy
+
+    async def is_connected(self) -> bool:
+        """Flag for when a client is connected."""
+        return self._connected
+
+    async def close(self) -> None:
+        """Close existing socket connection."""
+        if self.transport is not None:
+            self.transport.close()
+        self._connected = False
 
 
-async def main():
+async def main() -> None:
+    """CLI interface."""
     LEFT_PIPETTE_DEST = "left_pipette_model_name"
     RIGHT_PIPETTE_DEST = "right_pipette_model_name"
     GRIPPER_DEST = "use_gripper"
--- ot3_state_manager/tests/test_ot3_state_manager.py
+++ ot3_state_manager/tests/test_ot3_state_manager.py
@@ -1,17 +1,15 @@
 """Test for OT3StateManager object."""
 
 import asyncio
-from typing import Generator
+from typing import AsyncGenerator, Generator
+from unittest.mock import Mock, patch
 
 import pytest
-from opentrons.hardware_control.types import OT3Axis
 
 from ot3_state_manager.ot3_state import OT3State
 from ot3_state_manager.ot3_state_manager import OT3StateManager
 from ot3_state_manager.pipette_model import PipetteModel
-from tests.udp_client import (
-    EchoClientProtocol,
-)
+from tests.udp_client import EchoClientProtocol
 
 HOST = "localhost"
 PORT = 8088
@@ -24,47 +22,47 @@ def ot3_state() -> OT3State:
         PipetteModel.SINGLE_20.value, PipetteModel.SINGLE_20.value, True
     )
 
+
 @pytest.fixture
-def loop(event_loop: asyncio.AbstractEventLoop) -> asyncio.AbstractEventLoop:
+def loop(
+    event_loop: asyncio.AbstractEventLoop,
+) -> Generator[asyncio.AbstractEventLoop, None, None]:
+    """Create and yield event_loop."""
     yield event_loop
 
+
 @pytest.fixture
-def server(loop: asyncio.AbstractEventLoop, ot3_state: OT3State) -> Generator:
+async def server(
+    loop: asyncio.AbstractEventLoop, ot3_state: OT3State
+) -> AsyncGenerator[OT3StateManager, None]:
     """Start OT3StateManager server."""
-    cancel_handle = asyncio.ensure_future(
-        OT3StateManager(ot3_state).start_server(HOST, PORT), loop=loop
-    )
-    loop.run_until_complete(asyncio.sleep(0.01))
+    server = await OT3StateManager(ot3_state).start_server(HOST, PORT)
+    await server.is_connected()
+    yield server
+    await server.close()
 
-    try:
-        yield
-    finally:
-        cancel_handle.cancel()
 
 @pytest.fixture
-async def client(loop: asyncio.AbstractEventLoop) -> EchoClientProtocol:
+async def client(
+    loop: asyncio.AbstractEventLoop,
+) -> AsyncGenerator[EchoClientProtocol, None]:
+    """Create UDP Client."""
     client = await EchoClientProtocol.build(HOST, PORT, loop)
     await client.is_connected()
     yield client
     await client.close()
 
 
-@pytest.mark.parametrize(
-    "message,expected_val",
-    (
-        pytest.param("+ X", 1, id="pos_pulse"),
-        pytest.param("- X", -1, id="neg_pulse"),
-    ),
-)
+@patch.object(OT3StateManager, "datagram_received")
 async def test_message_received(
-    message: str,
-    expected_val: int,
-    server: None,
+    patched_object: Mock,
+    server: OT3StateManager,
     client: EchoClientProtocol,
     loop: asyncio.AbstractEventLoop,
-    ot3_state: OT3State
+    ot3_state: OT3State,
 ) -> None:
     """Confirm that pulse messages work correctly."""
-    await asyncio.ensure_future(client.send_message(message), loop=loop)
-    print("Performing assert")
-    assert ot3_state.axis_current_position(OT3Axis.X) == expected_val
+    client.send_message("+ X")
+    await asyncio.wait_for(server.is_connected(), 15.0)
+    datagram_received_data_arg_content = patched_object.call_args.args[0]
+    assert datagram_received_data_arg_content == b"+ X"
