c53565d4de64be5df3032911c8cb31cbd3c33197
Seth Foster <seth@opentrons.com>
2022-08-15 17:46:59 -0400

    refactor(simulators): add command line options
    We had a bunch of options we could pass to the simulators but they were
    documented in source and docs and not in an inspectable way. It also
    makes it annoying to add more.
    
    Instead, or really in addition, we can use boost::program_options to
    parse the environment variables. We can also really easily use it to
    provide command line flags alongside them, and then we get a -h option
    that can document both.
    
    While we're at it, add a new option for the eeprom that specifies a file
    to back up the eeprom contents.

--- gripper/simulator/main.cpp
+++ gripper/simulator/main.cpp
@@ -1,6 +1,11 @@
 #include <signal.h>
 
+#include <iostream>
+#include <memory>
+#include <string>
+
 #include "FreeRTOS.h"
+#include "boost/program_options.hpp"
 #include "can/simlib/sim_canbus.hpp"
 #include "eeprom/simulation/eeprom.hpp"
 #include "gripper/core/interfaces.hpp"
@@ -10,6 +15,8 @@
 #include "sensors/simulation/mock_hardware.hpp"
 #include "task.h"
 
+namespace po = boost::program_options;
+
 void signal_handler(int signum) {
     LOG("Interrupt signal (%d) received.", signum);
     exit(signum);
@@ -18,33 +25,65 @@ void signal_handler(int signum) {
 /**
  * The CAN bus.
  */
-static auto canbus = can::sim::bus::SimCANBus(can::sim::transport::create());
 
 static auto capsensor = fdc1004_simulator::FDC1004{};
-static auto sim_eeprom = eeprom::simulator::EEProm{};
 static auto sensor_map =
     i2c::hardware::SimI2C::DeviceMap{{capsensor.get_address(), capsensor}};
 static auto i2c2 = i2c::hardware::SimI2C{sensor_map};
-static auto i2c_device_map =
-    i2c::hardware::SimI2C::DeviceMap{{sim_eeprom.get_address(), sim_eeprom}};
-static auto i2c3 = i2c::hardware::SimI2C{i2c_device_map};
 
 static test_mocks::MockSensorHardware fake_sensor_hw{};
 
-int main() {
+auto handle_options(int argc, char** argv) -> po::variables_map {
+    auto cmdlinedesc =
+        po::options_description("simulator for the OT-3 gripper");
+    auto envdesc = po::options_description("");
+    cmdlinedesc.add_options()("help,h", "Show this help message.");
+    auto can_arg_xform = can::sim::transport::add_options(cmdlinedesc, envdesc);
+    auto eeprom_arg_xform =
+        eeprom::simulator::EEProm::add_options(cmdlinedesc, envdesc);
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, cmdlinedesc), vm);
+    if (vm.count("help")) {
+        std::cout << cmdlinedesc << std::endl;
+        std::exit(0);
+    }
+    po::store(po::parse_environment(
+                  envdesc,
+                  [can_arg_xform, eeprom_arg_xform](
+                      const std::string& input_val) -> std::string {
+                      auto can_xformed = can_arg_xform(input_val);
+                      if (can_xformed != "") {
+                          return can_xformed;
+                      }
+                      auto eeprom_xformed = eeprom_arg_xform(input_val);
+                      return eeprom_xformed;
+                  }),
+              vm);
+    po::notify(vm);
+    return vm;
+}
+
+int main(int argc, char** argv) {
     signal(SIGINT, signal_handler);
 
     LOG_INIT("GRIPPER", []() -> const char* {
         return pcTaskGetName(xTaskGetCurrentTaskHandle());
     });
-
+    auto options = handle_options(argc, argv);
+    auto sim_eeprom = std::make_shared<eeprom::simulator::EEProm>(options);
+    auto i2c_device_map = i2c::hardware::SimI2C::DeviceMap{
+        {sim_eeprom->get_address(), *sim_eeprom}};
+    auto i2c3 = std::make_shared<i2c::hardware::SimI2C>(i2c_device_map);
+    static auto canbus =
+        can::sim::bus::SimCANBus(can::sim::transport::create(options));
     z_motor_iface::initialize();
     grip_motor_iface::initialize();
     gripper_tasks::start_tasks(canbus, z_motor_iface::get_z_motor(),
                                grip_motor_iface::get_grip_motor(),
                                z_motor_iface::get_spi(),
                                z_motor_iface::get_tmc2130_driver_configs(),
-                               i2c2, i2c3, fake_sensor_hw, sim_eeprom);
+                               i2c2, *i2c3, fake_sensor_hw, *sim_eeprom);
 
     vTaskStartScheduler();
 }
--- include/eeprom/simulation/eeprom.hpp
+++ include/eeprom/simulation/eeprom.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <array>
+#include <functional>
+#include <string>
 
+#include "boost/program_options.hpp"
 #include "common/core/bit_utils.hpp"
 #include "common/core/logging.h"
 #include "eeprom/core/hardware_iface.hpp"
@@ -12,19 +15,25 @@ namespace eeprom {
 namespace simulator {
 
 using namespace i2c::hardware;
+namespace po = boost::program_options;
 
 class EEProm : public I2CDeviceBase,
                public hardware_iface::EEPromHardwareIface {
   public:
-    EEProm() : I2CDeviceBase(types::DEVICE_ADDRESS) { backing.fill(0xFF); }
-    EEProm(hardware_iface::EEPromChipType chip)
-        : I2CDeviceBase(types::DEVICE_ADDRESS),
-          hardware_iface::EEPromHardwareIface(chip) {
-        backing.fill(0xFF);
+    static auto add_options(po::options_description& cmdline_desc,
+                            po::options_description& env_desc)
+        -> std::function<std::string(std::string)> {
+        return BackingStore::add_options(cmdline_desc, env_desc);
     }
+    explicit EEProm(po::variables_map& options)
+        : I2CDeviceBase(types::DEVICE_ADDRESS), backing(options) {}
+    EEProm(hardware_iface::EEPromChipType chip, po::variables_map& options)
+        : I2CDeviceBase(types::DEVICE_ADDRESS),
+          hardware_iface::EEPromHardwareIface(chip),
+          backing(options) {}
 
-    auto handle_write(const uint8_t *data, uint16_t size) -> bool {
-        auto *iter = data;
+    auto handle_write(const uint8_t* data, uint16_t size) -> bool {
+        auto* iter = data;
 
         // Read the address
         iter = bit_utils::bytes_to_int(
@@ -40,7 +49,8 @@ class EEProm : public I2CDeviceBase,
         if (data_size > 0) {
             if (!write_protected) {
                 // Let the exception happen. Catch errors!
-                std::copy_n(iter, data_size, backing.begin() + current_address);
+                std::copy_n(iter, data_size,
+                            backing.value().begin() + current_address);
                 LOG("Writing %d bytes to address %X", data_size,
                     current_address);
             } else {
@@ -53,10 +63,10 @@ class EEProm : public I2CDeviceBase,
         return true;
     }
 
-    auto handle_read(uint8_t *data, uint16_t size) -> bool {
+    auto handle_read(uint8_t* data, uint16_t size) -> bool {
         LOG("Reading %d bytes from address %X", size, current_address);
         for (auto i = 0; i < size; i++) {
-            data[i] = backing[current_address++];
+            data[i] = backing.value()[current_address++];
         }
         return true;
     }
@@ -67,10 +77,45 @@ class EEProm : public I2CDeviceBase,
         write_protected = enabled;
     }
 
+    struct BackingStore {
+        static constexpr std::string_view TEMPFILE_KEY = "<temp file>";
+        using BackingType =
+            std::array<uint8_t,
+                       static_cast<size_t>(
+                           hardware_iface::EEpromMemorySize::ST_16_KBYTE)>;
+        explicit BackingStore(const po::variables_map& variables) {
+            auto filename = variables["eeprom-filename"].as<std::string>();
+            LOG("Using eeprom backing store %s", filename.c_str());
+            backing.fill(0xff);
+        }
+        static auto add_options(po::options_description& cmdline,
+                                po::options_description& env)
+            -> std::function<std::string(std::string)> {
+            cmdline.add_options()(
+                "eeprom-filename,f",
+                po::value<std::string>()->default_value(
+                    std::string(TEMPFILE_KEY)),
+                "path to backing file for eeprom. if unspecified, a temp will "
+                "be used. May be specified in an environment file called "
+                "EEPROM_FILENAME.");
+            env.add_options()("EEPROM_FILENAME",
+                              po::value<std::string>()->default_value(
+                                  std::string(TEMPFILE_KEY)));
+            return [](std::string input_val) -> std::string {
+                if (input_val == "EEPROM_FILENAME") {
+                    return "eeprom-filename";
+                }
+                return "";
+            };
+        }
+        auto value() -> BackingType& { return backing; }
+
+      private:
+        BackingType backing{};
+    };
+
   private:
-    std::array<uint8_t, static_cast<size_t>(
-                            hardware_iface::EEpromMemorySize::ST_16_KBYTE)>
-        backing{};
+    BackingStore backing;
     types::address current_address{0};
     bool write_protected{true};
 };
