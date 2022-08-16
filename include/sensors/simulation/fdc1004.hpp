0d6ac46650daf6e005d4e4635583dd42129684aa
Seth Foster <seth@opentrons.com>
2022-08-16 17:01:42 -0400

    refactor(sensors): smaller data in cap sensor sim
    The simulator harness for the capacitive sensor returned numbers large
    enough that the capdac would be adjusted, but then the harness didn't
    take the altered capdac into account. That means the numbers returned on
    can would change over time. By reducing the values returned from the
    sensor, we fix the problem.

--- include/sensors/simulation/fdc1004.hpp
+++ include/sensors/simulation/fdc1004.hpp
@@ -13,8 +13,8 @@ class FDC1004 : public I2CRegisterMap<uint8_t, uint16_t> {
         : I2CRegisterMap(fdc1004::ADDRESS,
                          {{fdc1004::CONFIGURATION_MEASUREMENT, 0},
                           {fdc1004::FDC_CONFIGURATION, 0},
-                          {fdc1004::MSB_MEASUREMENT_1, 2000},
-                          {fdc1004::LSB_MEASUREMENT_1, 200},
+                          {fdc1004::MSB_MEASUREMENT_1, 100},
+                          {fdc1004::LSB_MEASUREMENT_1, 0},
                           {fdc1004::DEVICE_ID_REGISTER, fdc1004::DEVICE_ID}}) {}
 };
 
