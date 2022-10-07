fa2a22f9ebf33427f91622f503353f60bba8570d
Ryan Howard <ryan.howard@opentrons.com>
2022-10-07 12:16:37 -0400

    disable the index pin on rev 1 the same as proto

--- gripper/firmware/motor_encoder_hardware_rev1.c
+++ gripper/firmware/motor_encoder_hardware_rev1.c
@@ -35,7 +35,7 @@ void Encoder_GPIO_Init(void) {
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    /*Configure GPIO pin : PA5 */
+    /*Configure GPIO pin : PA5
     GPIO_InitStruct.Pin = GPIO_PIN_5;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -48,6 +48,7 @@ void Encoder_GPIO_Init(void) {
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    */
 }
 
 void TIM2_EncoderG_Init(void) {
