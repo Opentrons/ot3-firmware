2af744bc13454ac72ad237b9b99d5ca0b288526f
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2022-10-28 11:53:11 -0400

    fix(gantry): set up encoder for XY (#475)

--- gantry/firmware/motor_hardware.h
+++ gantry/firmware/motor_hardware.h
@@ -10,13 +10,15 @@ extern "C" {
 
 extern SPI_HandleTypeDef hspi2;
 extern TIM_HandleTypeDef htim7;
+extern TIM_HandleTypeDef htim2;
 
 typedef void (*motor_interrupt_callback)();
+typedef void (*encoder_overflow_callback)(int32_t);
 
 HAL_StatusTypeDef initialize_spi(enum GantryAxisType);
 void gantry_driver_CLK_init(enum GantryAxisType);
 
-void initialize_timer(motor_interrupt_callback callback);
+void initialize_timer(motor_interrupt_callback callback, encoder_overflow_callback enc_callback);
 
 #ifdef __cplusplus
 }  // extern "C"
