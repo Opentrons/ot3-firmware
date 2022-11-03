03d573b735860da8838f4a952a45e6e565503037
Frank Sinapi <frank.sinapi@opentrons.com>
2022-11-03 13:09:26 -0400

    Formatted state manager parser

--- include/common/simulation/state_manager_parser.hpp
+++ include/common/simulation/state_manager_parser.hpp
@@ -18,23 +18,21 @@ using RT = std::variant<std::monostate, SyncPinState>;
 template <typename Input, typename Limit>
 requires std::forward_iterator<Input> && std::sized_sentinel_for<Limit, Input>
 auto parse_state_manager_response(Input begin, Limit &end) -> RT {
-    if(begin == end) {
+    if (begin == end) {
         return RT();
     }
     uint8_t msg_type = *begin;
     std::advance(begin, 1);
-    if(begin == end) {
+    if (begin == end) {
         return RT();
     }
-    if(msg_type != static_cast<uint8_t>(MessageID::get_sync_pin_state)) {
+    if (msg_type != static_cast<uint8_t>(MessageID::get_sync_pin_state)) {
         // For now, we only parse sync pin state responses
         return RT();
     }
     // Read the rest of the message as ascii text - 0 or 1
     char msg_value = *begin;
-    return RT(
-        msg_value == '1' ? SyncPinState::HIGH : SyncPinState::LOW
-    );
+    return RT(msg_value == '1' ? SyncPinState::HIGH : SyncPinState::LOW);
 }
 
-};
+};  // namespace state_manager_parser
