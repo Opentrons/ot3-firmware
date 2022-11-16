e1228bff01c2696a96f92bd061fe33e54963a8cf
Frank Sinapi <frank.sinapi@opentrons.com>
2022-11-16 17:41:14 -0500

    Fix tests

--- motor-control/tests/test_motor_position_tracker.cpp
+++ motor-control/tests/test_motor_position_tracker.cpp
@@ -8,27 +8,17 @@ TEST_CASE("position tracker functionality") {
     GIVEN("a motor hardware interface") {
         test_mocks::MockMotorHardware hardware;
         THEN("the position tracker is initialized to 0") {
-            REQUIRE(hardware.get_position_tracker() == 0);
+            REQUIRE(hardware.get_step_tracker() == 0);
         }
-        WHEN("incrementing the position tracker") {
-            auto ret = hardware.increment_position_tracker(20);
-            THEN("the results make sense") {
-                REQUIRE(ret.first == 0);
-                REQUIRE(ret.second == 20);
-            }
+        WHEN("changing the position tracker") {
+            hardware.set_step_tracker(20);
             THEN("getting the position tracker returns the right value") {
-                REQUIRE(hardware.get_position_tracker() == 20);
-            }
-        }
-        WHEN("setting the position tracker") {
-            hardware.set_position_tracker(100);
-            THEN("it is updated") {
-                REQUIRE(hardware.get_position_tracker() == 100);
+                REQUIRE(hardware.get_step_tracker() == 20);
             }
             AND_THEN("resetting the position tracker") {
-                hardware.reset_position_tracker();
+                hardware.reset_step_tracker();
                 THEN("it is reset to 0") {
-                    REQUIRE(hardware.get_position_tracker() == 0);
+                    REQUIRE(hardware.get_step_tracker() == 0);
                 }
             }
         }
