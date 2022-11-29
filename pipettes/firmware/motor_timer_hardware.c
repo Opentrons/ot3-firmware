abc3dc34db5d0cddf97d0707239e1d8f4c2fadd0
Laura Cox <31892318+Laura-Danielle@users.noreply.github.com>
2022-11-29 18:01:20 -0500

    fix(pipettes): make pick up motor timer half the time of the plunger motor (#514)

--- pipettes/firmware/motor_timer_hardware.c
+++ pipettes/firmware/motor_timer_hardware.c
@@ -82,7 +82,7 @@ void MX_TIM6_Init(void) {
     TIM_MasterConfigTypeDef sMasterConfig = {0};
 
     htim6.Instance = TIM6;
-    htim6.Init.Prescaler = 499;
+    htim6.Init.Prescaler = 849;
     htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
     htim6.Init.Period = 1;
     htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
