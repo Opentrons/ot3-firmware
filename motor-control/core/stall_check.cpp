75f166fa8da780e28c40c5742a45d9e7286cc9fb
Frank Sinapi <frank.sinapi@opentrons.com>
2022-12-05 13:58:38 -0500

    Fixed incorrect usage of labs instead of llabs

--- motor-control/core/stall_check.cpp
+++ motor-control/core/stall_check.cpp
@@ -53,7 +53,7 @@ auto StallCheck::reset_itr_counts(int32_t stepper_steps) -> void {
     if (!has_encoder()) {
         return true;
     }
-    sq31_31 diff = std::labs(_encoder_ideal_counts -
+    sq31_31 diff = std::llabs(_encoder_ideal_counts -
                              (static_cast<sq31_31>(encoder_steps) << RADIX));
     return diff <= _encoder_step_threshold;
 }
