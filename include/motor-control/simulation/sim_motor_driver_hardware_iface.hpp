ffb31bf23b38e1faf2efd1f5d57ad5f0f5d1c9b1
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2022-12-06 16:19:23 -0500

    update sim motor driver iface

--- include/motor-control/simulation/sim_motor_driver_hardware_iface.hpp
+++ include/motor-control/simulation/sim_motor_driver_hardware_iface.hpp
@@ -10,17 +10,25 @@ class SimBrushedMotorDriverIface : public BrushedMotorDriverIface {
   public:
     bool start_digital_analog_converter() final { return true; }
     bool stop_digital_analog_converter() final { return true; }
-    bool set_reference_voltage(float) final { return true; }
+    bool set_reference_voltage(float v) final {
+        v_ref = v;
+        return true;
+    }
     void setup() final {}
     void update_pwm_settings(uint32_t) final {}
     auto pwm_active_duty_clamp(uint32_t duty_cycle) -> uint32_t final {
         return std::clamp(duty_cycle, uint32_t(7), uint32_t(100));
     }
     [[nodiscard]] auto get_current_vref() const -> float final {
-        return conf.vref;
-    };
+        return v_ref;
+    }
     [[nodiscard]] auto get_current_duty_cycle() const -> uint32_t final {
-        return current_duty_cycle;
+        return duty_cycle;
     }
 
-}  // namespace sim_brushed_motor_hardware_iface
+  private:
+    float v_ref = 0;
+    uint32_t duty_cycle = 0;
+
+
+};  // namespace sim_brushed_motor_hardware_iface
