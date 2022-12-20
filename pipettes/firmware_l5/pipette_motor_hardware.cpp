7ee7eeea01d1174576a1731cbfe8137fbc1051a3
Ryan howard <ryan.howard@opentrons.com>
2022-12-20 10:53:33 -0500

    also hook up the new tasks to proto pipettes

--- pipettes/firmware_l5/pipette_motor_hardware.cpp
+++ pipettes/firmware_l5/pipette_motor_hardware.cpp
@@ -19,19 +19,15 @@ void MotorHardware::start_timer_interrupt() {
 void MotorHardware::stop_timer_interrupt() {
     motor_hardware_stop_timer(tim_handle);
 }
-auto MotorHardware::check_limit_switch() -> bool {
-    return gpio::is_set(pins.limit_switch);
+void MotorHardware::read_limit_switch() {
+    limit = gpio::is_set(pins.limit_switch);
 }
 
-auto MotorHardware::check_estop_in() -> bool {
-    return gpio::is_set(pins.estop_in);
-}
+void MotorHardware::read_estop_in() { estop = gpio::is_set(pins.estop_in); }
 
-auto MotorHardware::check_sync_in() -> bool {
-    return gpio::is_set(pins.sync_in);
-}
-auto MotorHardware::check_tip_sense() -> bool {
-    return gpio::is_set(pins.tip_sense);
+void MotorHardware::read_sync_in() { sync = gpio::is_set(pins.sync_in); }
+void MotorHardware::read_tip_sense() {
+    tip_sense = gpio::is_set(pins.tip_sense);
 }
 
 void MotorHardware::set_LED(bool status) {
