af2333db45848748ff82e963d32bcea92b2f0569
Ryan howard <ryan.howard@opentrons.com>
2022-12-22 10:58:04 -0500

    format

--- include/motor-control/core/tasks/motor_hardware_task.hpp
+++ include/motor-control/core/tasks/motor_hardware_task.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "motor-control/firmware/stepper_motor/motor_hardware.hpp"
 #include "motor-control/core/motor_hardware_interface.hpp"
+#include "motor-control/firmware/stepper_motor/motor_hardware.hpp"
 #include "pipettes/firmware/pipette_motor_hardware.hpp"
 
 #define TASK_STACK_DEPTH 512
@@ -11,23 +11,25 @@ namespace motor_hardware_task {
 class MotorHardwareTask {
   public:
     MotorHardwareTask(motor_hardware::MotorHardwareIface* hardware,
-                      const char* task_name) : hardware{hardware}, task_name{task_name} {}
+                      const char* task_name)
+        : hardware{hardware}, task_name{task_name} {}
     MotorHardwareTask(const MotorHardwareTask& c) = delete;
     MotorHardwareTask(const MotorHardwareTask&& c) = delete;
     auto operator=(const MotorHardwareTask& c) = delete;
     auto operator=(const MotorHardwareTask&& c) = delete;
     ~MotorHardwareTask() = default;
-    
+
     void start_task() {
-        xTaskCreateStatic(this->task_function, task_name, TASK_STACK_DEPTH, hardware, 6,
-                        backing.data(), &static_task);
+        xTaskCreateStatic(this->task_function, task_name, TASK_STACK_DEPTH,
+                          hardware, 6, backing.data(), &static_task);
     }
-    
+
     /**
      * Task entry point.
      */
-    [[noreturn]] static void task_function(void *hw_ptr) {
-        motor_hardware::MotorHardwareIface* hardware = reinterpret_cast<motor_hardware::MotorHardwareIface*>(hw_ptr);
+    [[noreturn]] static void task_function(void* hw_ptr) {
+        motor_hardware::MotorHardwareIface* hardware =
+            reinterpret_cast<motor_hardware::MotorHardwareIface*>(hw_ptr);
         for (;;) {
             vTaskDelay(10);
             hardware->read_estop_in();
@@ -35,7 +37,6 @@ class MotorHardwareTask {
             hardware->read_sync_in();
         }
     }
-
 
   private:
     motor_hardware::MotorHardwareIface* hardware;
@@ -45,7 +46,6 @@ class MotorHardwareTask {
 };
 }  // namespace motor_hardware_task
 
-
 namespace pipette_motor_hardware_task {
 
 /**
@@ -53,24 +53,28 @@ namespace pipette_motor_hardware_task {
  */
 class PipetteMotorHardwareTask {
   public:
-    PipetteMotorHardwareTask(motor_hardware::PipetteStepperMotorHardwareIface* hardware,
-                      const char* task_name) : hardware{hardware}, task_name{task_name} {}
+    PipetteMotorHardwareTask(
+        motor_hardware::PipetteStepperMotorHardwareIface* hardware,
+        const char* task_name)
+        : hardware{hardware}, task_name{task_name} {}
     PipetteMotorHardwareTask(const PipetteMotorHardwareTask& c) = delete;
     PipetteMotorHardwareTask(const PipetteMotorHardwareTask&& c) = delete;
     auto operator=(const PipetteMotorHardwareTask& c) = delete;
     auto operator=(const PipetteMotorHardwareTask&& c) = delete;
     ~PipetteMotorHardwareTask() = default;
-    
+
     void start_task() {
-        xTaskCreateStatic(this->task_function, task_name, TASK_STACK_DEPTH, hardware, 6,
-                        backing.data(), &static_task);
+        xTaskCreateStatic(this->task_function, task_name, TASK_STACK_DEPTH,
+                          hardware, 6, backing.data(), &static_task);
     }
-    
+
     /**
      * Task entry point.
      */
-     [[noreturn]] static void task_function(void *hw_ptr) {
-        motor_hardware::PipetteStepperMotorHardwareIface* hardware = reinterpret_cast<motor_hardware::PipetteStepperMotorHardwareIface*>(hw_ptr);
+    [[noreturn]] static void task_function(void* hw_ptr) {
+        motor_hardware::PipetteStepperMotorHardwareIface* hardware =
+            reinterpret_cast<motor_hardware::PipetteStepperMotorHardwareIface*>(
+                hw_ptr);
         for (;;) {
             vTaskDelay(10);
             hardware->read_estop_in();
@@ -79,7 +83,6 @@ class PipetteMotorHardwareTask {
             hardware->read_tip_sense();
         }
     }
-
 
   private:
     motor_hardware::PipetteStepperMotorHardwareIface* hardware;
