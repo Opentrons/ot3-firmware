a65052712e110152a17cbdc333e1b859faeaa2fe
Seth Foster <seth@opentrons.com>
2023-01-12 16:48:43 -0500

    remove bootloade rmount id pulls

--- bootloader/firmware/stm32G4/node_id_stm32g4xx.c
+++ bootloader/firmware/stm32G4/node_id_stm32g4xx.c
@@ -15,7 +15,7 @@ static CANNodeId update_dynamic_nodeid() {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     GPIO_InitStruct.Pin = GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
     return determine_pipette_node_id(
         (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) == GPIO_PIN_SET));
