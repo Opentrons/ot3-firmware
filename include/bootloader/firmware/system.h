6684593f7e55547e15fbe11b338e77192dd8b6dc
Seth Foster <seth@opentrons.com>
2023-01-12 14:06:34 -0500

    refactor(bootloader): drive mount id in bootloader
    The head board bootloader now drives its mount id. This uses a new
    system with the C programmer's favorite: a weak definition that can be a
    real function in some compile targets and a default empty function in
    others (this is also the way our interrupt handlers work).

--- include/bootloader/firmware/system.h
+++ include/bootloader/firmware/system.h
@@ -4,3 +4,6 @@
  * Initialize hardware and clocks. Defined in system_stmXXXX.c.
  */
 void HardwareInit(void);
+
+// May be defined on some systems. Has a weakly-defined default.
+void system_specific_startup(void);
