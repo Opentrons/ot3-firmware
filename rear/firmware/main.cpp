1ccd80346d3e46a5597c0450f7657e9fc0ebec55
Ryan howard <ryan.howard@opentrons.com>
2023-01-24 15:30:29 -0500

    use the right eeprom chip

--- rear/firmware/main.cpp
+++ rear/firmware/main.cpp
@@ -33,7 +33,7 @@ class EEPromHardwareInterface
   public:
     EEPromHardwareInterface()
         : eeprom::hardware_iface::EEPromHardwareIface(
-              eeprom::hardware_iface::EEPromChipType::ST_M24128_DF) {}
+              eeprom::hardware_iface::EEPromChipType::ST_M24128_BF) {}
     void set_write_protect(bool enable) final {
         if (enable) {
             disable_eeprom_write();
