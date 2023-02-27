dcac14386f1b760539c72058641a9baecaf777b9
Laura Cox <laura@opentrons.com>
2023-02-27 13:45:04 -0500

    fixup mount id utility function

--- pipettes/firmware/pipette_utility_gpio.h
+++ pipettes/firmware/pipette_utility_gpio.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdbool.h>
 
+#include "pipettes/core/pipette_type.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
@@ -8,7 +10,7 @@ extern "C" {
 // in both cases, 1 means present/high and 0 means absent/low
 
 int utility_gpio_tip_present();
-int utility_gpio_get_mount_id();
+int utility_gpio_get_mount_id(PipetteType pipette_type);
 
 #ifdef __cplusplus
 }  // extern "C"
