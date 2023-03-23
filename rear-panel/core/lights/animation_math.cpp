b98d6469ec5cdb730d6ceaa8d66a37ae58a37f35
Frank Sinapi <frank.sinapi@opentrons.com>
2023-03-23 11:02:13 -0400

    Cleaned up some comments/variable names in animation_math.cpp

--- rear-panel/core/lights/animation_math.cpp
+++ rear-panel/core/lights/animation_math.cpp
@@ -11,24 +11,27 @@ static auto calculate_linear_power(double start_power, double end_power,
     -> double {
     // Simple linear regression. At count of 0ms we return start_power, and at
     // the end we return the end_power.
-    double slope = end_power - start_power;
+    double powerdiff = end_power - start_power;
     double scale =
         static_cast<double>(ms_count) / static_cast<double>(ms_total);
-    return (scale * slope) + start_power;
+    return (scale * powerdiff) + start_power;
 }
 
 static auto calculate_sin_power(double start_power, double end_power,
                                 uint32_t ms_count, uint32_t ms_total)
     -> double {
-    double slope = end_power - start_power;
+    double powerdiff = end_power - start_power;
     // Percent done (count / total)
     double pct = static_cast<double>(ms_count) / static_cast<double>(ms_total);
-    // Scale to rads. 0% should be π rads, 100% should be 2π
+
+    // We want to use the range of cosine that will generate a curve from 
+    // [-1,1]. Therefore, when scaling from time, 0% progress should be π 
+    // rads, and 100% progress should be 2π rads
     double rads = PI + (pct * PI);
     // We want to scale the cosine of the angle in the range [0,1], so we add
     // 1 to the cos and then divide the result by 2
     double scale = (1.0 + std::cos(rads)) / 2.0F;
-    return (scale * slope) + start_power;
+    return (scale * powerdiff) + start_power;
 }
 
 auto lights::math::calculate_power(lights::Transition transition,
