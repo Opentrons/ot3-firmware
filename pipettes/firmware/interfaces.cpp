1b47a1eed6e52118eb366dca16e99dda4c098155
Ryan howard <ryan.howard@opentrons.com>
2023-03-24 17:18:08 -0400

    don't format the includes out of order

--- pipettes/firmware/interfaces.cpp
+++ pipettes/firmware/interfaces.cpp
@@ -1,9 +1,12 @@
+// clang-format off
 #include "FreeRTOS.h"
+#include "system_stm32g4xx.h"
+#include "task.h"
+
+// clang-format on
 #include "pipettes/core/configs.hpp"
 #include "pipettes/firmware/eeprom_keys.hpp"
 #include "pipettes/firmware/interfaces_g4.hpp"
-#include "system_stm32g4xx.h"
-#include "task.h"
 
 using namespace interfaces;
 
