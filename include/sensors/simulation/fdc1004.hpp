07eca7afd13600e6dd2623cda304ddd8cb127e85
Laura Cox <laura@opentrons.com>
2023-04-03 19:12:05 -0400

    formatting will be the death of me

--- include/sensors/simulation/fdc1004.hpp
+++ include/sensors/simulation/fdc1004.hpp
@@ -10,12 +10,14 @@ using namespace i2c::hardware;
 class FDC1004 : public I2CRegisterMap<uint8_t, uint16_t> {
   public:
     FDC1004()
-        : I2CRegisterMap(fdc1004::ADDRESS,
-                         {{static_cast<uint8_t>(fdc1004::Registers::CONF_MEAS1), 0},
-                          {static_cast<uint8_t>(fdc1004::Registers::FDC_CONF), 0},
-                          {static_cast<uint8_t>(fdc1004::Registers::MEAS1_MSB), 100},
-                          {static_cast<uint8_t>(fdc1004::Registers::MEAS1_LSB), 0},
-                          {static_cast<uint8_t>(fdc1004::Registers::DEVICE_ID), fdc1004_utils::DEVICE_ID}}) {}
+        : I2CRegisterMap(
+              fdc1004::ADDRESS,
+              {{static_cast<uint8_t>(fdc1004::Registers::CONF_MEAS1), 0},
+               {static_cast<uint8_t>(fdc1004::Registers::FDC_CONF), 0},
+               {static_cast<uint8_t>(fdc1004::Registers::MEAS1_MSB), 100},
+               {static_cast<uint8_t>(fdc1004::Registers::MEAS1_LSB), 0},
+               {static_cast<uint8_t>(fdc1004::Registers::DEVICE_ID),
+                fdc1004_utils::DEVICE_ID}}) {}
 };
 
 };  // namespace fdc1004_simulator
