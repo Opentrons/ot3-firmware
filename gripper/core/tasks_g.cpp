c8708e16bc09a1db368add6a3e1547a4d389a97d
Ryan howard <ryan.howard@opentrons.com>
2023-04-04 11:45:28 -0400

    disable usage storage for evt gripper

--- gripper/core/tasks_g.cpp
+++ gripper/core/tasks_g.cpp
@@ -23,8 +23,10 @@ static auto brushed_move_group_task_builder =
 static auto brushed_move_status_task_builder = freertos_task::TaskStarter<
     512, move_status_reporter_task::MoveStatusReporterTask>{};
 
+#if PCBA_PRIMARY_REVISION != 'b'
 static auto jaw_usage_storage_task_builder =
     freertos_task::TaskStarter<512, usage_storage_task::UsageStorageTask>{};
+#endif
 
 void g_tasks::start_task(
     brushed_motor::BrushedMotor<lms::GearBoxConfig>& grip_motor,
@@ -38,21 +40,28 @@ void g_tasks::start_task(
     auto& brushed_move_status_reporter = brushed_move_status_task_builder.start(
         5, "bdc move status", g_queues,
         grip_motor.motion_controller.get_mechanical_config(), g_queues);
+#if PCBA_PRIMARY_REVISION != 'b'
     auto& usage_storage_task = jaw_usage_storage_task_builder.start(
         5, "jaw usage storage", g_queues, main_queues);
+#else
+    std::ignore = main_queues;
+#endif
 
     gripper_tasks.brushed_motor_driver = &brushed_motor;
     gripper_tasks.brushed_motion_controller = &brushed_motion;
     gripper_tasks.brushed_move_group = &brushed_move_group;
     gripper_tasks.brushed_move_status_reporter = &brushed_move_status_reporter;
+#if PCBA_PRIMARY_REVISION != 'b'
     gripper_tasks.jaw_usage_storage_task = &usage_storage_task;
-
+#endif
     g_queues.brushed_motor_queue = &brushed_motor.get_queue();
     g_queues.brushed_motion_queue = &brushed_motion.get_queue();
     g_queues.brushed_move_group_queue = &brushed_move_group.get_queue();
     g_queues.brushed_move_status_report_queue =
         &brushed_move_status_reporter.get_queue();
+#if PCBA_PRIMARY_REVISION != 'b'
     g_queues.usage_storage_queue = &usage_storage_task.get_queue();
+#endif
 }
 
 g_tasks::QueueClient::QueueClient()
@@ -80,6 +89,13 @@ void g_tasks::QueueClient::send_brushed_move_status_reporter_queue(
 
 void g_tasks::QueueClient::send_usage_storage_queue(
     const usage_storage_task::TaskMessage& m) {
+#if PCBA_PRIMARY_REVISION == 'b'
+    // this task depends on the eeprom which does not work on the EVT gripper
+    // Since we don't create the task make sure we don't try to write to a
+    // non-existent queue
+    std::ignore = m;
+#else
     usage_storage_queue->try_write(m);
+#endif
 }
 auto g_tasks::get_queues() -> g_tasks::QueueClient& { return g_queues; }
