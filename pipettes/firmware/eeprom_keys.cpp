4d8d158805c788e98d3a9be87087a8bd3da2a71c
Ryan howard <ryan.howard@opentrons.com>
2023-04-21 15:40:59 -0400

    rev all the board's data tables for the new usage data

--- pipettes/firmware/eeprom_keys.cpp
+++ pipettes/firmware/eeprom_keys.cpp
@@ -12,6 +12,11 @@ const eeprom::data_rev_task::DataTableUpdateMessage data_table_rev1_sing_mult{
     .data_table = {std::make_pair(
         PLUNGER_MOTOR_STEP_KEY, usage_storage_task::distance_data_usage_len)}};
 
+const eeprom::data_rev_task::DataTableUpdateMessage data_table_rev2_sing_mult{
+    .data_rev = 2,
+    .data_table = {std::make_pair(P_SM_ERROR_COUNT_KEY,
+                                  usage_storage_task::error_count_usage_len)}};
+
 const eeprom::data_rev_task::DataTableUpdateMessage data_table_rev1_96ch{
     .data_rev = 1,
     .data_table = {
@@ -22,9 +27,20 @@ const eeprom::data_rev_task::DataTableUpdateMessage data_table_rev1_96ch{
         std::make_pair(GEAR_RIGHT_MOTOR_KEY,
                        usage_storage_task::distance_data_usage_len)}};
 
+const eeprom::data_rev_task::DataTableUpdateMessage data_table_rev2_96ch{
+    .data_rev = 2,
+    .data_table = {std::make_pair(P_96_ERROR_COUNT_KEY,
+                                  usage_storage_task::error_count_usage_len),
+                   std::make_pair(L_ERROR_COUNT_KEY,
+                                  usage_storage_task::error_count_usage_len),
+                   std::make_pair(R_ERROR_COUNT_KEY,
+                                  usage_storage_task::error_count_usage_len)}};
+
 const std::vector<eeprom::data_rev_task::DataTableUpdateMessage> table_updater =
     {
         // anytime there is an update to the data table add a message to this
         // vector with the new key/length pairs
         get_pipette_type() == NINETY_SIX_CHANNEL ? data_table_rev1_96ch
-                                                 : data_table_rev1_sing_mult};
+                                                 : data_table_rev1_sing_mult,
+        get_pipette_type() == NINETY_SIX_CHANNEL ? data_table_rev2_96ch
+                                                 : data_table_rev2_sing_mult};
