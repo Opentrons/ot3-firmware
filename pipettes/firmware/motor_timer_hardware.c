891b27d85889ba33bde2f1d085b1d45136febc3c
Laura Cox <laura@opentrons.com>
2023-05-05 15:52:46 -0400

    fix(pipettes): potentially fix the encoder underflow being missed on pipettes

--- pipettes/firmware/motor_timer_hardware.c
+++ pipettes/firmware/motor_timer_hardware.c
@@ -135,7 +135,7 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim) {
         /* Peripheral clock enable */
         __HAL_RCC_TIM2_CLK_ENABLE();
         /* TIM2 interrupt Init */
-        HAL_NVIC_SetPriority(TIM2_IRQn, 7, 0);
+        HAL_NVIC_SetPriority(TIM2_IRQn, 5, 0);
         HAL_NVIC_EnableIRQ(TIM2_IRQn);
     }
 }
