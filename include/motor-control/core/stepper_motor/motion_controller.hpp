004c1c384ca362cbdd28273273a4a2c777b8534d
Ryan howard <ryan.howard@opentrons.com>
2023-05-10 14:35:18 -0400

    turn off the encoder when a home finish, and ensure its on before commanding any move

--- include/motor-control/core/stepper_motor/motion_controller.hpp
+++ include/motor-control/core/stepper_motor/motion_controller.hpp
@@ -72,6 +72,7 @@ class MotionController {
             .stop_condition =
                 static_cast<MoveStopCondition>(can_msg.request_stop_condition),
             .usage_key = hardware.get_usage_eeprom_config().get_distance_key()};
+        hardware.enable_encoder();
         if (!enabled) {
             enable_motor();
         }
@@ -90,6 +91,7 @@ class MotionController {
             .seq_id = can_msg.seq_id,
             .stop_condition = MoveStopCondition::limit_switch,
             .usage_key = hardware.get_usage_eeprom_config().get_distance_key()};
+        hardware.enable_encoder();
         if (!enabled) {
             enable_motor();
         }
@@ -239,6 +241,8 @@ class PipetteMotionController {
             hardware.get_step_tracker(),
             can_msg.action,
             gear_motor_id};
+
+        hardware.enable_encoder();
         if (!enabled) {
             enable_motor();
         }
--- include/motor-control/core/stepper_motor/motor_interrupt_handler.hpp
+++ include/motor-control/core/stepper_motor/motor_interrupt_handler.hpp
@@ -242,6 +242,7 @@ class MotorInterruptHandler {
                 hardware.get_encoder_pulses();
             hardware.reset_step_tracker();
             hardware.reset_encoder_pulses();
+            hardware.disable_encoder();
             stall_checker.reset_itr_counts(0);
             hardware.position_flags.set_flag(
                 can::ids::MotorPositionFlags::stepper_position_ok);
