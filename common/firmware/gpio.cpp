f2762c0eab2f4c0f5939881136e3fcb0147c5a97
Frank Sinapi <frank.sinapi@opentrons.com>
2023-06-02 15:08:19 -0400

    Hard coded some common utils to go in ccmram

--- common/firmware/gpio.cpp
+++ common/firmware/gpio.cpp
@@ -2,14 +2,17 @@
 
 #include "gpio.h"
 
+__attribute__((section( ".ccmram" )))
 auto gpio::set(const gpio::PinConfig& pc) -> void {
     return gpio_set(pc.port, pc.pin, pc.active_setting);
 }
 
+__attribute__((section( ".ccmram" )))
 auto gpio::reset(const gpio::PinConfig& pc) -> void {
     return gpio_reset(pc.port, pc.pin, pc.active_setting);
 }
 
+__attribute__((section( ".ccmram" )))
 auto gpio::is_set(const gpio::PinConfig& pc) -> bool {
     return gpio_is_set(pc.port, pc.pin, pc.active_setting);
 }
