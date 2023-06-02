1929d5b79a7acd2f0f57a0828e0883edf6d00201
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2023-06-02 12:53:47 -0400

    math is hard

--- include/motor-control/core/linear_motion_system.hpp
+++ include/motor-control/core/linear_motion_system.hpp
@@ -45,7 +45,7 @@ struct LinearMotionSystemConfig {
         return (steps_per_rev * microstep) / (mech_config.get_mm_per_rev());
     }
     [[nodiscard]] constexpr auto get_usteps_per_um() const -> float {
-        return (steps_per_rev * microstep) / (mech_config.get_mm_per_rev()) * 1000.0;
+        return (steps_per_rev * microstep) / (mech_config.get_mm_per_rev() * 1000.0);
     }
     [[nodiscard]] constexpr auto get_um_per_step() const -> float {
         return (mech_config.get_mm_per_rev()) / (steps_per_rev * microstep) *
