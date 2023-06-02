e555e6d69f1efb1c2392895875315c533890d524
Frank Sinapi <frank.sinapi@opentrons.com>
2023-06-02 15:14:33 -0400

    Formatting

--- motor-control/core/types.cpp
+++ motor-control/core/types.cpp
@@ -1,23 +1,22 @@
 #include "motor-control/core/types.hpp"
 
-__attribute__((section( ".ccmram" )))
-auto MotorPositionStatus::set_flag(MotorPositionStatus::Flags flag) -> void {
+__attribute__((section(".ccmram"))) auto MotorPositionStatus::set_flag(
+    MotorPositionStatus::Flags flag) -> void {
     backing.fetch_or(static_cast<uint8_t>(flag));
 }
 
-__attribute__((section( ".ccmram" )))
-auto MotorPositionStatus::clear_flag(MotorPositionStatus::Flags flag) -> void {
+__attribute__((section(".ccmram"))) auto MotorPositionStatus::clear_flag(
+    MotorPositionStatus::Flags flag) -> void {
     backing.fetch_and(~static_cast<uint8_t>(flag));
 }
 
-__attribute__((section( ".ccmram" )))
-[[nodiscard]] auto MotorPositionStatus::check_flag(
-    MotorPositionStatus::Flags flag) const -> bool {
+__attribute__((section(".ccmram"))) [[nodiscard]] auto
+MotorPositionStatus::check_flag(MotorPositionStatus::Flags flag) const -> bool {
     return (backing.load() & static_cast<uint8_t>(flag)) ==
            static_cast<uint8_t>(flag);
 }
 
-__attribute__((section( ".ccmram" )))
-[[nodiscard]] auto MotorPositionStatus::get_flags() const -> uint8_t {
+__attribute__((section(".ccmram"))) [[nodiscard]] auto
+MotorPositionStatus::get_flags() const -> uint8_t {
     return backing.load();
 }
