7d68e705e508ca98780bc9d9c5704c90bb26d6a9
Frank Sinapi <frank.sinapi@opentrons.com>
2023-06-29 11:21:26 -0400

    format format format format format format

--- i2c/firmware/i2c_comms.cpp
+++ i2c/firmware/i2c_comms.cpp
@@ -1,6 +1,5 @@
 #include "i2c/firmware/i2c_comms.hpp"
 
-
 using namespace i2c::hardware;
 
 /*
@@ -23,7 +22,7 @@ auto I2C::central_receive(uint8_t* data, uint16_t size, uint16_t dev_address,
     return hal_i2c_master_receive(handle, dev_address, data, size, timeout);
 }
 
-auto I2C::set_handle(HAL_I2C_HANDLE i2c_handle) -> void { 
-    handle = i2c_handle; 
+auto I2C::set_handle(HAL_I2C_HANDLE i2c_handle) -> void {
+    handle = i2c_handle;
     i2c_register_handle(handle);
 }
