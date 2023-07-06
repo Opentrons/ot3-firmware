c48e7bd1b7245909abe50c760567714293c3af26
Frank Sinapi <frank.sinapi@opentrons.com>
2023-07-06 17:09:21 -0400

    Make the encoder overflow atomic

--- include/motor-control/firmware/stepper_motor/motor_hardware.hpp
+++ include/motor-control/firmware/stepper_motor/motor_hardware.hpp
@@ -76,7 +76,7 @@ class MotorHardware : public StepperMotorHardwareIface {
     void* tim_handle;
     void* enc_handle;
     const UsageEEpromConfig& eeprom_config;
-    int32_t motor_encoder_overflow_count = 0;
+    std::atomic<int32_t> motor_encoder_overflow_count = 0;
     std::atomic<bool> cancel_request = false;
 };
 
