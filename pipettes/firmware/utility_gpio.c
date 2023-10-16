1dae3d9aa71410b4b60b3d99d03e74f79eb40c5f
caila-marashaj <caila.marashaj@opentrons.com>
2023-10-16 15:50:13 -0400

    remove nvic setup for 96ch interrupts

--- pipettes/firmware/utility_gpio.c
+++ pipettes/firmware/utility_gpio.c
@@ -52,26 +52,10 @@ static void tip_sense_gpio_init() {
  * @retval None
  */
 static void nvic_priority_enable_init() {
-    PipetteType pipette_type = get_pipette_type();
-
-    if (pipette_type == NINETY_SIX_CHANNEL) {
-        IRQn_Type block_9_5 = get_interrupt_line(gpio_block_9_5);
-        IRQn_Type block_15_10 = get_interrupt_line(gpio_block_15_10);
-        /* EXTI interrupt init tip sense rear*/
-        HAL_NVIC_SetPriority(block_9_5, 10, 0);
-        HAL_NVIC_EnableIRQ(block_9_5);
-
-        /* EXTI interrupt init tip sense front*/
-        HAL_NVIC_SetPriority(block_15_10, 10, 0);
-        HAL_NVIC_EnableIRQ(block_15_10);
-    } else {
-        IRQn_Type block_2 = get_interrupt_line(gpio_block_2);
-        /* EXTI interrupt init block tip sense*/
-        HAL_NVIC_SetPriority(block_2, 10, 0);
-        HAL_NVIC_EnableIRQ(block_2);
-
-    }
-
+    IRQn_Type block_2 = get_interrupt_line(gpio_block_2);
+    /* EXTI interrupt init block tip sense*/
+    HAL_NVIC_SetPriority(block_2, 10, 0);
+    HAL_NVIC_EnableIRQ(block_2);
 }
 
 /**
