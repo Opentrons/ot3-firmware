762178e985fd874246a5c059502fa522402c13a1
vegano1 <brayan.almonte@gmail.com>
2024-01-12 15:04:57 -0500

    remove unused test function

--- common/firmware/gpio.h
+++ common/firmware/gpio.h
@@ -13,8 +13,6 @@ void gpio_reset(void* port, uint16_t pin, uint8_t active_setting);
 
 bool gpio_is_set(void* port, uint16_t pin, uint8_t active_setting);
 
-uint8_t gpio_read(void* port, uint16_t pin);
-
 #ifdef __cplusplus
 }  // extern "C"
 #endif  // __cplusplus
