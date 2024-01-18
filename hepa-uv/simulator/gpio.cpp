736ee454bed5ae7c2e889d4bae340ae1c818ff02
vegano1 <brayan.almonte@gmail.com>
2024-01-18 10:16:53 -0500

    add fake gpio?

---
+++ hepa-uv/simulator/gpio.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstdint>
+
+namespace gpio {
+
+struct PinConfig {
+    void* port;
+    uint16_t pin;
+    uint8_t active_setting;
+};
+
+auto set(const PinConfig& pc) -> void;
+auto reset(const PinConfig& pc) -> void;
+auto is_set(const PinConfig& pc) -> bool;
+
+};  // namespace gpio
