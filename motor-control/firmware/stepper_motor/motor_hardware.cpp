fb13acb42089c95c23e9ead25c6c2a0d153a24ee
Brayan Almonte <vegano1@users.noreply.github.com>
2024-04-16 15:57:00 -0400

    fix(motor-control): add delay to give enough time for ebrake to physically disengage. (#769)

--- motor-control/firmware/stepper_motor/motor_hardware.cpp
+++ motor-control/firmware/stepper_motor/motor_hardware.cpp
@@ -18,14 +18,14 @@ void MotorHardware::activate_motor() {
     gpio::set(pins.enable);
     if (pins.ebrake.has_value()) {
         gpio::reset(pins.ebrake.value());
+        motor_hardware_delay(20);
     }
 }
 void MotorHardware::deactivate_motor() {
     if (pins.ebrake.has_value()) {
         gpio::set(pins.ebrake.value());
-        motor_hardware_delay(10);
+        motor_hardware_delay(20);
     }
-    motor_hardware_delay(10);
     gpio::reset(pins.enable);
 }
 void MotorHardware::start_timer_interrupt() {
