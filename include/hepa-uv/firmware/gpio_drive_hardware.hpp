683a52c69a64bdd2e8f19d4ade5258c5be186f48
vegano1 <brayan.almonte@gmail.com>
2024-06-17 11:56:37 -0400

    lint!!

--- include/hepa-uv/firmware/gpio_drive_hardware.hpp
+++ include/hepa-uv/firmware/gpio_drive_hardware.hpp
@@ -6,7 +6,7 @@
 
 namespace gpio_drive_hardware {
 
-// NOLINTBEGIN
+// NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init)
 struct GpioDrivePins {
     gpio::PinConfig door_open;
     gpio::PinConfig reed_switch;
@@ -16,6 +16,5 @@ struct GpioDrivePins {
     gpio::PinConfig uv_on_off;
     std::optional<gpio::PinConfig> safety_relay_active = std::nullopt;
 };
-// NOLINTEND
 
 }  // namespace gpio_drive_hardware
