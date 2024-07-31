b3e812eebd2155af387f646556237fae87ee03a3
pmoegenburg <pbm53@cornell.edu>
2024-07-31 11:19:04 -0400

    proposed change

--- head/firmware/stm32g4xx_it.c
+++ head/firmware/stm32g4xx_it.c
@@ -182,7 +182,8 @@ void EXTI15_10_IRQHandler(void) {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_15);
     /*if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_13)) {
         HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
-    } else if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_15)) {
+    }
+    if (__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_15)) {
         HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_15);
     }*/
 }
