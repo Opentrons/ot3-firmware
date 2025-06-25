ee50c718886ce3da93b01c9185a546f7677449fe
Seth Foster <seth@opentrons.com>
2025-06-25 12:43:26 -0400

    read all about it

--- common/cmake/ClangTidy.cmake
+++ common/cmake/ClangTidy.cmake
@@ -62,7 +62,7 @@ macro(add_clang_tidy_target)
   # This helps with clang accepting what GCC accepts around the implementations of the message queue
   list(APPEND _actt_clang_extra_args_${_actt_TARGET_NAME} "--extra-arg=-frelaxed-template-template-args")
   # This helps with modern clang-tidy not liking the gcc specs setting for cross builds
-  list(APPEND _actt_clang_extra_args_${_actt_TARGET_NAME}  "-Wno-error=unused-command-line-argument")
+  list(APPEND _actt_clang_extra_args_${_actt_TARGET_NAME}  "--extra-arg=-Wno-error=unused-command-line-argument")
   add_custom_target(${_actt_TARGET_NAME}
     ALL
     COMMAND ${Clang_CLANGTIDY_EXECUTABLE} "--extra-arg=-DOPENTRONS_CLANG_TIDY_WORKAROUND_44178" ${_actt_clang_extra_args_${_actt_TARGET_NAME}} -p ${CMAKE_BINARY_DIR} ${_actt_LINT_SOURCES})
