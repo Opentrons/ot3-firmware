4f0f2cac83191054cd8ad87a3e929db7bd97b46d
Seth Foster <seth@opentrons.com>
2025-07-31 12:53:51 -0400

    fix: buffer backpressures to MSR
    propagate queue-full failures to send back to the move status reporter
    task, which should ensure that acks get sent when they might otherwise
    have not made it into the buffer.

--- include/motor-control/core/tasks/move_status_reporter_task.hpp
+++ include/motor-control/core/tasks/move_status_reporter_task.hpp
@@ -9,6 +9,7 @@
 #include "motor-control/core/tasks/messages.hpp"
 #include "motor-control/core/tasks/usage_storage_task.hpp"
 #include "motor-control/core/utils.hpp"
+#include "common/core/hardware_delay.hpp"
 
 namespace move_status_reporter_task {
 
@@ -46,7 +47,9 @@ class MoveStatusMessageHandler {
     void handle_message(std::monostate&) {}
 
     void handle_message(const can::messages::ErrorMessage& msg) {
-        can_client.send_can_message(can::ids::NodeId::host, msg);
+        while (!can_client.send_can_message(can::ids::NodeId::host, msg)) {
+            vtask_hardware_delay(1);
+        }
     }
 
     void handle_message(const can::messages::StopRequest& msg) {
@@ -69,7 +72,9 @@ class MoveStatusMessageHandler {
             .encoder_position_um = end_position,
             .position_flags = message.position_flags,
             .ack_id = static_cast<uint8_t>(message.ack_id)};
-        can_client.send_can_message(can::ids::NodeId::host, msg);
+        while (!can_client.send_can_message(can::ids::NodeId::host, msg)) {
+            vtask_hardware_delay(1);
+        }
 
         int32_t distance_traveled_um =
             end_position - fixed_point_multiply(um_per_encoder_pulse,
@@ -90,7 +95,9 @@ class MoveStatusMessageHandler {
             .encoder_position = fixed_point_multiply(
                 um_per_encoder_pulse, message.encoder_pulses, radix_offset_0{}),
             .position_flags = message.position_flags};
-        can_client.send_can_message(can::ids::NodeId::host, msg);
+        while (!can_client.send_can_message(can::ids::NodeId::host, msg)) {
+            vtask_hardware_delay(1);
+        }
     }
 
     void handle_message(const usage_messages::IncreaseForceTimeUsage& message) {
