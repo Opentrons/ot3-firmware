004d100b5919d4bf4118fdd2e4fb785bf2ea0639
Seth Foster <seth@opentrons.com>
2025-08-01 15:31:55 -0400

    fix: move-complete message retries (#829)
    * fix: enable fdcan transmit pause
    
    This causes transmitters to back off for 2 bit times on
    repeated-transmit, which allows transmitters that might otherwise be
    bursting with high-priority messages to let their fellows get a word in edgewise.
    
    * fix: buffer backpressures to MSR
    
    propagate queue-full failures to send back to the move status reporter
    task, which should ensure that acks get sent when they might otherwise
    have not made it into the buffer.

--- pipettes/tests/test_gear_move_status_handling.cpp
+++ pipettes/tests/test_gear_move_status_handling.cpp
@@ -19,8 +19,9 @@ struct MockCanClient {
     std::deque<std::pair<can::ids::NodeId, can::messages::ResponseMessageType>>
         queue{};
     auto send_can_message(can::ids::NodeId node_id,
-                          const can::messages::ResponseMessageType& m) -> void {
+                          const can::messages::ResponseMessageType& m) -> bool {
         queue.push_back(std::make_pair(node_id, m));
+        return true;
     }
 };
 struct MockUsageClient {
