53924a2aac4596f49b278b50c749d9f306a47411
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-03-06 15:34:31 -0500

    Add unit tests for boundary-setting

---
+++ eeprom/tests/test_update_data_rev_task.cpp
@@ -0,0 +1,44 @@
+#include <cstring>
+#include "../../include/eeprom/core/dev_data.hpp"
+
+extern "C" {
+    void vTaskDelay(const int x) {std::ignore = x;}
+    void vTaskDelete(void* x) {std::ignore = x;}
+}
+
+#include "../../include/eeprom/core/types.hpp"
+#include "../../include/eeprom/core/update_data_rev_task.hpp"
+#include "../../include/eeprom/tests/mock_eeprom_task_client.hpp"
+#include "../../stm32-tools/catch2/src/single_include/catch2/catch.hpp"
+
+using namespace eeprom;
+
+SCENARIO("Sending migrate data message") {
+    // migrate data message
+    const uint16_t data_revision = 1;
+    std::vector<std::pair<types::address, types::data_length>> data = {
+        {0, 8},
+        {1, 8},
+        {2, 8},
+    };
+
+    const data_rev_task::MigrateDataMessage mock_data_message{
+        .data_rev = data_revision,
+        .data_table = data
+    };
+
+    // Test handler
+    auto eeprom_client = MockEEPromTaskClient{};
+    auto tail_accessor = eeprom::dev_data::DevDataTailAccessor{eeprom_client};
+    auto data_rev_handler = data_rev_task::UpdateDataRevHandler{
+    eeprom_client, tail_accessor};
+
+    GIVEN("Finding boundary of old data") {
+        data_rev_handler.handle_message(mock_data_message);
+
+        THEN("address should have updated to reflect the new location") {
+            REQUIRE(addresses::ot_library_begin == addresses::data_address_begin);
+            REQUIRE(addresses::ot_library_end == 64);
+        }
+    }
+}
