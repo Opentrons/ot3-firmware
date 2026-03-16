f5e0cc677de7b2a4bb05abd8b79abf828ece6048
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-03-16 13:32:39 -0400

    modifying PageType struct and BookAccessor scaffolding

--- include/eeprom/core/ot_library_accessor.hpp
+++ include/eeprom/core/ot_library_accessor.hpp
@@ -1,23 +1,41 @@
 #pragma once
 #include <cstdint>
-#include <queue>
 
 #include "accessor.hpp"
+#include "addresses.hpp"
 #include "task.hpp"
 
 namespace eeprom {
 namespace ot_library_accessor {
 
-template <std::size_t SIZE>
-using DataBufferType = std::array<uint8_t, SIZE>;
+template <size_t SIZE>
+using DataBufferType = std::array<std::byte, SIZE>;
 using DataTailType =
     std::array<uint8_t, eeprom::addresses::lookup_table_tail_length>;
+
+/*
+ * The PageType represents a page on which data will exist. It will be used to
+ * organise data during reads and writes.
+ * */
+template <size_t SIZE>
+struct PageType {
+    uint16_t crc;
+    uint8_t length;
+    DataBufferType<SIZE> data;
+
+    struct checkValid {
+        uint8_t errors;
+        uint16_t counter;
+        uint8_t retire;
+    };
+};
 
 /**OTLibraryAccessor**/
 template <task::TaskClient EEpromTaskClient,
           eeprom::types::address ot_library_begin>
 class OTLibraryAccessor
-    : public eeprom::accessor::EEPromAccessor<EEpromTaskClient, data_begin> {
+    : public eeprom::accessor::EEPromAccessor<EEpromTaskClient,
+                                              addresses::ot_library_begin> {
   public:
     template <std::size_t SIZE>
     explicit OTLibraryAccessor(EEpromTaskClient& eeprom_client,
@@ -30,7 +48,20 @@ class OTLibraryAccessor
                   eeprom_client, readListener,
                   accessor::AccessorBuffer(buffer.begin(), buffer.end())){};
 
-    void write_data(uint16_t key, uint16_t len) { std::ignore = key, len; }
+    template <size_t SIZE>
+    void write_data(uint16_t key, uint16_t len,
+                    std::array<std::byte, SIZE> data) {
+        PageType<SIZE> page;
+
+        // Organise data to be sent to book
+        uint16_t crc = calculate_crc(data);
+        page.crc = crc;
+        page.length = sizeof(data);
+        page.data = data;
+
+        // TODO Create book accessor and send this to book accessor to perform
+        // actual write
+    };
 
   private:
     auto calculate_crc(uint16_t data) -> uint16_t {
@@ -38,5 +69,10 @@ class OTLibraryAccessor
         std::ignore = data;
     }
 };
+
+template <task::TaskClient EEpromTaskClient>
+class BookAccessor
+    : public eeprom::accessor::EEPromAccessor<EEpromTaskClient,
+                                              addresses::ot_library_begin> {};
 }  // namespace ot_library_accessor
 }  // namespace eeprom
