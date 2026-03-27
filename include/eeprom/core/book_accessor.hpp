2cd4224061460af2b29d2419edc65ef1784371ac
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-03-27 18:01:48 -0400

    Begin read mechanic

--- include/eeprom/core/book_accessor.hpp
+++ include/eeprom/core/book_accessor.hpp
@@ -2,9 +2,13 @@
 #include <bitset>
 #include <cstddef>
 #include <cstdint>
+#include <vector>
 
 #include "accessor.hpp"
 #include "addresses.hpp"
+#include "common/core/bit_utils.hpp"
+#include "dev_data.hpp"
+#include "messages.hpp"
 #include "task.hpp"
 #include "types.hpp"
 
@@ -15,6 +19,8 @@ template <size_t SIZE>
 using DataBufferType = std::array<std::byte, SIZE>;
 using DataTailType =
     std::array<uint8_t, eeprom::addresses::lookup_table_tail_length>;
+using TableAction = dev_data::TableAction;
+using table_entry_action = dev_data::table_entry_action;
 
 /*
  * The PageType represents a page on which data will exist. It will be used to
@@ -25,54 +31,140 @@ struct PageType {
     std::array<std::byte, 2> crc;
     uint8_t length;
     DataBufferType<SIZE> data;
+    uint16_t counter;
 
     struct checkValid {
         uint8_t errors;
-        uint16_t counter;
         uint8_t retire;
     };
 };
 
+struct BookAccessorIntermediate {
+  protected:
+    DataBufferType<64> intermediate_buffer;
+};
+
 /* Addresses are taken as Bit Arrays, Make sure these arrays are BigEngdian*/
-template <task::TaskClient EEpromTaskClient>
+template <task::TaskClient EEpromTaskClient, size_t SIZE>
 class BookAccessor
     : public eeprom::accessor::EEPromAccessor<EEpromTaskClient,
-                                              addresses::ot_library_begin> {
+                                              addresses::ot_library_begin>,
+      eeprom::accessor::ReadListener,
+      BookAccessorIntermediate {
   public:
-    template <size_t SIZE>
     explicit BookAccessor(EEpromTaskClient& eeprom_client,
                           accessor::ReadListener& read_listener,
                           DataBufferType<SIZE>& buffer)
-        : accessor::EEPromAccessor<EEpromTaskClient,
+        : read_listener(read_listener),
+          buffer(buffer),
+          BookAccessorIntermediate(),
+          accessor::EEPromAccessor<EEpromTaskClient,
                                    addresses::ot_library_begin>(
-              eeprom_client, read_listener,
-              accessor::AccessorBuffer(buffer.begin(), buffer.end())){};
+              eeprom_client, *this,
+              accessor::AccessorBuffer(intermediate_buffer.begin(),
+                                       intermediate_buffer.end())) {
+        ;
+        eeprom_client.send_eeprom_queue(
+            message::ConfigRequestMessage{config_req_callback, this});
+    }
 
-    template <size_t SIZE>
     void create_new_data_part(uint8_t key, uint16_t len,
                               std::array<std::byte, SIZE> data) {
         std::ignore = key, len, data;
     }
 
-    template <size_t SIZE>
     void write_data(uint8_t key, uint16_t len,
                     std::array<std::byte, SIZE> data) {
         std::ignore = key, len, data;
     }
 
-    // WRITE_DATA CONVENIENCE METHODS
+    // write WRITE_DATA CONVENIENCE METHODS
 
-    void get_data(uint8_t key, uint16_t len, uint16_t offset,
+    void get_data(uint16_t key, uint16_t len, uint16_t offset,
                   uint32_t message_index) {
-        // figure otut what the offset and message_index stuff are for
+        if (read_write_ready()) {
+            auto table_location = calculate_table_entry_start(key);
+            if (table_location > tail_accessor.get_data_tail()) {
+                LOG("Error, attemping to read uninitalized value");
+                return;
+            }
+            action_cmd_m = table_entry_action{.key = key,
+                                              .offset = offset,
+                                              .len = len,
+                                              .action = TableAction::READ};
+
+            // call a read to the table entry so we know where
+            // to read the data
+            //
+            // MAKE A READ CALLBACK
+            this->eeprom_client.send_eeprom_queue(message::ReadEepromMessage{
+                .message_index = message_index,
+                .memory_address = table_location,
+                .length = static_cast<types::data_length>(2 * conf.addr_bytes),
+                .callback = table_action_callback,
+                .callback_param = this});
+        }
+    }
+
+    auto read_write_ready() -> bool {
+        return table_ready() && tail_accessor.data_rev_complete();
+    }
+
+    auto table_ready() -> bool {
+        return config_updated && tail_accessor.get_tail_updated();
+    }
+
+    void read_complete(uint32_t message_index) override {
+        // 1. save what's in buffer to FullRead.reads
+        std::copy_n(buffer.begin(), buffer.size(),
+                    check_read.reads[check_read.book_index]);
+        // increment check_read
+        check_read.book_index += 1;
+        // 2. check if reads length >= 4
+        //
+        if (check_read.book_index < 4) {
+            // turn a book address into a regular address
+            auto read_address =
+                static_cast<types::address>(check_read.book_address) << 8;
+
+            types::address page_relative = 0;
+
+            // move the address to read different pages inside book
+            switch (check_read) {
+                case 1:
+                    page_relative = 0b0000000001000000;
+                case 2:
+                    page_relative = 0b0000000010000000;
+                case 3:
+                    page_relative = 0b0000000011000000;
+            }
+            read_address &= page_relative;
+
+            this->start_read_at_offset(read_address, read_address + 64,
+                                       message_index);
+        }
+        // 3. if yes, call new method "read_final"
+        // 4. if no, initiate another read at next page address
     }
 
     // GET_DATA CONVENIENCE METHODS
   private:
+    struct FullRead {
+        types::book_address book{};
+        uint8_t book_index = 0;
+        std::array<std::array<std::byte, SIZE>, 4> reads;
+    };
+
     // fields, decide what they are
     // Add a tail accessor?
+    dev_data::DevDataTailAccessor<EEpromTaskClient>& tail_accessor;
+    message::ConfigResponseMessage conf = message::ConfigResponseMessage{};
+    bool config_updated{false};
+    table_entry_action action_cmd_m = dev_data::table_entry_action{};
+    ReadListener& read_listener;
+    DataBufferType<SIZE> buffer;
+    FullRead check_read;
 
-    template <size_t SIZE>
     auto calc_crc(std::array<std::byte, SIZE> data)
         -> std::array<std::byte, 2> {
         // convert data array into a bitset, to make bit manipulation easier
@@ -132,8 +224,12 @@ class BookAccessor
         return bits;
     }
 
+    void read_final(uint16_t message_index) {
+        // TODO: find most current value
+        read_listener.read_complete(message_index);
+    }
+
     // convert bitset to bytes
-    template <uint16_t SIZE>
     auto bitsettobytes(std::bitset<SIZE> bits)
         -> std::array<std::byte, SIZE / 8> {
         std::array<std::byte, SIZE / 8> output{};
@@ -149,11 +245,73 @@ class BookAccessor
         return output;
     }
 
-    template <size_t SIZE>
     void write_callback(uint8_t key, uint16_t len,
                         std::array<std::byte, SIZE> data) {
         std::ignore = data;
     }
+
+    // Methods from DevDataAccessor
+
+    // callbacks
+    void config_req_callback(const message::ConfigResponseMessage& m) {
+        conf = m;
+        config_updated = true;
+        tail_accessor.set_config(conf);
+        tail_accessor.start_read(0);
+    }
+
+    static void config_req_callback(const message::ConfigResponseMessage& m,
+                                    void* param) {
+        auto* self =
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+            reinterpret_cast<dev_data::DevDataAccessor<EEpromTaskClient>*>(
+                param);
+        self->config_req_callback(m);
+    }
+    // Calculates data's location on the lookup table
+    auto calculate_table_entry_start(uint16_t key) -> types::address {
+        types::address addr = 0;
+        if (config_updated) {
+            addr = addresses::data_address_begin + (key * 2 * conf.addr_bytes);
+        }  // todo else
+        return addr;
+    }
+
+    void table_entry_action(const message::EepromMessage& m) {
+        const auto* data_iter = m.data.begin();
+        types::address data_addr = 0;
+        types::data_length data_len = 0;
+        data_iter = bit_utils::bytes_to_int(
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            data_iter, data_iter + conf.addr_bytes, data_addr);
+        data_iter = bit_utils::bytes_to_int(
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            data_iter, data_iter + conf.addr_bytes, data_len);
+        if (conf.chip == hardware_iface::EEPromChipType::MICROCHIP_24AA02T) {
+            data_addr = data_addr >> hardware_iface::ADDR_BITS_DIFFERENCE;
+            data_len = data_len >> hardware_iface::ADDR_BITS_DIFFERENCE;
+        }
+
+        switch (action_cmd_m.action) {
+            case TableAction::INITALIZE:
+                [[fallthrough]];
+            case TableAction::CREATE:
+                [[fallthrough]];
+            case TableAction::WRITE:
+                [[fallthrough]];
+            case TableAction::READ:
+                data_addr += action_cmd_m.offset;
+                // if the read action has length 0, read the whole value else
+                // only read as much as requested
+                if (action_cmd_m.len != 0) {
+                    data_len = action_cmd_m.len;
+                }
+                this->start_read_at_offset(data_addr, data_addr + 64,
+                                           m.message_index);
+                break;
+        }
+    }
 };
+
 }  // namespace ot_library_accessor
 }  // namespace eeprom
