e980a169651ec27ef0037d14ddfa4f51b959aafc
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-06-10 12:25:44 -0400

    change message sizes

--- include/eeprom/core/accessor.hpp
+++ include/eeprom/core/accessor.hpp
@@ -126,9 +126,6 @@ class EEPromAccessor {
                 .length = amount_to_write,
                 .data = write});
 
-            //            vTaskDelay(2 * portTICK_PERIOD_MS);  // delay to avoid
-            //            overwhelming
-            // the i2c task
             write_addr += amount_to_write;
             // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
             type_iter += amount_to_write;
