4855bb7b07d6aa236567c0220c71f3eab785affd
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-06-17 13:22:16 -0400

    more experimentation

--- eeprom/simulation/crc16.cpp
+++ eeprom/simulation/crc16.cpp
@@ -12,10 +12,9 @@ uint16_t crc16_accumulate(const uint8_t* data, uint8_t length) {
     return crc16_compute(data, length);
 }
 
-__attribute__((weak)) void crc16_init(void) {}
+void crc16_init(void) {}
 
-__attribute__((weak)) uint16_t crc16_compute(const uint8_t* data,
-                                             uint8_t length) {
+uint16_t crc16_compute(const uint8_t* data, uint8_t length) {
     // Guard against zero-length data (like empty_data initialization)
     // to prevent underflows or out-of-bound indexing in the bitset logic
     if (length == 0 || data == nullptr) {
