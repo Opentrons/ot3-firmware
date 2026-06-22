f8d1b1971129f9e97f22f18672be2168c80aac04
munanwugo-opentrons <muna.nwugo@opentrons.com>
2026-06-22 12:17:25 -0400

    fix pipette simulators

--- pipettes/simulator/main.cpp
+++ pipettes/simulator/main.cpp
@@ -115,6 +115,8 @@ static auto& sensor_queue_client = sensor_tasks::get_queues();
 
 static auto tail_accessor =
     eeprom::dev_data::DevDataTailAccessor{sensor_queue_client};
+static auto book_tail_accessor =
+    eeprom::dev_data::DevDataTailAccessor{sensor_queue_client};
 
 auto initialize_motor_tasks(
     can::ids::NodeId id,
@@ -137,7 +139,7 @@ auto initialize_motor_tasks(
     linear_motor_tasks::start_tasks(
         *central_tasks::get_tasks().can_writer, linear_motion_control,
         peripheral_tasks::get_spi_client(), conf.linear_motor, id, lmh_tsk,
-        tail_accessor);
+        tail_accessor, book_tail_accessor);
 
     // TODO Convert gear motor tasks
     gear_motor_tasks::start_tasks(
@@ -177,7 +179,7 @@ auto initialize_motor_tasks(
     linear_motor_tasks::start_tasks(
         *central_tasks::get_tasks().can_writer, linear_motion_control,
         peripheral_tasks::get_spi_client(), conf.linear_motor, id, lmh_tsk,
-        tail_accessor);
+        tail_accessor, book_tail_accessor);
 }
 
 auto handle_options(int argc, char** argv) -> po::variables_map {
